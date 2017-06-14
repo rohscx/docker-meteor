86964470cc6053e48c9c9874ebf284ebe8971ea1
rohscx <R_E__g_@Macintosh.local>
2017-06-14 14:18:00 -0400

    setup

--- server/main.js
+++ server/main.js
@@ -1,4 +1,11 @@
 import { Meteor } from 'meteor/meteor';
+
+const Tests = new Mongo.collection('tests');
+
 Meteor.startup(() => {
+  Test.insert({
+    name: 'Hi',
+    value: 1
+  });
   // code to run on server at startup
 });
