72416831f1267be48c77a82637588c496d8a2695
rohscx <R_E__g_@Macintosh.local>
2017-06-14 15:42:14 -0400

    setup

--- imports/client/Item.js
+++ imports/client/Item.js
@@ -3,9 +3,18 @@ import React, {Component} from 'react';
  export default class Item extends Component {
    render() {
      return (
-       <div>
-         {this.props.item.itemOne.text}
+       <div className='item'>
+         <div className='vote-one'>
+         <span>{this.props.item.itemOne.value}</span>
+         <h3>{this.props.item.itemOne.text}</h3>
        </div>
+       <span>VS</span>
+         <div className='vote-two'>
+         <span>{this.props.item.itemTwo.value}</span>
+         <h3>{this.props.item.itemTwo.text}</h3>
+       </div>
+       </div>
+
      )
    }
  }
