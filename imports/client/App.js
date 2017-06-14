fa46d5f15c0503cc296b6cf7acc99384efb99ff4
rohscx <R_E__g_@Macintosh.local>
2017-06-14 13:34:16 -0400

    setup

--- imports/client/App.js
+++ imports/client/App.js
@@ -2,10 +2,15 @@ import React, {Component} from 'react';
 
 let hello = 'Rick Hunter';
 
+let headingClick = function () {
+  console.log('helo');
+}
+
+
 export default class App extends Component {
   render() {
     return (
-      <h1>Hello {hello}!</h1>
+      <h1 onClick={headingClick}>Hello {hello}!</h1>
     );
   }
 }
