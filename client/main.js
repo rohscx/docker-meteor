100e62a04319acf2a24e9306db6590b6b384d725
rohscx <R_E__g_@Macintosh.local>
2017-06-14 13:29:49 -0400

    setup

--- client/main.js
+++ client/main.js
@@ -3,6 +3,7 @@ import {Meteor} from 'meteor/meteor';
 import {render} from 'react-dom';
 
 import App from '../imports/client/App';
+
 Meteor.startup(() => {
   render(<App />, document.getElementById('render-target'));
 });
