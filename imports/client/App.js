3a5654c695561faee4a0496ff063ca05b6bce5d7
rohscx <R_E__g_@Macintosh.local>
2017-06-14 13:45:09 -0400

    setup

--- imports/client/App.js
+++ imports/client/App.js
@@ -1,19 +1,19 @@
 import React, {Component} from 'react';
 
-let hello = 'Rick Hunter';
-/*
-let headingClick = function () {
-  console.log('hello');
-}
-*/
-
 export default class App extends Component {
+  contructor() {
+    super();
+    this.state = {
+      count: 0
+    }
+  }
   headingClick() {
     console.log('hello');
   }
+
   render() {
     return (
-      <h1 onClick={this.headingClick}>Hello {hello}!</h1>
+      <h1 onClick={this.headingClick}>Hello {this.state.count}!</h1>
     );
   }
 }
