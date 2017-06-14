7dad418f6bb6395f096cf1e233ec0551887cb606
rohscx <R_E__g_@Macintosh.local>
2017-06-14 13:21:02 -0400

    setup

--- imports/client/App.js
+++ imports/client/App.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react';
 
+let hello = 'Rick';
+
 export default class App extends Component {
   render() {
     return (
-      <h1>Hello!</h1>
+      <h1>Hello {hello}!</h1>
     );
   }
 }
