40a5f10ff8d5a160254b191eb6a350120a4f1101
rohscx <R_E__g_@Macintosh.local>
2017-06-14 10:30:13 -0400

    setup

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,3 +1,13 @@
 import {Template} from 'meteor/templating';
 
 import './body.html';
+
+Template.body.helpers({
+  hello: 'Hi World'
+});
+
+Template.body.events({
+  'click .test': function(event) {
+    console.log('helo');
+  }
+});
