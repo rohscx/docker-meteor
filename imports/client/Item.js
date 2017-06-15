6ef158d8bd46341c2ac63a9b96e0ded1e04e6b3a
rohscx <R_E__g_@Macintosh.local>
2017-06-15 09:59:27 -0400

    setup

--- imports/client/Item.js
+++ imports/client/Item.js
@@ -8,7 +8,13 @@ import Items from '../api/Items'
        }
      })
    }
-
+   voteTwo() {
+     Items.update(this.props.item._id, {
+       $inc: {
+         'itemTwo.value': 1
+       }
+     })
+   }
 
    render() {
      return (
@@ -18,7 +24,7 @@ import Items from '../api/Items'
          <h3>{this.props.item.itemOne.text}</h3>
        </div>
        <span>VS</span>
-       <div className='vote-two'>
+       <div className='vote-two' onClick={this.voteTwo.bind(this)>
          <span>{this.props.item.itemTwo.value}</span>
          <h3>{this.props.item.itemTwo.text}</h3>
        </div>
