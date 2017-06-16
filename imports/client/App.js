688a4f49ac769fb779a118178711a518a1899c4f
rohscx <rohscx@gmail.com>
2017-06-16 09:18:02 -0400

    Update App.js

--- imports/client/App.js
+++ imports/client/App.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import {createContainer} from 'meteor/react-meteor-data'
 import {LoginButtons} from 'meteor/okgrow:accounts-ui-react'
 
-import (
+import { Accounts } from 'meteor/std:accounts-ui'
 
 import Item from './Item'
 
