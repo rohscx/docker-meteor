f959a1eea8762190f575753fb924ce46709571be
rohscx <R_E__g_@Macintosh.local>
2017-06-16 10:34:38 -0400

    setup

--- client/main.js
+++ client/main.js
@@ -2,8 +2,4 @@ import React, {Component} from 'react';
 import {Meteor} from 'meteor/meteor';
 import {render} from 'react-dom';
 
-import App from '../imports/client/App';
-
-Meteor.startup(() => {
-  render(<App />, document.getElementById('render-target'));
-});
+import '../imports/client/routes';
