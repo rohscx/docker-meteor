9216b08079a5eebfcd58c34cc1cf83b440a1022a
rohscx <R_E__g_@Macintosh.local>
2017-06-16 15:21:31 -0400

    setup

--- imports/server/accounts.js
+++ imports/server/accounts.js
@@ -1,4 +1,5 @@
 Accounts.onCreateUser((options, user) => {
   console.log(options, user);
+  user.hair = 'brown';
   return user;
 });
--- server/main.js
+++ server/main.js
@@ -4,7 +4,15 @@ import Items from '../imports/api/Items';
 
 import '../imports/server/accounts';
 
-Meteor.startup(() => {
+//publish user data in mini mongo
+Meteor.publish('currentUser', function() {
+  return Meteor.users.find({_id: this.userID}, {
+    fields: {
+      hair: 1
+    }
+  });
+});
 
+Meteor.startup(() => {
   // code to run on server at startup
 });
