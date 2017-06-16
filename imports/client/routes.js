bd438db9f835bd4af2433d231ddb5df13cb34ee0
rohscx <R_E__g_@Macintosh.local>
2017-06-16 11:28:35 -0400

    setup

--- imports/client/routes.js
+++ imports/client/routes.js
@@ -7,7 +7,7 @@ import {
 } from 'react-router';
 import { render } from 'react-dom';
 import App from './App';
-import MainLayout from './layouts/MainLayout';
+import MainLayout from '../layouts/MainLayout';
 
 Meteor.startup(() => {
   render(
