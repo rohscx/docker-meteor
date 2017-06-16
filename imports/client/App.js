9070f7a9b371aeac2c10ef7058289a023edb5a75
rohscx <rohscx@gmail.com>
2017-06-16 09:37:16 -0400

    Update App.js

--- imports/client/App.js
+++ imports/client/App.js
@@ -2,7 +2,6 @@ import React, {Component} from 'react';
 import {createContainer} from 'meteor/react-meteor-data'
 import {LoginButtons} from 'meteor/okgrow:accounts-ui-react'
 
-import { Accounts } from 'meteor/std:accounts-ui'
 
 import Item from './Item'
 
@@ -45,7 +44,6 @@ import Items from '../api/Items'
         <header>
           <h1>Level Up Voting</h1>
           <LoginButtons />
-          <Accounts.ui.LoginForm />
           <button onClick={this.showAll.bind(this)}>
             Show {this.props.showAll ? 'One': 'All'}
           </button>
