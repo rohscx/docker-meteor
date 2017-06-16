f9b708b88cd578f9fa95be4f0aad90c325670e64
rohscx <R_E__g_@Macintosh.local>
2017-06-16 15:11:49 -0400

    setup

--- client/main.js
+++ client/main.js
@@ -2,4 +2,3 @@ import React, {Component} from 'react';
 import {Meteor} from 'meteor/meteor';
 
 import '../imports/client/routes';
-import '../imports/server/accounts';
--- server/main.js
+++ server/main.js
@@ -2,6 +2,8 @@ import { Meteor } from 'meteor/meteor';
 
 import Items from '../imports/api/Items';
 
+import '../imports/server/accounts';
+
 Meteor.startup(() => {
 
   // code to run on server at startup
