1c65e0ba0642ccf1dd111232ae2d5b3d94cd8d7c
rohscx <R_E__g_@Macintosh.local>
2017-06-16 11:55:33 -0400

    setup

--- imports/client/layouts/MainLayout.js
+++ imports/client/layouts/MainLayout.js
@@ -5,8 +5,11 @@ import {LoginButtons} from 'meteor/okgrow:accounts-ui-react'
 const MainLayout = ({children}) =>
   <div className='main-layout'>
     <header>
-      <h1>Level Up Voting</h1>
+      <h1><a href='/'>Level Up Voting</a></h1>
       <LoginButtons />
+      <nav>
+        <a href='/about'>About</a>
+      </nav>
     </header>
     {children}
   </div>
