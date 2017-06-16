6979176c3675b7cd2a67ad927d31f7ea7d7db44e
rohscx <R_E__g_@Macintosh.local>
2017-06-16 18:10:41 -0400

    setup

--- imports/client/utilities/IsRole.js
+++ imports/client/utilities/IsRole.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const IsRole = ({role, children}) => {
-  if (Roles.userIsInRole(Meteor.userId(Meteor.userId), role)) {
+  if (Roles.userIsInRole(Meteor.userId(), role)) {
     return children;
   }
   return null;
