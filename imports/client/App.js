767d844d9ca4fe8d58caf78342eb7cdfccc0600d
rohscx <R_E__g_@Macintosh.local>
2017-06-16 15:47:36 -0400

    setup

--- imports/client/App.js
+++ imports/client/App.js
@@ -38,7 +38,7 @@ import Items from '../api/Items'
       return <div>Loading...</div>
     }
 
-    const showAll = Roles.userIsinRole(Meteor.userID(), 'admin') ? (
+    const showAll = Roles.userIsInRole(Meteor.userID(), 'admin') ? (
       <button onClick={this.showAll}>
         Show {this.props.showAll ? 'One': 'All'}
       </button>
