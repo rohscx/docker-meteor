9c9accf6ab3d59ff57f53e9f6eecb1034a703ce9
rohscx <rohscx@gmail.com>
2017-06-19 14:41:37 -0400

    Update MainLayout.js

--- imports/client/layouts/MainLayout.js
+++ imports/client/layouts/MainLayout.js
@@ -2,18 +2,15 @@ import React from 'react';
 import {LoginButtons} from 'meteor/okgrow:accounts-ui-react'
 import { Link } from 'react-router';
 
-
 // stateless functional component. Functuion returns what ever you like
 const MainLayout = ({children}) =>
   <div className='main-layout'>
     <header>
-      <h1><Link to='/'>FROSTYFACE</Link></h1>
+      <h1><Link to='/'>Level Up Voting</Link></h1>
       <LoginButtons />
       <nav>
-        <Link to='/about' style={{ textDecoration: 'none' }} activeStyle={{ color: '#eee' }}>About</Link>
+        <Link to='/about'>About</Link>
       </nav>
     </header>
     {children}
   </div>
-
-export default MainLayout;
