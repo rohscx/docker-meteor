ba8e7dd1ab6b95ab598372249e8bf70e27ba54ca
rohscx <rohscx@gmail.com>
2017-06-19 14:33:23 -0400

    Update MainLayout.js

--- imports/client/layouts/MainLayout.js
+++ imports/client/layouts/MainLayout.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router';
 
 // stateless functional component. Functuion returns what ever you like
 const MainLayout = ({children}) =>
-  <div className='main-layout'>
+  <div className='main-layout' style={{ textDecoration: 'none' }} activeStyle={{ color: '#eee' }}>
     <header>
       <h1><Link to='/'>FROSTYFACE</Link></h1>
       <LoginButtons />
