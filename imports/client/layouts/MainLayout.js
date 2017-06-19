9728723325d0982eda9055a7497c3decedc660bc
rohscx <rohscx@gmail.com>
2017-06-19 13:57:27 -0400

    Update MainLayout.js

--- imports/client/layouts/MainLayout.js
+++ imports/client/layouts/MainLayout.js
@@ -3,12 +3,11 @@ import {LoginButtons} from 'meteor/okgrow:accounts-ui-react'
 import { Link } from 'react-router';
 
 
-set style = "textDecoration: /'none/'";
 // stateless functional component. Functuion returns what ever you like
 const MainLayout = ({children}) =>
   <div className='main-layout'>
     <header>
-      <h1><Link to='/' style={style} activeStyle={{ color: '#eee' }}>FROSTYFACE</Link></h1>
+      <h1><Link to='/' style={{ textDecoration: 'none' }} activeStyle={{ color: '#eee' }}>FROSTYFACE</Link></h1>
       <LoginButtons />
       <nav>
         <Link to='/about' style={{ textDecoration: 'none' }} activeStyle={{ color: '#eee' }}>About</Link>
