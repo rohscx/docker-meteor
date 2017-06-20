be00e912e6a19e0baa18f744d34d74dff1e402b5
rohscx <rohscx@gmail.com>
2017-06-20 11:25:06 -0400

    Create Apic.js

---
+++ imports/api/Apic.js
@@ -0,0 +1,43 @@
+var http = require("https");
+
+function getJSON(options, cb){
+var req = http.request(options, function (res) {
+  var chunks = [];
+
+  res.on("data", function (chunk) {
+    chunks.push(chunk);
+  });
+
+  res.on("end", function () {
+    var body = Buffer.concat(chunks);
+    cb(null, result);
+    console.log(body.toString());
+  });
+  res.on('error', cb)
+});
+
+req.write(JSON.stringify({ username: 'devnetuser', password: 'Cisco123!' }));
+req
+.on('error', cb)
+.end();
+}
+
+var options = {
+  "method": "GET",
+  "hostname": "devnetapi.cisco.com",
+  "port": null,
+  "path": "/sandbox/apic_em/api/v1/host",
+  "headers": {
+    "content-type": "application/json",
+    "x-auth-token": "ST-15-QHrXeIL6CWTF1Xu6dSSQ-cas",
+    "cache-control": "no-cache",
+    "postman-token": "7e1ab28c-68e1-e3e3-bf2c-880eebc13136"
+  }
+};
+
+getJSON(options, function(err, result){
+  if(err){
+  return console.log('Error while trying to get REST result: ',err)
+  }
+  console.log(result);
+});
