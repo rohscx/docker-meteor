67e5c8aeb13b2302f579a72038783db151603e83
rohscx <R_E__g_@Macintosh.local>
2017-06-19 20:45:20 -0400

    setup

--- imports/client/layouts/MainLayout.js
+++ imports/client/layouts/MainLayout.js
@@ -1,43 +1,19 @@
 import React from 'react';
 import {LoginButtons} from 'meteor/okgrow:accounts-ui-react'
 import { Link } from 'react-router';
-import { MenuItem } from 'react-router';
 
 
 // stateless functional component. Functuion returns what ever you like
-
-
-  const MainLayout = (
-  <ButtonToolbar>
-    <Dropdown id="dropdown-custom-1">
-      <Dropdown.Toggle>
-        <Glyphicon glyph="star" />
-        Pow! Zoom!
-      </Dropdown.Toggle>
-      <Dropdown.Menu className="super-colors">
-        <MenuItem eventKey="1">Action</MenuItem>
-        <MenuItem eventKey="2">Another action</MenuItem>
-        <MenuItem eventKey="3" active>Active Item</MenuItem>
-        <MenuItem divider />
-        <MenuItem eventKey="4">Separated link</MenuItem>
-      </Dropdown.Menu>
-    </Dropdown>
-
-    <Dropdown id="dropdown-custom-2">
-      <Button bsStyle="info">
-        mix it up style-wise
-      </Button>
-      <Dropdown.Toggle bsStyle="success"/>
-      <Dropdown.Menu className="super-colors">
-        <MenuItem eventKey="1">Action</MenuItem>
-        <MenuItem eventKey="2">Another action</MenuItem>
-        <MenuItem eventKey="3" active>Active Item</MenuItem>
-        <MenuItem divider />
-        <MenuItem eventKey="4">Separated link</MenuItem>
-      </Dropdown.Menu>
-    </Dropdown>
-
-  </ButtonToolbar>
-);
+const MainLayout = ({children}) =>
+  <div className='main-layout'>
+    <header>
+      <h1><Link to='/' tyle={{ textDecoration: 'none' }}>FROST</Link></h1>
+      <LoginButtons/>
+      <nav>
+        <Link to='/about'>About</Link>
+      </nav>
+    </header>
+    {children}
+  </div>
 
 export default MainLayout;
