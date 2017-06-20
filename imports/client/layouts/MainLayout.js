38b3e6ad39ef771eeb04ebc7f90b63cef24c710c
rohscx <rohscx@gmail.com>
2017-06-20 09:39:00 -0400

    Update MainLayout.js

--- imports/client/layouts/MainLayout.js
+++ imports/client/layouts/MainLayout.js
@@ -3,11 +3,21 @@ import {LoginButtons} from 'meteor/okgrow:accounts-ui-react'
 import { Link } from 'react-router';
 
 
+// style object for nav bar
+const styleObj = {
+  color: '#eee', 
+  textDecoration: 'none'
+};
+// active style object for nav bar
+const activeStyleObj = {
+  color: '#eee'
+};
+
 // stateless functional component. Functuion returns what ever you like
 const MainLayout = ({children}) =>
   <div className='main-layout'>
     <header>
-      <h1><Link to='/' style={{color: '#eee', textDecoration: 'none'}} activeStyle={{color: '#eee'}}>FROST</Link></h1>
+      <h1><Link to='/' style={styleObj} activeStyle={activeStyleObj}>FROST</Link></h1>
       <LoginButtons/>
       <nav>
         <Link to='/about' style={{color: '#eee', textDecoration: 'none'}} activeStyle={{color: '#eee'}}>About</Link>
