6ae650615d4042598c69985cd1904fb684017a54
rohscx <rohscx@gmail.com>
2017-06-20 16:29:36 -0400

    Update AppApic.js

--- imports/client/AppApic.js
+++ imports/client/AppApic.js
@@ -6,8 +6,22 @@ import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import IsRole from './utilities/IsRole';
 
 import ApicTicket from '../api/ApicTicket';
+import { HTTP } from 'meteor/http';
 
-
+Meteor.http.post(
+    "https://devnetapi.cisco.com/sandbox/apic_em/api/v1/ticket"
+    params:
+        "USER": "sdk-three_api1.sdk.com"
+        "PWD": "QFZCWN5HZM8VBG7Q"
+        "SIGNATURE": "A-IzJhZZjhg29XQ2qnhapuwxIDzyAZQ92FRP5dqBzVesOkzbdUONzmOU"
+        "VERSION": "64.0"
+        "PAYMENTREQUEST_0_PAYMENTACTION": "Sale"
+        "PAYMENTREQUEST_0_AMT": "19.95"
+        "RETURNURL": "https://www.YourReturnURL.com"
+        "CANCELURL": "https://www.YourCancelURL.com"
+        "METHOD": "SetExpressCheckout"
+    (error, result) -> console.log(result)
+)
 
 
 @autobind
