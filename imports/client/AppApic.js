6efd86ca7c0591679fbbe14e69f2c8269f953cd5
rohscx <R_E__g_@Macintosh.local>
2017-06-20 17:34:40 -0400

    setup

--- imports/client/AppApic.js
+++ imports/client/AppApic.js
@@ -8,7 +8,7 @@ import IsRole from './utilities/IsRole';
 import ApicTicket from '../api/ApicTicket';
 import { HTTP } from 'meteor/http';
 
-const cats = function {
+let cats = function {
   var http = require("https");
 
 var options = {
