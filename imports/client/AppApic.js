9bab2d417a81be3ec13c58dfc62ae3752118b8cd
rohscx <rohscx@gmail.com>
2017-06-20 13:16:18 -0400

    Create AppApic.js

---
+++ imports/client/AppApic.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+import {createContainer} from 'meteor/react-meteor-data';
+import { autobind } from 'core-decorators';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+
+import IsRole from './utilities/IsRole';
+
+import ApicTicket from '../api/ApicTicket'
+
+@autobind
+ class AppApic extends Component {
+
+
+  render() {
+
+    if (!this.props.ready) {
+      return <div>Loading APIC...</div>
+    }
+
+    // inline conditional test. If true the conditional will be displayed
+    const test = false;
+    return (
+        <main>
+        <h1>Page loaded place holder... and son...</h1>
+        </main>
+    );
+  }
+}
+
+export default createContainer(({params}) => {
+  let userSub = Meteor.subscribe('currentUser');
+  let showAll = Session.get('showAll');
+  return {
+    showAll,
+    ready: userSub.ready(),
+  }
+}, AppApic);
