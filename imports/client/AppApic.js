f78a202aa9d2d54f2ccb4bf9635c8b6ab5eab18b
rohscx <rohscx@gmail.com>
2017-06-20 16:04:01 -0400

    Update AppApic.js

--- imports/client/AppApic.js
+++ imports/client/AppApic.js
@@ -7,7 +7,30 @@ import IsRole from './utilities/IsRole';
 
 import ApicTicket from '../api/ApicTicket'
 
-let cats = 'ASDdsafasdf';
+let cats = Meteor.methods({
+    score_app:  function(){
+        var test = HTTP.call("POST", "https://devnetapi.cisco.com/sandbox/apic_em/api/v1/ticket",
+            {   headers:  {     
+                        "Content-Type": "application/json"             
+        },
+        // This is where the problem is.  Have tried multiple syntax versions and tried using the `params`options for the HTTP call instead of `data`
+        data: {'username': 'devnetuser',
+               'password': 'Cisco123!'
+        }
+        },
+    function (error, result) {
+
+    // The syntax below should be if not an error, log the result (for testing etc, otherwise, log "http post error".  I may have incorrectly switched this around, but the original version I got from an online example had it the console.log statements in the reverse order.
+    if (!error) {
+        console.log(result);
+        return result;
+    } else{
+
+        console.log("http post error");
+    };
+    });
+    }
+});
 
 @autobind
  class AppApic extends Component {
