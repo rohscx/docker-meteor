1aee24b4df445b4ba6ae8eb4fa7a718513b3b678
rohscx <rohscx@gmail.com>
2017-06-20 13:18:12 -0400

    Update routes.js

--- imports/client/routes.js
+++ imports/client/routes.js
@@ -10,13 +10,14 @@ import App from './App';
 import MainLayout from './layouts/MainLayout';
 import About from './pages/About';
 import Apic from './pages/Apic';
+import AppApic from './AppApic';
 
 Meteor.startup(() => {
   render(
     <Router history={browserHistory}>
       <Route path="/" component={MainLayout} >
         <IndexRoute component={App} />
-        <Route path="/apic" component={Apic} />
+        <Route path="/apic" component={AppApic} />
         <Route path="/about" component={About} />
         <Route path="/:id" component={App} />
         
