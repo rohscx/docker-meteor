e0ce083a03b48403e06506138bfc53cca4a6c295
rohscx <rohscx@gmail.com>
2017-06-20 11:28:44 -0400

    Update Apic.js

--- imports/api/Apic.js
+++ imports/api/Apic.js
@@ -1,27 +1,5 @@
 var http = require("https");
 
-function getJSON(options, cb){
-var req = http.request(options, function (res) {
-  var chunks = [];
-
-  res.on("data", function (chunk) {
-    chunks.push(chunk);
-  });
-
-  res.on("end", function () {
-    var body = Buffer.concat(chunks);
-    cb(null, result);
-    console.log(body.toString());
-  });
-  res.on('error', cb)
-});
-
-req.write(JSON.stringify({ username: 'devnetuser', password: 'Cisco123!' }));
-req
-.on('error', cb)
-.end();
-}
-
 var options = {
   "method": "GET",
   "hostname": "devnetapi.cisco.com",
@@ -31,13 +9,22 @@ var options = {
     "content-type": "application/json",
     "x-auth-token": "ST-15-QHrXeIL6CWTF1Xu6dSSQ-cas",
     "cache-control": "no-cache",
-    "postman-token": "7e1ab28c-68e1-e3e3-bf2c-880eebc13136"
+    "postman-token": "90d322cc-4ccb-4e04-6191-da79e1d77e42"
   }
 };
 
-getJSON(options, function(err, result){
-  if(err){
-  return console.log('Error while trying to get REST result: ',err);
-  }
-  console.log(result);
+var req = http.request(options, function (res) {
+  var chunks = [];
+
+  res.on("data", function (chunk) {
+    chunks.push(chunk);
+  });
+
+  res.on("end", function () {
+    var body = Buffer.concat(chunks);
+    console.log(body.toString());
+  });
 });
+
+req.write(JSON.stringify({ username: 'devnetuser', password: 'Cisco123!' }));
+req.end();
