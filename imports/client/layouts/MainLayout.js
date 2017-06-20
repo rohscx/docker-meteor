55772f4577ee4233876c733d88f7683bec1ab6cf
rohscx <R_E__g_@Macintosh.local>
2017-06-19 20:55:44 -0400

    setup

--- imports/client/layouts/MainLayout.js
+++ imports/client/layouts/MainLayout.js
@@ -7,7 +7,7 @@ import { Link } from 'react-router';
 const MainLayout = ({children}) =>
   <div className='main-layout'>
     <header>
-      <h1><Link to='/' tyle={{ textDecoration: 'none' }}>FROST</Link></h1>
+      <h1><Link to='/' style={{color: 'white'}} activeStyle={{color: #333}}>FROST</Link></h1>
       <LoginButtons/>
       <nav>
         <Link to='/about'>About</Link>
