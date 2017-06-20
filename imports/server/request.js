a48403c0d3ab76ca7ca17f693341337536e63cda
rohscx <R_E__g_@Macintosh.local>
2017-06-20 17:54:49 -0400

    setup

--- imports/server/request.js
+++ imports/server/request.js
@@ -1,35 +1,29 @@
-Meteor.methods({
-  ok:function () {
-    var http = require("https");
+var http = require("https");
 
-    var options = {
-      "method": "POST",
-      "hostname": "devnetapi.cisco.com",
-      "port": null,
-      "path": "/sandbox/apic_em/api/v1/ticket",
-      "headers": {
-        "content-type": "application/json",
-        "cache-control": "no-cache",
-        "postman-token": "5210b0cf-3524-230d-f3b4-0a1d1dd9e258"
-      }
-    };
-
-    var req = http.request(options, function (res) {
-      var chunks = [];
-
-      res.on("data", function (chunk) {
-        chunks.push(chunk);
-      });
-
-      res.on("end", function () {
-        var body = Buffer.concat(chunks);
-        console.log(body.toString());
-      });
-    });
-
-    req.write(JSON.stringify({ username: 'devnetuser', password: 'Cisco123!' }));
-    req.end();
-    }
+var options = {
+  "method": "POST",
+  "hostname": "devnetapi.cisco.com",
+  "port": null,
+  "path": "/sandbox/apic_em/api/v1/ticket",
+  "headers": {
+    "content-type": "application/json",
+    "cache-control": "no-cache",
+    "postman-token": "25808d6f-9e3e-4430-13a7-f91e4d0d2689"
   }
+};
+
+var req = http.request(options, function (res) {
+  var chunks = [];
+
+  res.on("data", function (chunk) {
+    chunks.push(chunk);
+  });
+
+  res.on("end", function () {
+    var body = Buffer.concat(chunks);
+    console.log(body.toString());
+  });
 });
-console.log('lkfhdasl;kfjals;dfj;l');
+
+req.write(JSON.stringify({ username: 'devnetuser', password: 'Cisco123!' }));
+req.end();
