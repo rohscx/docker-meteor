db0840b9020a02725d219fd2599501f1b4d68e61
rohscx <R_E__g_@Macintosh.local>
2017-06-19 20:08:04 -0400

    setup

--- imports/client/layouts/MainLayout.js
+++ imports/client/layouts/MainLayout.js
@@ -5,12 +5,14 @@ import { Link } from 'react-router';
 // stateless functional component. Functuion returns what ever you like
 const MainLayout = ({children}) =>
   <div className='main-layout'>
+    <div>
       <ul>
         <li><a>Home</a></li>
         <li><a>News</a></li>
         <li><a>Contact</a></li>
         <li style="float:right"><a>About</a></li>
       </ul>
+    </div>
     {children}
   </div>
 
