80ff5055a6e3f628569d0d71a1a670a8e638e0f8
rohscx <R_E__g_@Macintosh.local>
2017-06-19 20:34:28 -0400

    setup

--- imports/client/layouts/MainLayout.js
+++ imports/client/layouts/MainLayout.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import {LoginButtons} from 'meteor/okgrow:accounts-ui-react'
 import { Link } from 'react-router';
+import { MenuItem } from 'react-router';
+MenuItem
 
 // stateless functional component. Functuion returns what ever you like
 const MainLayout = ({children}) =>
   <div className='main-layout'>
     <header>
-      <h1><Link to='/' tyle={{ textDecoration: 'none' }}>FROST</Link></h1>
+      <h1><Link to='/' tyle={{ textDecoration: 'none' }}>
+    <MenuItem style={{ paddingLeft: 13 }}>Team 1</MenuItem></Link></h1>
       <LoginButtons/>
       <nav>
         <Link to='/about'>About</Link>
