8fe2fb9d246c7558ebe4bc2137d741be322597bb
rohscx <rohscx@gmail.com>
2017-06-20 11:07:01 -0400

    Update Rest.js

--- imports/api/Rest.js
+++ imports/api/Rest.js
@@ -1,20 +1,29 @@
-var http = require('http');
+var http = require("https");
 
 var options = {
-host: 'api-sandbox.oanda.com',
-port: '80',
-path: '/v1/quote?instruments=USD_ZAR',
-method: 'GET'
+  "method": "POST",
+  "hostname": "devnetapi.cisco.com",
+  "port": null,
+  "path": "/sandbox/apic_em/api/v1/ticket",
+  "headers": {
+    "content-type": "application/json",
+    "cache-control": "no-cache",
+    "postman-token": "19088f21-a360-dc0b-d452-a2da93f86f0a"
+  }
 };
 
-http.request(options, function() {
-  var body= '';
-  
-  res.on('data', function(chunk) {
-    body+= chunk;
+var req = http.request(options, function (res) {
+  var chunks = [];
+
+  res.on("data", function (chunk) {
+    chunks.push(chunk);
   });
-  res.on('end', function(){
-    var price = JSON.parse(body);
-    console.log(price);
-  })
-}).end();
+
+  res.on("end", function () {
+    var body = Buffer.concat(chunks);
+    console.log(body.toString());
+  });
+});
+
+req.write(JSON.stringify({ username: 'devnetuser', password: 'Cisco123!' }));
+req.end();
