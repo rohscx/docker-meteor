b565b986b6732da1e935ab0ee5f162f6e95f940c
rohscx <rohscx@gmail.com>
2017-06-21 10:52:01 -0400

    Update AppApic.js

--- imports/client/AppApic.js
+++ imports/client/AppApic.js
@@ -28,7 +28,7 @@ const cats = Meteor.call('checkApic', (err, res) => {
     const test = false;
     return (
         <main>
-        <h1>Page loaded place holder... and son...</h1>
+        <h1>Page loaded place holder... and son...{JSON.stringify(cats, null, 2) }</h1>
         </main>
     );
   }
