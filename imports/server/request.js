4e3ed36ecf0db66062d4a12fdf60df993cdeccb8
rohscx <rohscx@gmail.com>
2017-06-21 09:51:50 -0400

    Update request.js

--- imports/server/request.js
+++ imports/server/request.js
@@ -8,6 +8,7 @@ const result = HTTP.call('POST', 'https://devnetapi.cisco.com/sandbox/apic_em/ap
         headers: { contentType: 'application/json' },
 	data: {username: 'devnetuser', password: 'Cisco123!'}
 });
+	    console.log(result);
       return true;
     } catch (e) {
       // Got a network error, timeout, or HTTP error in the 400 or 500 range.
