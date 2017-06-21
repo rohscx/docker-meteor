f57a523adacdc35b5b806ef3b9a3d030ab09a0a4
rohscx <rohscx@gmail.com>
2017-06-21 09:35:30 -0400

    Update request.js

--- imports/server/request.js
+++ imports/server/request.js
@@ -1,9 +1,5 @@
 import { HTTP } from 'meteor/http'
 
-
-
-
-
 Meteor.methods({
   checkApic() {
     this.unblock();
@@ -11,11 +7,6 @@ Meteor.methods({
 const result = HTTP.call('POST', 'https://devnetapi.cisco.com/sandbox/apic_em/api/v1/ticket', {
         headers: { contentType: 'application/json' },
 	data: {username: 'devnetuser', password: 'Cisco123!'}
-}, (error, result) => {
-  if (!error) {
-    console.log(error);
-  }
-	console.log(result);
 });
       return true;
     } catch (e) {
