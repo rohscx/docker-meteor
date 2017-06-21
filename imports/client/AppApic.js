678556dc2b4a0b3daf73e4a69a1dee627990020f
rohscx <rohscx@gmail.com>
2017-06-21 09:18:43 -0400

    Update AppApic.js

--- imports/client/AppApic.js
+++ imports/client/AppApic.js
@@ -14,6 +14,10 @@ import { HTTP } from 'meteor/http';
 const result = HTTP.call('POST', 'https://devnetapi.cisco.com/sandbox/apic_em/api/v1/ticket', {
         headers: { contentType: 'application/json' },
 	data: {username: 'devnetuser', password: 'Cisco123!'}
+}, (error, result) => {
+  if (!error) {
+    console.log(error);
+  }
 });
 
 @autobind
