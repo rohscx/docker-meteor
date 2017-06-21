6577cf997ca4529b959e5d84d57499a4b86ea682
rohscx <rohscx@gmail.com>
2017-06-21 10:00:59 -0400

    Update request.js

--- imports/server/request.js
+++ imports/server/request.js
@@ -1,18 +1,17 @@
 import { HTTP } from 'meteor/http'
 
+import { HTTP } from 'meteor/http'
+
 Meteor.methods({
   checkApic() {
     this.unblock();
     try {
-      const result = HTTP.call('POST', 'http://api.twitter.com/xyz', {
-  data: { some: 'json', stuff: 1 }
-}, (error, result) => {
-  if (!error) {
-    Session.set('twizzled', true);
-  }
+const result = HTTP.call('POST', 'https://devnetapi.cisco.com/sandbox/apic_em/api/v1/ticket', {
+        headers: { contentType: 'application/json' },
+	data: {username: 'devnetuser', password: 'Cisco123!'}
 });
       console.log(result);
-      return true;
+      return result;
     } catch (e) {
       // Got a network error, timeout, or HTTP error in the 400 or 500 range.
       console.log(result);
