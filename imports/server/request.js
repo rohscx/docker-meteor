dd9683ad212747b7417415710693fe7c042e6c72
rohscx <rohscx@gmail.com>
2017-06-21 09:55:43 -0400

    Update request.js

--- imports/server/request.js
+++ imports/server/request.js
@@ -7,8 +7,12 @@ Meteor.methods({
 const result = HTTP.call('POST', 'https://devnetapi.cisco.com/sandbox/apic_em/api/v1/ticket', {
         headers: { contentType: 'application/json' },
 	data: {username: 'devnetuser', password: 'Cisco123!'}
+}, (error, result) => {
+  if (!error) {
+    console.log(error);
+  }
+	console.log(result);
 });
-	    console.log(result);
       return true;
     } catch (e) {
       // Got a network error, timeout, or HTTP error in the 400 or 500 range.
