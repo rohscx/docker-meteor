8ae017296c1c2dd20662aac618a459c40e916f23
rohscx <rohscx@gmail.com>
2017-06-22 15:02:42 -0400

    Update AppApic.js

--- imports/client/AppApic.js
+++ imports/client/AppApic.js
@@ -3,15 +3,28 @@ import {createContainer} from 'meteor/react-meteor-data';
 import { autobind } from 'core-decorators';
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
-import restRequest from '../api/Apic'
+import restRequest from '../api/Apic';
 
 import IsRole from './utilities/IsRole';
 import { Session } from 'meteor/session';
 import { ReactiveVar } from 'meteor/reactive-var';
 import { Template } from 'meteor/templating';
 
-import ItemApic from './ItemApic'
+import ItemApic from './ItemApic';
 
+/*
+// creates NEW Object for the rest request
+let object = new restRequest('GET', 'https://devnetapi.cisco.com/sandbox/apic_em/api/v1/host', {
+        headers: { 'conten-type': 'application/json'}
+});
+// console.log(object); // debug
+
+// Requests NEW ticket from APIC
+object.makeTicket(); 
+
+// console.log(object); // debug
+
+*/
 
 @autobind
  class AppApic extends Component {
