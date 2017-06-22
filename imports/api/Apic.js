e5177d9b0e6e5c074a2fac7e959b1a0c19bdb2e8
rohscx <rohscx@gmail.com>
2017-06-22 13:53:34 -0400

    Update Apic.js

--- imports/api/Apic.js
+++ imports/api/Apic.js
@@ -28,6 +28,7 @@ restRequest.prototype.makeTicket = function() {
     this.ticket = res.data.response.serviceTicket;
     this.options.headers['x-auth-token'] = res.data.response.serviceTicket;
     Session.set("data", res.data.response.serviceTicket);
+    this.useTicket();
   }
 })};
 
