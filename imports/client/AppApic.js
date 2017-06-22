8a88e9b14ed3d4685d8b54c4f58c184742d5cedc
rohscx <rohscx@gmail.com>
2017-06-22 15:01:33 -0400

    Update AppApic.js

--- imports/client/AppApic.js
+++ imports/client/AppApic.js
@@ -12,19 +12,7 @@ import { Template } from 'meteor/templating';
 
 import ItemApic from './ItemApic'
 
-/*
-// creates NEW Object for the rest request
-let object = new restRequest('GET', 'https://devnetapi.cisco.com/sandbox/apic_em/api/v1/host', {
-        headers: { 'conten-type': 'application/json'}
-});
-// console.log(object); // debug
 
-// Requests NEW ticket from APIC
-object.makeTicket(); 
-
-// console.log(object); // debug
-
-*/
 @autobind
  class AppApic extends Component {
   render() {
