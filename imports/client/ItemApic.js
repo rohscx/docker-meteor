6cddea046e01eb0071594bbb668db3680020986c
rohscx <R_E__g_@Macintosh.local>
2017-06-22 19:29:40 -0400

    setup

--- imports/client/ItemApic.js
+++ imports/client/ItemApic.js
@@ -2,19 +2,11 @@ import React, {Component} from 'react';
 
 import restRequest from '../api/Apic';
 
-let object = new restRequest('GET', 'https://devnetapi.cisco.com/sandbox/apic_em/api/v1/host', {
-        headers: { 'conten-type': 'application/json'}
-});
-// console.log(object); // debug
-
-// Requests NEW ticket from APIC
-object.makeTicket(); 
-
-console.log(object); // debug
 
 
 
-export default class ItemApic extends Component {   
+
+export default class ItemApic extends Component {
    render() {
      return (
        <div className='ItemApic'>
