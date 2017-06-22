eea61cab5d0463d9d2e7655f7d1caa548a3cc67d
rohscx <rohscx@gmail.com>
2017-06-22 15:21:02 -0400

    Update ItemApic.js

--- imports/client/ItemApic.js
+++ imports/client/ItemApic.js
@@ -4,7 +4,7 @@ import Apic from '../api/Apic'
  export default class ItemApic extends Component {
          
 // creates NEW Object for the rest request
-let object = new restRequest('GET', 'https://devnetapi.cisco.com/sandbox/apic_em/api/v1/host', {
+object = new restRequest('GET', 'https://devnetapi.cisco.com/sandbox/apic_em/api/v1/host', {
         headers: { 'conten-type': 'application/json'}
 });
 // console.log(object); // debug
