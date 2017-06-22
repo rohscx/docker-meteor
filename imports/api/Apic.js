0eeca2a85196b12b480059d000cd02c39ed8a8b9
rohscx <rohscx@gmail.com>
2017-06-22 14:12:43 -0400

    Update Apic.js

--- imports/api/Apic.js
+++ imports/api/Apic.js
@@ -13,18 +13,17 @@ function restRequest(type,url,options) {
   this.optionsTicket = {
         headers: { 'content-type': 'application/json' },
 	data: {username: 'devnetuser', password: 'Cisco123!'}
-};
-  this.ticket = '';
-
+   };
 }
 
+// Method REQUEST a ticket from APIC
 restRequest.prototype.makeTicket = function() {
   Meteor.call('checkApic', this.typeTicket, this.urlTicket, this.optionsTicket, (err, res) => {
   if (err) {
     alert(err);
   } else {
     // success!
-    console.log(res.data.response.serviceTicket);
+    // console.log(res.data.response.serviceTicket);	// debug
     this.ticket = res.data.response.serviceTicket;
     this.options.headers['x-auth-token'] = res.data.response.serviceTicket;
     Session.set("data", res.data.response.serviceTicket);
@@ -32,13 +31,15 @@ restRequest.prototype.makeTicket = function() {
   }
 })};
 
+
+// Method USE the ticket from APIC
 restRequest.prototype.useTicket = function() {
   Meteor.call('checkApic', this.type, this.url, this.options, (err, res) => {
   if (err) {
     alert(err);
   } else {
     // success!
-    console.log(res);
+    // console.log(res); // debug
     this.response = res;
   }
 })};
