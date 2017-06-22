1deba289479fce303237186a448e0f497966c4d1
rohscx <rohscx@gmail.com>
2017-06-22 15:41:59 -0400

    Update ItemApic.js

--- imports/client/ItemApic.js
+++ imports/client/ItemApic.js
@@ -16,7 +16,7 @@ export default class ItemApic extends Component {
          
    render() {
      return (
-       <div className='apic'>
+       <div className='ItemApic'>
          <div className='apic-list' onClick={console.log('List Clicked')}>
          <span>CATS{this.props.object.statusCode.value}</span>
          <h3>DOGS{this.props.object.statusCode.text}</h3>
