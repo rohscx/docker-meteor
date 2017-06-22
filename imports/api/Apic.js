04bd41a21be3273827196c6533a3b1b0c0212bad
rohscx <rohscx@gmail.com>
2017-06-22 10:40:06 -0400

    Update Apic.js

--- imports/api/Apic.js
+++ imports/api/Apic.js
@@ -2,14 +2,14 @@ import { Session } from 'meteor/session';
 import { ReactiveVar } from 'meteor/reactive-var';
 import { Template } from 'meteor/templating';
 
-const type = 'POST';
-const url = 'https://devnetapi.cisco.com/sandbox/apic_em/api/v1/ticket';
-const options = {
+const typeTicket = 'POST';
+const urlTicket = 'https://devnetapi.cisco.com/sandbox/apic_em/api/v1/ticket';
+const optionsTicket = {
         headers: { contentType: 'application/json' },
 	data: {username: 'devnetuser', password: 'Cisco123!'}
 };
 
-const restRequest = Meteor.call('checkApic', type, url, options, (err, res) => {
+const restRequest = Meteor.call('checkApic', typeTicket, urlTicket, optionsTicket, (err, res) => {
   if (err) {
     alert(err);
   } else {
