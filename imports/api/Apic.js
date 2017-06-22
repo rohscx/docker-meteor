b36c6133b6c165d949a984e9e40c28a92d4d7843
rohscx <R_E__g_@Macintosh.local>
2017-06-22 10:47:23 -0400

    setup

--- imports/api/Apic.js
+++ imports/api/Apic.js
@@ -9,13 +9,29 @@ const optionsTicket = {
 	data: {username: 'devnetuser', password: 'Cisco123!'}
 };
 
+const typeApic = 'POST';
+const urlApic = 'https://devnetapi.cisco.com/sandbox/apic_em/api/v1/ticket';
+const optionsApic = {
+        headers: { contentType: 'application/json' },
+	data: {username: 'devnetuser', password: 'Cisco123!'}
+};
+
 const restRequest = Meteor.call('checkApic', typeTicket, urlTicket, optionsTicket, (err, res) => {
   if (err) {
     alert(err);
   } else {
     // success!
     Session.set("data", res.data.response.serviceTicket);
-    return Session.get("data");
+    Meteor.call('checkApic', typeApic, urlApic, optionsApic, (err, res) => {
+      if (err) {
+        alert(err);
+      } else {
+        // success!
+        Session.set("data", res.data.response.serviceTicket);
+        return Session.get("data");
+      }
+    });
+    //return Session.get("data");
   }
 });
 
