5df99286c11110e5c02693bef5ab95d1f9c78def
rohscx <rohscx@gmail.com>
2017-06-22 12:33:51 -0400

    Update Apic.js

--- imports/api/Apic.js
+++ imports/api/Apic.js
@@ -2,12 +2,35 @@ import { Session } from 'meteor/session';
 import { ReactiveVar } from 'meteor/reactive-var';
 import { Template } from 'meteor/templating';
 
-const typeTicket = 'POST';
-const urlTicket = 'https://devnetapi.cisco.com/sandbox/apic_em/api/v1/ticket';
-const optionsTicket = {
+// Constructor
+function restRequest(type,url,options) {
+  // always initialize all instance properties
+  this.type = type;
+  this.url = url;
+  this.options = options;
+  this.typeTicket = 'POST';
+  this.urlTicket = 'https://devnetapi.cisco.com/sandbox/apic_em/api/v1/ticket';
+  this.optionsTicket = {
         headers: { 'content-type': 'application/json' },
 	data: {username: 'devnetuser', password: 'Cisco123!'}
 };
+  this.ticket = '';
+
+}
+
+restRequest.prototype.makeTicket = function() {
+  Meteor.call('checkApic', type, url, options, (err, res) => {
+  if (err) {
+    alert(err);
+  } else {
+    // success!
+    console.log(res.data.response.serviceTicket);
+    this.ticket = res.data.response.serviceTicket;
+    Session.set("data", res.data.response.serviceTicket);
+  }
+});
+
+
 
 const typeApic = 'GET';
 const urlApic = 'https://devnetapi.cisco.com/sandbox/apic_em/api/v1/host';
@@ -15,26 +38,7 @@ const optionsApic = {
         headers: { 'conten-type': 'application/json',
 		  'x-auth-token': Session.get("data"),
 		 }
-};
 
-const restRequest = Meteor.call('checkApic', typeTicket, urlTicket, optionsTicket, (err, res) => {
-  if (err) {
-    alert(err);
-  } else {
-    // success!
-    Session.set("data", res.data.response.serviceTicket);
-    Meteor.call('checkApic', typeApic, urlApic, optionsApic, (err, res) => {
-      if (err) {
-        alert(err);
-      } else {
-        // success!
-        Session.set("data1", res.content);
-        return Session.get("data");
-      }
-    });
-    //return Session.get("data");
-  }
-});
 
 
 export default restRequest;
