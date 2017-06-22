1f6a79a03755ba89f364a15d1abccbfa3b43c441
rohscx <rohscx@gmail.com>
2017-06-22 16:49:58 -0400

    Update AppApic.js

--- imports/client/AppApic.js
+++ imports/client/AppApic.js
@@ -31,7 +31,7 @@ import ItemApic from './ItemApic';
     );
   }
 }
-console.log(Component());
+console.log(this.props);
 export default createContainer(({params}) => {
   let userSub = Meteor.subscribe('currentUser');
   let showAll = Session.get('showAll');
