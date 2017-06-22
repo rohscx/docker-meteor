b6335a6041d4aceff060ea46162f7727441c6b0d
rohscx <rohscx@gmail.com>
2017-06-22 07:43:15 -0400

    Update AppApic.js

--- imports/client/AppApic.js
+++ imports/client/AppApic.js
@@ -14,7 +14,7 @@ const cats = Meteor.call('checkApic', (err, res) => {
     alert(err);
   } else {
     // success!
-    Session.set("data", res.data)
+    Session.set("data", res.data.response)
   }
 });
 
