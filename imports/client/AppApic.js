386c815e3d064f529f4c098b172a7066ec9a8f57
rohscx <R_E__g_@Macintosh.local>
2017-06-22 19:24:54 -0400

    setup

--- imports/client/AppApic.js
+++ imports/client/AppApic.js
@@ -13,10 +13,39 @@ import { Template } from 'meteor/templating';
 import ItemApic from './ItemApic';
 
 
+
+
+
+
+
 @autobind
  class AppApic extends Component {
+
+getItems() {
+  let object = new restRequest('GET', 'https://devnetapi.cisco.com/sandbox/apic_em/api/v1/host', {
+          headers: { 'conten-type': 'application/json'}
+  });
+  // console.log(object); // debug
+
+  // Requests NEW ticket from APIC
+  object.makeTicket();
+
+  console.log(object); // debug
+}
+
+showAll() {
+  if(this.props.showAll) {
+    Session.set('showAll', false);
+  } else {
+    Session.set('showAll', true);
+  }
+
+}
+
+
+
+
   render() {
-     console.log(this);
     if (!this.props.ready) {
       return <div>Loading APIC...</div>
     }
@@ -25,9 +54,24 @@ import ItemApic from './ItemApic';
     const test = false;
     return (
         <main>
+          <IsRole role={['admin']} {... this.props}>
+            <button onClick={this.showAll}>
+              Show {this.props.showAll ? 'One': 'All'}
+            </button>
+          </IsRole>
         <h1>Page loaded place holder... and son...</h1>
             <p>Current Ticket:  {Session.get("apicTicket")}</p>
              <ItemApic/>
+             <ReactCSSTransitionGroup
+               transitionName='item'
+               transitionEnterTimeout={600}
+               transitionLeaveTimeout={600}
+               transitionAppear={true}
+               transistionAppearTimeout={600}>
+             {this.props.items.map((item) => {
+               return <Item item={item} key={item._id}/>
+             })}
+           </ReactCSSTransitionGroup>
         </main>
     );
   }
