e402a05b3f2e8252be006980e441e5fc5f44b620
rohscx <rohscx@gmail.com>
2017-06-22 07:27:52 -0400

    Update AppApic.js

--- imports/client/AppApic.js
+++ imports/client/AppApic.js
@@ -31,7 +31,7 @@ const cats = Meteor.call('checkApic', (err, res) => {
     const test = false;
     return (
         <main>
-        <h1>Page loaded place holder... and son...{Session.get("data")}</h1>
+        <h1>Page loaded place holder... and son...{Session.get("data.statusCode")}</h1>
        
         </main>
     );
