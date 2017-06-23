1e3b48b769320d858f2d9fd45a90231a45b5f0a4
rohscx <R_E__g_@Macintosh.local>
2017-06-23 18:33:47 -0400

    setup

--- imports/client/ItemApic.js
+++ imports/client/ItemApic.js
@@ -8,12 +8,7 @@ export default class ItemApic extends Component {
    render() {
      return (
        <div className='item'>
-         <div className='vote-one' onClick={this.voteOne.bind(this)}>
-         <span>{this.props.item.itemOne.value}</span>
-         <h3>{this.props.item.itemOne.text}</h3>
-       </div>
-       <div className='ItemApic'>
-         <div className='apic-list' onClick={console.log('List Clicked')}>
+         <div className='vote-one' onClick={console.log('List Clicked')}>
          <span>CATS</span>
          <h3>DOGS=</h3>
        </div>
