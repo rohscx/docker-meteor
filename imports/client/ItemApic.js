278c566074ef9bbdadaac988a96354a839fc8980
rohscx <R_E__g_@Macintosh.local>
2017-06-23 15:11:06 -0400

    setup

--- imports/client/ItemApic.js
+++ imports/client/ItemApic.js
@@ -6,6 +6,7 @@ import { Session } from 'meteor/session';
 
 
 let da = Session.get("apicResponse");
+let blah = JSON.parse(JSON.stringify(da))
 
 
 export default class ItemApic extends Component {
@@ -14,7 +15,7 @@ export default class ItemApic extends Component {
        <div className='ItemApic'>
          <div className='apic-list' onClick={console.log('List Clicked')}>
          <span>CATS</span>
-         <h3>DOGS={JSON.parse(JSON.stringify(da)}</h3>
+         <h3>DOGS={blah}</h3>
        </div>
        </div>
      )
