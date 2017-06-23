3c705b17c08b0071ce91c6898b5fc6ffed315b44
rohscx <R_E__g_@Macintosh.local>
2017-06-23 18:38:06 -0400

    setup

--- imports/client/ItemApic.js
+++ imports/client/ItemApic.js
@@ -5,6 +5,25 @@ import restRequest from '../api/Apic';
 import { Session } from 'meteor/session';
 
 export default class ItemApic extends Component {
+  getItems() {
+
+    let object = new restRequest('GET', 'https://devnetapi.cisco.com/sandbox/apic_em/api/v1/host', {
+            headers: { 'conten-type': 'application/json'}
+    });
+    // console.log(object); // debug
+
+    // Requests NEW ticket from APIC
+    object.makeTicket();
+
+    // console.log(object); // debug
+    console.log(this); // debug
+
+      return (
+        <div className="cssClass">
+          hello
+        </div>
+      );
+    }
    render() {
      return (
        <div className='item'>
