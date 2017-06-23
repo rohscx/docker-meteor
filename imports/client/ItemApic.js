502a6d0352506963284000c1ab46da7351b4fb7a
rohscx <R_E__g_@Macintosh.local>
2017-06-23 19:26:26 -0400

    setup

--- imports/client/ItemApic.js
+++ imports/client/ItemApic.js
@@ -5,65 +5,12 @@ import restRequest from '../api/Apic';
 import { Session } from 'meteor/session';
 
 export default class ItemApic extends Component {
-  getItems() {
-
-    let object = new restRequest('GET', 'https://devnetapi.cisco.com/sandbox/apic_em/api/v1/host', {
-            headers: { 'conten-type': 'application/json'}
-    });
-    // console.log(object); // debug
-
-    // Requests NEW ticket from APIC
-    object.makeTicket();
-
-    // console.log(object); // debug
-    console.log(this); // debug
-
-      return (
-        <div className="cssClass">
-          hello
-        </div>
-      );
-    }
-
-    Comment(props) {
-      return (
-        <div className="Comment">
-          <div className="UserInfo">
-            <img className="Avatar"
-                 src={props.author.avatarUrl}
-                 alt={props.author.name} />
-            <div className="UserInfo-name">
-              {props.author.name}
-            </div>
-          </div>
-          <div className="Comment-text">
-            {props.text}
-          </div>
-          <div className="Comment-date">
-            {formatDate(props.date)}
-          </div>
-        </div>
-      );
-    }
-
-    comment = {
-  date: new Date(),
-  text: 'I hope you enjoy learning React!',
-  author: {
-    name: 'Hello Kitty',
-    avatarUrl: 'http://placekitten.com/g/64/64'
-  }
-};
 
    render() {
      return (
        <div className='item'>
          <div className='vote-one' onClick={this.getItems}>
          <span>CATS</span>
-         <Comment
-           date={this.comment.date}
-           text={this.comment.text}
-           author={this.comment.author} />
          <h3>DOGS=</h3>
        </div>
        </div>
