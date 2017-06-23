0454206547fff7597dfb5315094697ae993bb56c
rohscx <R_E__g_@Macintosh.local>
2017-06-23 09:36:50 -0400

    setup

--- imports/client/AppApic.js
+++ imports/client/AppApic.js
@@ -23,14 +23,13 @@ import ItemApic from './ItemApic';
 
    getItems() {
 
-     let object = new restRequest('GET', 'https://devnetapi.cisco.com/sandbox/apic_em/api/v1/host', {
+     let this.props.object = new restRequest('GET', 'https://devnetapi.cisco.com/sandbox/apic_em/api/v1/host', {
              headers: { 'conten-type': 'application/json'}
      });
      // console.log(object); // debug
 
      // Requests NEW ticket from APIC
      object.makeTicket();
-     const this.props.test = object;
 
      console.log(this); // debug
    }
