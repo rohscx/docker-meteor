40b5d57cccfceed44b1ca086569e4199d120e031
rohscx <R_E__g_@Macintosh.local>
2017-06-23 19:07:45 -0400

    setup

--- imports/client/ItemApic.js
+++ imports/client/ItemApic.js
@@ -24,6 +24,28 @@ export default class ItemApic extends Component {
         </div>
       );
     }
+
+    function Comment(props) {
+      return (
+        <div className="Comment">
+          <div className="UserInfo">
+            <img className="Avatar"
+                 src={props.author.avatarUrl}
+                 alt={props.author.name} />
+            <div className="UserInfo-name">
+              {props.author.name}
+            </div>
+          </div>
+          <div className="Comment-text">
+            {props.text}
+          </div>
+          <div className="Comment-date">
+            {formatDate(props.date)}
+          </div>
+        </div>
+      );
+    }
+
    render() {
      return (
        <div className='item'>
