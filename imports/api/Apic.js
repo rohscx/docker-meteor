a5b088b03e74af3c42bd06addc7355467e68781d
rohscx <R_E__g_@Macintosh.local>
2017-06-24 10:19:32 -0400

    setup

--- imports/api/Apic.js
+++ imports/api/Apic.js
@@ -43,6 +43,7 @@ restRequest.prototype.useTicket = function() {
     console.log(JSON.parse(JSON.stringify(res))); // debug
     this.response = res;
     Session.set("apicResponse", res.data.response);
+    return res.data.response;
   }
 })};
 
