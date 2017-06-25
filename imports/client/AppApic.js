5f49e8f3f312a46818e6e1127f14cac7f3ee6c8f
rohscx <R_E__g_@Macintosh.local>
2017-06-25 11:49:30 -0700

    setup

--- imports/client/AppApic.js
+++ imports/client/AppApic.js
@@ -79,7 +79,7 @@ import ItemApic from './ItemApic';
     }
     console.log(this);
     if(!this.state.render) {
-      //this.setState.({ticket: "LoaDing..."});
+      this.setState.({ticket: "LoaDing..."});
       console.log('NOT SET TEST');
     }
     return (
