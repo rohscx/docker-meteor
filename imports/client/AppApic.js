526ed593b2bf656b70bec79bfce3d8dc53b2562b
rohscx <R_E__g_@Macintosh.local>
2017-06-25 09:24:54 -0700

    setup

--- imports/client/AppApic.js
+++ imports/client/AppApic.js
@@ -47,7 +47,7 @@ import { Tracker } from 'meteor/tracker';
    //this.setState({ticket: Session.get("apicTicket")});
    let count = new ReactiveVar(Session.get("apicTicket"));
 this.setState({ticket: 'WOWOWWER'});
-cout.set('CARP')
+count.set('CARP')
    var doSomething = function() {
 // Do something when the session value changes
 console.log(Session.get("apicTicket"));
