84e033a99d9e82d3320cca2b0419d970be3ea5bf
rohscx <R_E__g_@Macintosh.local>
2017-06-26 16:02:42 -0700

    setup

--- imports/client/Apic.js
+++ imports/client/Apic.js
@@ -39,7 +39,7 @@ makeRequest() {
       console.log(this.ticket);
       console.log('TICKET ABOVE');
       console.log(this);
-      this.addToDB('cats');
+      this.addToDB(this.ticket);
     }
   })};
 
@@ -60,7 +60,7 @@ makeRequest() {
   })};
 
   restRequest.prototype.addToDB = function() {
-    Meteor.call('insertNewApic', (err, res) => {
+    Meteor.call('insertNewApic', input (err, res) => {
     if (err) {
       alert(err);
     } else {
