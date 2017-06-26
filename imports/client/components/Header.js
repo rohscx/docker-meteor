3e9468f60dc72ff57a5703ba975de387b2f1879a
rohscx <R_E__g_@Macintosh.local>
2017-06-26 09:35:53 -0700

    setup

--- imports/client/components/Header.js
+++ imports/client/components/Header.js
@@ -10,7 +10,7 @@ export default class Header extends Component {
     console.log(this);
       return(
         <div>
-            <Title />
+            <Title {... this.props}/>
         </div>
       )
   }
