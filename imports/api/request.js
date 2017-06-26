42c5eca0f4fd0039e2834e14b5ac1b595aa6f3e4
rohscx <R_E__g_@Macintosh.local>
2017-06-26 15:44:54 -0700

    setup

--- imports/api/request.js
+++ imports/api/request.js
@@ -12,17 +12,69 @@ const ItemApicSchema = new SimpleSchema ({
   value: SimpleSchema.Integer
 });
 
-Meteor.methods({
-  checkApic(type, url, options) {
-    this.unblock();
-    try {
-const result = HTTP.call(type, url, options);
-      // console.log(result); // debug
-      return result;
-    } catch (e) {
-      // Got a network error, timeout, or HTTP error in the 400 or 500 range.
-	    console.log(e) // debug
-      return e;
-    }
+const ItemsApcicSchema = new SimpleSchema ({
+  itemOne: ItemApicSchema,
+  lastUpdated : {
+    type: Date,
+    optional: true
   }
 });
+
+ItemsApic.attachSchema(ItemsApicSchema);
+
+if (Meteor.isServer) {
+
+  Meteor.publish('allApicItems', function() {
+    return ItemsApic.find({}, {
+      // limits the number of return json items from DB
+      limit: 50,
+      // value 1 (OLDEST) or -1 (NEWEST) determines directions of lastUpdated
+      sort: {lastUpdated: 1}
+    });
+  });
+
+
+  Meteor.methods({
+    checkApic(type, url, options) {
+      this.unblock();
+      try {
+  const result = HTTP.call(type, url, options);
+        // console.log(result); // debug
+        return result;
+      } catch (e) {
+        // Got a network error, timeout, or HTTP error in the 400 or 500 range.
+        console.log(e) // debug
+        return e;
+      }
+    },
+    insertNewApic(itemOne) {
+      ItemsApic.insert({
+          itemOne: {
+            text: itemOne,
+            value: 0,
+          }
+        });
+        Roles.addUsersToRoles(Meteor.userId(), 'sumitter')
+    },
+    voteOnItem(item, position) {
+      check(item, Object);
+      let lastUpdated = new Date();
+      if(Meteor.userId()) {
+        if(position == 'itemOne') {
+          ItemsApic.update(item._id, {
+            $inc: {
+              'itemOne.value': 1
+            },
+            $set: {
+              lastUpdated
+            }
+          })
+        }
+        Roles.addUsersToRoles(Meteor.userId(), 'voter')
+      }
+    }
+  });
+}
+
+
+export default ItemsApic;
