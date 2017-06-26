3f742596d613af57affee41a16d455e20deffcb2
rohscx <R_E__g_@Macintosh.local>
2017-06-26 11:36:44 -0700

    setup

--- imports/client/Apic.js
+++ imports/client/Apic.js
@@ -36,7 +36,7 @@ makeRequest() {
       Session.set("apicTicket", res.data.response.serviceTicket);
       console.log(this.ticket);
       console.log('TICKET ABOVE');
-      this.props.changeTicket();
+      return this.props.changeTicket();
     }
   })};
 
