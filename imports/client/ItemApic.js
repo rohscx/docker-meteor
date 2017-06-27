aaefd2dd0b548995458ecca8cb7f2bb801e2024b
rohscx <R_E__g_@Macintosh.local>
2017-06-27 13:53:41 -0700

    setup

--- imports/client/ItemApic.js
+++ imports/client/ItemApic.js
@@ -35,9 +35,7 @@ export default class ItemApic extends Component {
          <span>{this.props.item.apicData.dataObj.response["0"].hostType}</span>
          <h3>{Object.keys(this.props.item.apicData.dataObj.response["0"]).map(function(key, index) {
            //console.log(key);
-           let name = key;
-           console.log(this.props.item.apicData.dataObj.response["0"]["key"]);
-           return <li>{name} </li>
+           return <li value={key}>{this.props.item.apicData.dataObj.response["0"][key]} </li>
          })};
         </h3>
        </div>
