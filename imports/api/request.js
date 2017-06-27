78d006b3547f09f6473f38e583ed85e5495c1713
rohscx <R_E__g_@Macintosh.local>
2017-06-27 05:47:23 -0700

    setup

--- imports/api/request.js
+++ imports/api/request.js
@@ -52,7 +52,7 @@ if (Meteor.isServer) {
       ItemsApic.insert({
           itemOne: {
             text: itemOne,
-            dataObj: dataObj,
+            dataObj: {dataObj},
             value: 0,
           }
         });
