9e2ba7f50d06eb96874395241732d6aedf40c874
rohscx <R_E__g_@Macintosh.local>
2017-06-27 05:33:35 -0700

    setup

--- imports/client/Apic.js
+++ imports/client/Apic.js
@@ -50,9 +50,9 @@ makeRequest() {
       // success!
       console.log(res); // debug
       console.log(JSON.parse(JSON.stringify(res))); // debug
-      this.dataObj = res.data;
+      this.dataObj = res.data.response;
       this.response = res;
-      //console.log(this.dataObj);
+      console.log(this.dataObj);
       //Session.set("apicResponse", res.data.response);
       this.addToDB();
     }
