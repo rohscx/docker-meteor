256cbf2743f583d1e5aa32d8537bc3e50916313c
rohscx <R_E__g_@Macintosh.local>
2017-06-27 15:52:44 -0700

    setup

--- imports/client/components/List.js
+++ imports/client/components/List.js
@@ -2,24 +2,15 @@ import React, {Component} from 'react';
 import { Session } from 'meteor/session';
 
 export default class List extends Component {
-  handleChange(){
-    console.log(this);
-  }
-/*
-<h3>{Object.entries(this.props.item.apicData.dataObj.response["0"]).map(([key,value])=>{
-  return (
-    <h3>{key} : {value}</h3>
-  );
-})}
-</h3>
-*/
-
   render() {
     console.log(this);
       return(
-        <div>
-            <p>TEST DATA</p>
-        </div>
+        <h3>{Object.entries(this.props.itemList).map(([key,value])=>{
+          return (
+            <h3>{key} : {value}</h3>
+          );
+        })}
+        </h3>
       )
   }
 }
