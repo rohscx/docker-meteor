8eda76fb7b5271d54368abd606068be077f31e72
rohscx <R_E__g_@Macintosh.local>
2017-06-27 10:03:29 -0700

    setup

--- imports/client/ItemApic.js
+++ imports/client/ItemApic.js
@@ -20,7 +20,7 @@ export default class ItemApic extends Component {
        <span>Return</span>
        <div className='vote-two'>
          <span>{this.props.item.apicData.dataObj.response["0"].hostType}</span>
-         <h3>{this.props.item.apicData.dataObj.response["0"].toString()}</h3>
+         <h3>{JSON.stringify(this.props.item.apicData.dataObj.response["0"], null, 2)}</h3>
        </div>
      </div>
      )
