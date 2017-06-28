99c3025618c571209a2bb02b3c5b54af8e164d72
rohscx <R_E__g_@Macintosh.local>
2017-06-28 06:55:08 -0700

    setup

--- imports/client/AppApic.js
+++ imports/client/AppApic.js
@@ -124,9 +124,9 @@ export default createContainer(({params}) => {
   } else {
     itemsArray = ItemsApic.find({}, {
       // ternary operator. a form of IF THEN statement
-      //limit: showAll ? 50 : 1,
+      limit: 100,
       // value 1 (OLDEST) or -1 (NEWEST) determines directions of lastUpdated
-      sort: {"lastUpdated": 1}
+      sort: {lastUpdated: 1}
     }).fetch()
   }
   return {
