c4dec18a13618a46c0d74b13886167d9964b6bb7
rohscx <R_E__g_@Macintosh.local>
2017-06-30 12:27:26 -0400

    setup

--- imports/client/AppApic.js
+++ imports/client/AppApic.js
@@ -106,6 +106,7 @@ import ItemsApic from '../api/request'
                transitionAppear={true}
                transistionAppearTimeout={600}>
                {this.props.items.map((item) => {
+                 console.log(item);
                  return <ItemApic item={item} key={item}/>
                })}
            </ReactCSSTransitionGroup>
