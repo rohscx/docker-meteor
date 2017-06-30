43815f7a7996d4cbfe0cdefdba82bfb261b88806
rohscx <R_E__g_@Macintosh.local>
2017-06-30 12:31:25 -0400

    setup

--- imports/client/components/List.js
+++ imports/client/components/List.js
@@ -4,6 +4,7 @@ import { Session } from 'meteor/session';
 export default class List extends Component {
   render() {
     returnList = Object.entries(this.props.itemList).map(([key,value])=>{
+      console.log(key);
         return (
           <div className='two-a' key={key}><sup>{key}</sup><p >{value}</p></div>
       );
