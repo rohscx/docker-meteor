8e6df365d4e2b94850cc6e498af77cee877c6fc3
rohscx <R_E__g_@Macintosh.local>
2017-06-30 15:58:27 -0400

    setup

--- imports/client/AppApic.js
+++ imports/client/AppApic.js
@@ -100,7 +100,7 @@ import ItemsApic from '../api/request'
             <Apic {... this.state} ticketStatus={this.ticketStatus.bind(this)}/>
 
              <ReactCSSTransitionGroup
-               transitionName='item'
+               transitionName='itemApic'
                transitionEnterTimeout={600}
                transitionLeaveTimeout={600}
                transitionAppear={true}
