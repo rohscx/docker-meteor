3ab63d01501cac9e39e91449ec8fdacbb3fa11ad
rohscx <R_E__g_@Macintosh.local>
2017-06-30 22:15:39 -0400

    setup

--- imports/client/Apic.js
+++ imports/client/Apic.js
@@ -53,7 +53,7 @@ makeRequest() {
       console.log(res); // debug
       // console.log(JSON.parse(JSON.stringify(res))); // debug
       if(response.date.length == 0){
-        this.dataObj = {response: {dateError: emptyArray}}
+        this.dataObj = {yo: {response: {dataError: emptyArray}}};
         this.addToDB();
       } else {
         this.dataObj = res.data;
