cfe28b2fba0f754e2e8c4971363266cc2a77b810
rohscx <R_E__g_@Macintosh.local>
2017-07-03 10:48:29 -0400

    setup

--- imports/client/AppApic.js
+++ imports/client/AppApic.js
@@ -141,7 +141,7 @@ import ApicTrace from './components/ApicTrace';
    ipTrace(){
      return (
        <button type="button" className="btn btn-primary" onClick={this.showTrace}>
-         Trace {this.props.ipTrace ? 'Show': 'Hide'}
+         Trace {this.props.showTrace ? 'Show': 'Hide'}
        </button>
      );
    }
