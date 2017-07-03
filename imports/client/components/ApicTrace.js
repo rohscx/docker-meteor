dff3a1c7d44c101ea9d492122bf5186fb13aa2e5
rohscx <R_E__g_@Macintosh.local>
2017-07-03 09:55:20 -0400

    setup

--- imports/client/components/ApicTrace.js
+++ imports/client/components/ApicTrace.js
@@ -17,22 +17,35 @@ export default class ApicTrace extends Component {
     });
     console.log(this);
       return(
-        <div className='item'>
 
+        <div className='item'>
+          <div className='vote-one' onClick={this.voteOne.bind(this)}>
+            <label>Source</label>
+            <input type="source" ref="sourceIP" />
+        </div>
+        <span>VS</span>
+        <div className='vote-two' onClick={this.voteTwo.bind(this)}>
+          <label>Destination</label>
+          <input type="destination" ref="destinationIP" />
+        </div>
+        </div>
+
+
+        /*
+        <div className='item'>
+          <h3>Apic Trace</h3>
           <form onSubmit={this.handleSubmit.bind(this)}>
             <div className='vote-one'>
-                <h3>Apic Trace</h3>
-              <label>Source</label>
-              <input type="source" ref="sourceIP" />
+
             </div>
             <span>VS</span>
             <div className='vote-two'>
-              <label>Destination</label>
-              <input type="destination" ref="destinationIP" />
+
             </div>
             <input type="submit" value="Submit" />
           </form>
         </div>
+        */
       )
   }
 }
