b3fdbd78464045acbc4f0506dbceaeebd42211eb
rohscx <R_E__g_@Macintosh.local>
2017-07-04 13:03:51 -0400

    setup

--- imports/client/components/ApicTrace.js
+++ imports/client/components/ApicTrace.js
@@ -2,40 +2,59 @@ import React, {Component} from 'react';
 import { Session } from 'meteor/session';
 
 export default class ApicTrace extends Component {
+
+  constructor(props) {
+  super(props);
+  this.state = {
+    value: {
+      source:'',
+      destination:''
+    }};
+
+  this.handleChange = this.handleChange.bind(this);
+  this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value.source: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+
+
   static defaultProps = {
     categories: ['Web Design', 'Web Development', 'Mobile Development']
   }
 
-  handleSubmit(e){
-    e.preventDefault();
-    console.log(this);
-    console.log(e);
-    }
 
-    traceForm(){
-        if(this.props.showTrace){
-          return (
-            <form onSubmit={this.handleSubmit.bind(this)}>
-              <div className='trace'>
-                <div className='source'>
-                  <label> Source </label>
-                  <input type="source" ref="sourceIP" />
-              </div>
-              <div className='destination'>
-                <label> Destination </label>
-                <input type="destination" ref="destinationIP" />
-              </div>
-              </div>
-              <input type="submit" value="Submit" className="btn"/>
-            </form>
+  traceForm(){
+      if(this.props.showTrace){
+        return (
+          <form onSubmit={this.handleSubmit}>
+            <div className='trace'>
+              <div className='source'>
+                <label> Source </label>
+                <input type="text" value={this.state.value} onChange={this.handleChange} />
+            </div>
+            <div className='destination'>
+              <label> Destination </label>
+              <input type="text />
+            </div>
+            </div>
+            <input type="submit" value="Submit" className="btn"/>
+          </form>
 
-          );
-        } else {
-          return (
-            <div/>
-          );
-        }
-    }
+        );
+      } else {
+        return (
+          <div/>
+        );
+      }
+  }
 
   render() {
     let categoryOptions = this.props.categories.map(category => {
