7e5fed5b9917ef20ec16482631a84cc197df8d73
rohscx <R_E__g_@Macintosh.local>
2017-07-05 16:17:12 -0400

    setup

--- imports/client/Apic.js
+++ imports/client/Apic.js
@@ -33,6 +33,7 @@ makeRequest() {
     } else {
       // success!
       console.log(res);	// debug
+      console.log(this); // debug
       this.ticket = res.data.response.serviceTicket;
       this.options.headers['x-auth-token'] = res.data.response.serviceTicket;
       //Session.set("apicTicket", res.data.response.serviceTicket);
