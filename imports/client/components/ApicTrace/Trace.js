7f03626fc116932925da5c7079e531d5aa8ec109
rohscx <R_E__g_@Macintosh.local>
2017-07-05 18:53:46 -0400

    setup

--- imports/client/components/ApicTrace/Trace.js
+++ imports/client/components/ApicTrace/Trace.js
@@ -69,7 +69,7 @@ export default class Trace extends Component {
           console.log(this.dataObj);
           //Session.set("apicFlowResponse", res.data.response.flowAnalysisId);
           //this.addToDB();
-          return console.log('THIS SHOULD BE COMPLETED SOME TIME IN THE PAST OR SOMETHING LIKE THAT');
+          return this.setState({isLoading: false});
         }
       }
     })};
