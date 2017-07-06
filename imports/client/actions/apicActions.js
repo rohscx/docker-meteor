46622fa1934e55b828961f361afb4f8a9e03876a
rohscx <R_E__g_@Macintosh.local>
2017-07-06 10:17:17 -0400

    setup

--- imports/client/actions/apicActions.js
+++ imports/client/actions/apicActions.js
@@ -1,3 +1,5 @@
+import React, {Component} from 'react';
+
 export function setTicket(ticket) {
   return {
     type: "SET_TICKET",
@@ -17,4 +19,46 @@ export function setTrace(trace) {
     type: "SET_TRACE",
     payload: trace
   };
+}
+
+export function getTicket(trace) {
+  return dispatch => {
+    function restRequest(options) {
+      // always initialize all instance properties
+      this.apicAPI = 'https://devnetapi.cisco.com/sandbox/apic_em';
+      this.apicTicket = '/api/v1/ticket';
+      this.apicFlow = '/api/v1/flow-analysis';
+      this.apicFlowAnalysisId= '';
+      this.apicTicketURL = this.apicAPI + this.apicTicket;
+      this.apicFlowURL = this.apicAPI + this.apicFlow;
+      this.apicFlowAnalysisIdURL= this.apicAPI + this.apicFlowAnalysisId;
+      this.apicTicketOptions = {
+        headers: { 'content-type': 'application/json' },
+        data: {username: 'devnetuser', password: 'Cisco123!'}
+      }
+      this.apicFlowOptions = options;
+    }
+    // Method REQUEST a ticket from APIC
+    restRequest.prototype.makeTicket = function() {
+      Meteor.call('checkApic', 'POST', this.apicTicketURL, this.apicTicketOptions, (err, res) => {
+      if (err) {
+        alert(err);
+      } else {
+        // success!
+        this.ticket = res.data.response.serviceTicket;
+        this.apicFlowOptions.headers['x-auth-token'] = res.data.response.serviceTicket;
+        //Session.set("apicTicket", res.data.response.serviceTicket);
+        console.log(res);	// debug
+        console.log(this); // debug
+        //this.makeFlowID();
+      }
+    })};
+    
+    let apic = new restRequest({
+          headers: { 'content-type': 'application/json'},
+          data: { 'sourceIP': '10.2.1.22', 'destIP': '10.1.12.20'}
+        });
+        apic.makeTicket();
+        console.log('LAST ACTION');
+  }
 }
