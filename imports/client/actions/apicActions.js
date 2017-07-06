618484def328ba6df3c83865666ec39fe77407d5
rohscx <R_E__g_@Macintosh.local>
2017-07-06 16:04:21 -0400

    setup

--- imports/client/actions/apicActions.js
+++ imports/client/actions/apicActions.js
@@ -26,7 +26,7 @@ export function getTicket(ticket) {
   return dispatch => {
     //dispatch(getTicket(ticket))
     const  myHeaders = new Headers ({
-      "content-type": "application/json; charset=UTF-8",
+      "content-type": "multipart/form-data",
       "cache-control": "no-cache",
       'Access-Control-Allow-Origin':'*'
     });
