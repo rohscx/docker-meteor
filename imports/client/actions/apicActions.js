2c2d260592aa9ca3815e6356bd1a1a658785cec5
rohscx <rohscx@gmail.com>
2017-07-06 14:50:14 -0400

    Update apicActions.js

--- imports/client/actions/apicActions.js
+++ imports/client/actions/apicActions.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import fetch from 'isomorphic-fetch';
 
 export function setTicket(ticket) {
   return {
@@ -21,46 +22,10 @@ export function setTrace(trace) {
   };
 }
 
-export function getTicket(ticket) {
+function fetchTicket(ticket) {
   return dispatch => {
-    function restRequest(options) {
-      // always initialize all instance properties
-      this.apicAPI = 'https://devnetapi.cisco.com/sandbox/apic_em';
-      this.apicTicket = '/api/v1/ticket';
-      this.apicFlow = '/api/v1/flow-analysis';
-      this.apicFlowAnalysisId= '';
-      this.apicTicketURL = this.apicAPI + this.apicTicket;
-      this.apicFlowURL = this.apicAPI + this.apicFlow;
-      this.apicFlowAnalysisIdURL= this.apicAPI + this.apicFlowAnalysisId;
-      this.apicTicketOptions = {
-        headers: { 'content-type': 'application/json' },
-        data: {username: 'devnetuser', password: 'Cisco123!'}
-      }
-      this.apicFlowOptions = options;
-    }
-    // Method REQUEST a ticket from APIC
-    restRequest.prototype.makeTicket = function() {
-      Meteor.call('checkApic', 'POST', this.apicTicketURL, this.apicTicketOptions, (err, res) => {
-      if (err) {
-        alert(err);
-      } else {
-        // success!
-        this.ticket = res.data.response.serviceTicket;
-        this.apicFlowOptions.headers['x-auth-token'] = res.data.response.serviceTicket;
-        //Session.set("apicTicket", res.data.response.serviceTicket);
-        console.log(res);	// debug
-        console.log(this); // debug
-        //this.makeFlowID();
-        return this;
-      }
-    })};
-
-    let apic = new restRequest({
-          headers: { 'content-type': 'application/json'},
-          data: { 'sourceIP': '10.2.1.22', 'destIP': '10.1.12.20'}
-        });
-
-        apic.makeTicket()
-
+    dispatch(requestPosts(ticket))
+    return fetch(`https://www.reddit.com/r/${subreddit}.json`)
+      .then(response => response.json())
   }
 }
