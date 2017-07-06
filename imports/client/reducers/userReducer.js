81988c84aa7a79d3197bb00e31fc25a8986205e3
rohscx <R_E__g_@Macintosh.local>
2017-07-06 09:29:35 -0400

    setup

--- imports/client/reducers/userReducer.js
+++ imports/client/reducers/userReducer.js
@@ -4,10 +4,7 @@ const userReducer = (state = {
 }, action) => {
   switch (action.type) {
     case "SET_NAME":
-      state = {
-        ...state,
-        name: action.payload,
-      };
+      state = name: action.payload
       break;
     case "SET_AGE":
       state.result = {
