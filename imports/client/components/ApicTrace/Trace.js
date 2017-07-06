98a0a726970a107e54c86b38ba2366858fdfd6d5
rohscx <R_E__g_@Macintosh.local>
2017-07-05 21:25:09 -0400

    setup

--- imports/client/components/ApicTrace/Trace.js
+++ imports/client/components/ApicTrace/Trace.js
@@ -58,7 +58,7 @@ export default class Trace extends Component {
         } else {
           this.dataObj = res.data;
           this.apicFlowAnalysisId = res.data.response.url;
-          this.apicFlowAnalysisIdURL = this.apicAPI + his.apicFlowAnalysisId;
+          this.apicFlowAnalysisIdURL = this.apicAPI + this.apicFlowAnalysisId;
           console.log(this);
           console.log(this.apicFlowAnalysisIdURL);
           //Session.set("apicFlowResponse", res.data.response.flowAnalysisId);
