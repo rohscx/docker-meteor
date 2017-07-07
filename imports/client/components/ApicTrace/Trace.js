802a74194c6e7c0705375e72ff089b54a2e420a5
rohscx <R_E__g_@Macintosh.local>
2017-07-07 14:03:47 -0400

    setup

--- imports/client/components/ApicTrace/Trace.js
+++ imports/client/components/ApicTrace/Trace.js
@@ -3,15 +3,19 @@ import { Session } from 'meteor/session';
 // WORK IN PROGRESS
 export default class Trace extends Component {
 
+  loopThrough(){
+    Object.keys(this.props.traceObj).map(function(key,index){
+      console.log(item[key]);
+      return (
+        {item[key]}
+      );
+    })
+  }
+
 
   render() {
     return(
-      Object.keys(this.props.traceObj).map(function(key,index){
-        console.log(item[key]);
-        return (
-          {key}
-        );
-      })
+      {this.loopThrough()}
     )
   }
 }
