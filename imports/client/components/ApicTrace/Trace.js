cf4de2a44937c6637439d0497b1db22c6a430fef
rohscx <R_E__g_@Macintosh.local>
2017-07-07 15:16:46 -0400

    setup

--- imports/client/components/ApicTrace/Trace.js
+++ imports/client/components/ApicTrace/Trace.js
@@ -9,7 +9,7 @@ export default class Trace extends Component {
       console.log("key: ",key);
       console.log("index: ",index);
       return (
-        <div key={index}>{item[key]}</div>
+        <div key={index}>{key} {item[key]}</div>
       );
     })
     return myReturn;
