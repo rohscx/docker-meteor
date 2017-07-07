918bd10e6313f13b7183b308bc7a9bfbb125c42d
rohscx <R_E__g_@Macintosh.local>
2017-07-07 13:22:30 -0400

    setup

--- imports/client/components/ApicTrace/Trace.js
+++ imports/client/components/ApicTrace/Trace.js
@@ -110,6 +110,7 @@ export default class Trace extends Component {
     let cats = this.props.apic.flow.map(item => {
       console.log(item);
       let flowObj = Object.keys(item).map(function(key,index){
+        console.log({item[key]});
         return (
           <div key={index}>
             {item[key]}
