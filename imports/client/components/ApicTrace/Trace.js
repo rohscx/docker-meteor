bc8d1cf706638716e91adc912be3c0d219b8aea3
rohscx <R_E__g_@Macintosh.local>
2017-07-08 10:29:38 -0400

    setup

--- imports/client/components/ApicTrace/Trace.js
+++ imports/client/components/ApicTrace/Trace.js
@@ -6,13 +6,8 @@ export default class Trace extends Component {
   render() {
     return(
       <div>
-      <button onClick={
-        () => console.log(this)
-      }>LOG PROPS TRACE</button>
-      <div>
         {this.props.flowItem}
       </div>
-    </div>
     )
   }
 }
