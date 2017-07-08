21ec73ee565b8a6ea64a61879932f1bdc04820d9
rohscx <R_E__g_@Macintosh.local>
2017-07-08 18:20:45 -0400

    setup

--- imports/client/components/ApicTrace.js
+++ imports/client/components/ApicTrace.js
@@ -108,8 +108,8 @@ class ApicTrace extends Component {
   flowID(){
     this.props.getFlowId(
       this.props.apic.ticket,
-      this.props.apic.traceIp[source],
-      this.props.apic.traceIp[destination]
+      this.props.apic.traceIp["source"],
+      this.props.apic.traceIp["destination"]
     )
   }
 
