430cd9682416462af72e9f8890d5dbf1b7636955
rohscx <R_E__g_@Macintosh.local>
2017-07-09 10:06:23 -0400

    setup

--- imports/client/components/ApicTrace.js
+++ imports/client/components/ApicTrace.js
@@ -98,7 +98,7 @@ class ApicTrace extends Component {
       <div>
       {this.props.apic.ticket} <br/>
       {this.props.apic.flowId} <br/>
-      {this.props.apic.traceStatus.status} <br/>
+      {this.props.apic.traceStatus.status ? this.props.apic.traceStatus.status : "NO STATUS"} <br/>
 
       <button type="button" className="btn btn-primary" onClick={
         () => this.props.apic.showTrace ? this.props.setShowTrace(false) : this.props.setShowTrace(true)
