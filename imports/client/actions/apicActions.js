fed8f6eb265e78d68c4c6a8663cb933ceb7269f9
rohscx <R_E__g_@Macintosh.local>
2017-07-09 14:59:40 -0400

    setup

--- imports/client/actions/apicActions.js
+++ imports/client/actions/apicActions.js
@@ -145,7 +145,7 @@ export function getFlowStatus(ticket,flowId) {
       if (err) {
         alert(err);
       } else if (res.data.response.request.status != "COMPLETED") {
-        //dispatch(setTraceStatus(res.data.response.request));
+        dispatch(setTraceStatus(res.data.response.request));
         console.log("STATUS OF REQUEST : ", res.data.response.request.status)
         setTimeout(getFlowStatus(ticket,flowId), 1000);
       } else {
