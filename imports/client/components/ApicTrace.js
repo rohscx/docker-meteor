f4358f570127905ca922f85801cbcb130c514df3
rohscx <R_E__g_@Macintosh.local>
2017-07-09 17:07:47 -0400

    setup

--- imports/client/components/ApicTrace.js
+++ imports/client/components/ApicTrace.js
@@ -119,9 +119,9 @@ class ApicTrace extends Component {
         )
       }>GET FLOW ID</button>
 
-      <button type="button" className="btn btn-primary" onClick={
+      <button type="button" className="btn btn-primary ld-ext-right running" onClick={
         () => this.props.getFlowStatus(this.props.apic.ticket, this.props.apic.flowId)
-      }><i className={this.props.apic.traceStatus.isready ? "" : "loadingSpinner"} ></i>GET FLOW STATUS</button>
+      }><i className={this.props.apic.traceStatus.isready ? "" : "<div class="ld ld-ring ld-spin"></div>"} ></i>GET FLOW STATUS</button>
 
       <button type="button" className="btn btn-primary" onClick={
         () => this.props.getFlow(this.props.apic.ticket, this.props.apic.flowId)
