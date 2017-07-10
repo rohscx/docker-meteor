702b983a37f8a22fb3f683b5790d3b6794b7a0b1
rohscx <R_E__g_@Macintosh.local>
2017-07-10 14:27:06 -0400

    setup

--- imports/client/components/ApicTrace/Test.js
+++ imports/client/components/ApicTrace/Test.js
@@ -28,7 +28,7 @@ export default class Test extends Component {
     return (
       <div className="row">
         <div className="col-sm-12" style={divStyleTrace}>
-          {this.props.flowItemType + "" + this.props.flowItemName}
+          {this.props.flowItemName + " " + this.props.flowItemType}
         </div>
       </div>
     );
