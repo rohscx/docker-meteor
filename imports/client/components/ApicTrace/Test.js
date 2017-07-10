16187dbdbc24dc97defc44738b779211b35da9df
rohscx <R_E__g_@Macintosh.local>
2017-07-10 11:46:42 -0400

    setup

--- imports/client/components/ApicTrace/Test.js
+++ imports/client/components/ApicTrace/Test.js
@@ -10,8 +10,8 @@ export default class Trace extends Component {
       borderBottomColor: "black",
       borderBottomWidth: 10
     };
+    const ransom = ["<div><li>adfasf</li><li>adfasf</li><li>adfasf</li><li>adfasf</li><li>adfasf</li></div>"];
     return (
-      const ransom = ["<div><li>adfasf</li><li>adfasf</li><li>adfasf</li><li>adfasf</li><li>adfasf</li></div>"];
       <div className="row">
         <div className="col-sm-12" style={divStyleLavTraceData}>
 
