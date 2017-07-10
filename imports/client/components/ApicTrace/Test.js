9615c4db64ac8efaa78d3e29ec753ea76a8b7ded
rohscx <R_E__g_@Macintosh.local>
2017-07-10 12:08:18 -0400

    setup

--- imports/client/components/ApicTrace/Test.js
+++ imports/client/components/ApicTrace/Test.js
@@ -13,8 +13,8 @@ export default class Trace extends Component {
     const ransom = ["<div><li>adfasf</li><li>adfasf</li><li>adfasf</li><li>adfasf</li><li>adfasf</li></div>","<li>1231f</li><li>1231231</li></div>"];
     return (
       <div className="row">
-        <div className="col-sm-12" style={divStyleLavTraceData} }>
-          <ul dangerouslySetInnerHTML={{__html: ransom.join("")}>
+        <div className="col-sm-12" style={divStyleLavTraceData} >
+          <ul dangerouslySetInnerHTML={{__html: ransom.join("")}}>
           </ul>
         </div>
       </div>
