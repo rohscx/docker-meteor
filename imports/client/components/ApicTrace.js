bfc06d31084cb2e2fcbd4173979bf4ae03c3df6c
rohscx <R_E__g_@Macintosh.local>
2017-07-10 16:16:00 -0400

    setup

--- imports/client/components/ApicTrace.js
+++ imports/client/components/ApicTrace.js
@@ -103,12 +103,13 @@ class ApicTrace extends Component {
       let deviceInfo = [];
       let deviceType = [];
       let deviceName = [];
-      if(Object.getOwnPropertyNames(item).length >= 1){
+      if(Object.getOwnPropertyNames(item).length <= 4){
         console.log("THIS IS SOME PROP",Object.getOwnPropertyNames(item).length);
         if(this.props.apic.traceStatus.status == "INPROGRESS"){
           console.log("STATUS OF REQUEST : ", this.props.apic.traceStatus.status)
           setTimeout(this.props.getFlowStatus(this.props.apic.ticket, this.props.apic.flowId), 1000);
         }
+        return <div></div>
       }else{
         let rickets = Object.keys(item).map(function(key,index){
           console.log("item[key]: ",item[key]);
