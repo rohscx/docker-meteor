872222d734b1b80c3307679bbda04a1021cc1f6f
rohscx <R_E__g_@Macintosh.local>
2017-07-09 23:14:21 -0400

    setup

--- imports/client/components/ApicTrace.js
+++ imports/client/components/ApicTrace.js
@@ -75,22 +75,24 @@ class ApicTrace extends Component {
         }
       }else{
         console.log("REAL RESULT WAS RUN");
-        return Object.keys(item).map(function(key,index){
-          console.log("item[key]: ",item[key]);
-          console.log("key: ",key);
-          console.log("index: ",index);
-          console.log("item: ", item);
-          console.log("typeof item: ",typeof item);
-          console.log("typeof item[key]: ",typeof item[key]);
-          console.log("typeof key: ",typeof key);
-          console.log("typeof index: ",typeof index);
-          if(typeof item[key] === 'object'){
-            console.log("item[key].physicalInterface.name: ",key,item[key].physicalInterface.name);
-            return <Trace flowItem={item[key].physicalInterface.name} flowIndex={key} key={index} />;
-          } else {
-            return <Trace flowItem={item[key]} flowIndex={key} key={index} />;
-          }
-        })
+        return (
+          Object.keys(item).map(function(key,index){
+            console.log("item[key]: ",item[key]);
+            console.log("key: ",key);
+            console.log("index: ",index);
+            console.log("item: ", item);
+            console.log("typeof item: ",typeof item);
+            console.log("typeof item[key]: ",typeof item[key]);
+            console.log("typeof key: ",typeof key);
+            console.log("typeof index: ",typeof index);
+            if(typeof item[key] === 'object'){
+              console.log("item[key].physicalInterface.name: ",key,item[key].physicalInterface.name);
+              return <Trace flowItem={item[key].physicalInterface.name} flowIndex={key} key={index} />;
+            } else {
+              return <Trace flowItem={item[key]} flowIndex={key} key={index} />;
+            }
+          })
+        )
       }
     })
   }
