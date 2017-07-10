f5e5b22fb80f72777eeee595401bcb3517183c27
rohscx <R_E__g_@Macintosh.local>
2017-07-09 21:49:29 -0400

    setup

--- imports/client/components/ApicTrace/Trace.js
+++ imports/client/components/ApicTrace/Trace.js
@@ -4,23 +4,36 @@ import {Accordion, AccordionSection}  from 'redux-accordion';
 // WORK IN PROGRESS
 export default class Trace extends Component {
 
+  trace(){
+    return (
+      <div className="row">
+        <div className="col-sm-12">
+          {this.props.flowItem}
+        </div>
+      </div>
+    );
+  }
+
   render() {
+    const divStyleLav = {
+      backgroundColor: "lavender"
+    };
+    const divStyleLavBlush = {
+      textAlign: "center"
+    };
 
     return(
       <Accordion
-        {...this.props} uniqId={this.props.flowIndex}
-        settings={{
-          headerBackgroundColor: "red",
-          headerBackgroundColorActive: 'pink',
-          headerColor: "black",
-          contentBackgroundColor: "blue",
-          borderBottom: "2px solid yellow",
-          round: "4px"
-        }}>
+        {...this.props}
+        uniqId={this.props.flowIndex}>
         <AccordionSection
            title={this.props.flowIndex}>
-
+            <div style={divStyleLavBlush}>
+              {this.trace()}
+            </div>
         </AccordionSection>
+
+
       </Accordion>
     )
   }
