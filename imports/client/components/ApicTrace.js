e1f2ae8425589a07ff955828ab6813885bb84cb8
rohscx <R_E__g_@Macintosh.local>
2017-07-09 23:21:29 -0400

    setup

--- imports/client/components/ApicTrace.js
+++ imports/client/components/ApicTrace.js
@@ -76,34 +76,33 @@ class ApicTrace extends Component {
       }else{
         console.log("REAL RESULT WAS RUN");
         return (
+          Object.keys(item).map(function(key,index){
+            return (
+              <Accordion
+                {...this.props}
+                uniqId={this.props.flowIndex}>
+                <AccordionSection
+                   title={this.trace()}>
 
-          const cats = Object.keys(item).map(function(key,index){
-            console.log("item[key]: ",item[key]);
-            console.log("key: ",key);
-            console.log("index: ",index);
-            console.log("item: ", item);
-            console.log("typeof item: ",typeof item);
-            console.log("typeof item[key]: ",typeof item[key]);
-            console.log("typeof key: ",typeof key);
-            console.log("typeof index: ",typeof index);
-            if(typeof item[key] === 'object'){
-              console.log("item[key].physicalInterface.name: ",key,item[key].physicalInterface.name);
-              return <Trace flowItem={item[key].physicalInterface.name} flowIndex={key} key={index} />;
-            } else {
-              return <Trace flowItem={item[key]} flowIndex={key} key={index} />;
-            }
-          })
+                      {            console.log("item[key]: ",item[key]);
+                                  console.log("key: ",key);
+                                  console.log("index: ",index);
+                                  console.log("item: ", item);
+                                  console.log("typeof item: ",typeof item);
+                                  console.log("typeof item[key]: ",typeof item[key]);
+                                  console.log("typeof key: ",typeof key);
+                                  console.log("typeof index: ",typeof index);
+                                  if(typeof item[key] === 'object'){
+                                    console.log("item[key].physicalInterface.name: ",key,item[key].physicalInterface.name);
+                                    return <Trace flowItem={item[key].physicalInterface.name} flowIndex={key} key={index} />;
+                                  } else {
+                                    return <Trace flowItem={item[key]} flowIndex={key} key={index} />;
+                                  }
+                                })}
 
-          <Accordion
-            {...this.props}
-            uniqId={"test"}>
-            <AccordionSection
-               title={"more test"
-
-                  {cats}
-
-            </AccordionSection>
-          </Accordion>
+                </AccordionSection>
+              </Accordion>
+            )
         )
       }
     })
