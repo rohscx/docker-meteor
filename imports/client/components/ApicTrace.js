a89d27ac68e11ef1cc9f943a40c4ac658e0a3d5e
rohscx <R_E__g_@Macintosh.local>
2017-07-11 00:37:08 -0400

    setup

--- imports/client/components/ApicTrace.js
+++ imports/client/components/ApicTrace.js
@@ -34,23 +34,15 @@ class ApicTrace extends Component {
 
 
   tracePath(){
-
-    function resolveAfter2Seconds(x) {
-      return new Promise(resolve => {
-        setTimeout(() => {
-          resolve(x);
-        }, 2000);
-      });
-    }
-    async function add1(x) {
-      var a = resolveAfter2Seconds(20);
-      var b = resolveAfter2Seconds(30);
-      return x + await a + await b;
-    };
-
-    add1(10).then(v => {
-      console.log(v);  // prints 60 after 2 seconds.
-    });
+    async function getProcessedData() {
+      let v;
+      try {
+        v = await this.props.getTicket();
+      } catch(e) {
+        v = await this.props.getTicket();
+      }
+      return processDataInWorker(v);
+}
 
   }
 
