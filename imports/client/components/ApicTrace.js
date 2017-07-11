4920db13f18b91dc0ae80f72b034360b07ab7c88
rohscx <R_E__g_@Macintosh.local>
2017-07-10 22:53:33 -0400

    setup

--- imports/client/components/ApicTrace.js
+++ imports/client/components/ApicTrace.js
@@ -41,7 +41,8 @@ class ApicTrace extends Component {
 
   traceForm(){
     const mainDiv = {
-      width: "40%"
+      width: "40%",
+      borderRadius: "15px";
     }
       if(this.props.apic.showTrace){
         return (
