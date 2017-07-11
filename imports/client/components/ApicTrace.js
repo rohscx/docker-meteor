0218c1ef8a6c6459bae498a87b1adf16f903918f
rohscx <R_E__g_@Macintosh.local>
2017-07-10 22:24:47 -0400

    setup

--- imports/client/components/ApicTrace.js
+++ imports/client/components/ApicTrace.js
@@ -43,6 +43,7 @@ class ApicTrace extends Component {
       if(this.props.apic.showTrace){
         return (
           <form onSubmit={this.handleSubmit}>
+            <div className="footer">This footer will always be positioned at the bottom of the page, but <strong>not fixed</strong>.</div>
             <div className='trace'>
               <div className='source'>
                 <label> Source </label>
