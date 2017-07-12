9ada41116c0acd4236da026040bf2a02f909f712
rohscx <R_E__g_@Macintosh.local>
2017-07-11 21:55:34 -0400

    setup

--- imports/client/layouts/MainLayout.js
+++ imports/client/layouts/MainLayout.js
@@ -35,7 +35,7 @@ const MainLayout = ({children}) =>
   <nav className="navbar navbar-inverse navbar-fixed-top">
     <div className="container-fluid">
       <div className="navbar-header">
-          <button type="button" className="navbar-toggle" dataToggle="collapse" dataTarget="#myNavbar">
+          <button type="button" className="navbar-toggle">
             <span className="icon-bar"></span>
             <span className="icon-bar"></span>
             <span className="icon-bar"></span>
