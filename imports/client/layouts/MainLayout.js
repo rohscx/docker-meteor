b9f621b57390203302771c98aaa48735916557d7
rohscx <R_E__g_@Macintosh.local>
2017-07-12 01:07:10 -0400

    setup

--- imports/client/layouts/MainLayout.js
+++ imports/client/layouts/MainLayout.js
@@ -37,38 +37,40 @@ const activeStyleObj = {
 
 export default class MainLayout extends Component {
   MainLayout(children){
-    <div>
-      <nav className="navbar navbar-inverse navbar-fixed-top">
-        <div className="container-fluid">
-          <div className="navbar-header">
-              <button type="button" className="navbar-toggle" dataToggle="collapse" dataTarget="#myNavbar">
-                <span className="icon-bar"></span>
-                <span className="icon-bar"></span>
-                <span className="icon-bar"></span>
-            </button>
-            <a className="navbar-brand" href="/">Frost</a>
-          </div>
-          <div>
-            <div className="collapse navbar-collapse" id="myNavbar">
-              <ul className="nav navbar-nav">
-                <li><a href="/apic">Section 1</a></li>
-                <li><a href="/ise">Section 2</a></li>
-                <li><a href="about">Section 3</a></li>
-                <li className="dropdown"><a className="dropdown-toggle" dataToggle="dropdown" href="#">Section 4 <span className="caret"></span></a>
-                  <ul className="dropdown-menu">
-                    <li><a href="#section41">Section 4-1</a></li>
-                    <li><a href="#section42">Section 4-2</a></li>
-                  </ul>
-                </li>
-              </ul>
+    return (
+      <div>
+        <nav className="navbar navbar-inverse navbar-fixed-top">
+          <div className="container-fluid">
+            <div className="navbar-header">
+                <button type="button" className="navbar-toggle" dataToggle="collapse" dataTarget="#myNavbar">
+                  <span className="icon-bar"></span>
+                  <span className="icon-bar"></span>
+                  <span className="icon-bar"></span>
+              </button>
+              <a className="navbar-brand" href="/">Frost</a>
+            </div>
+            <div>
+              <div className="collapse navbar-collapse" id="myNavbar">
+                <ul className="nav navbar-nav">
+                  <li><a href="/apic">Section 1</a></li>
+                  <li><a href="/ise">Section 2</a></li>
+                  <li><a href="about">Section 3</a></li>
+                  <li className="dropdown"><a className="dropdown-toggle" dataToggle="dropdown" href="#">Section 4 <span className="caret"></span></a>
+                    <ul className="dropdown-menu">
+                      <li><a href="#section41">Section 4-1</a></li>
+                      <li><a href="#section42">Section 4-2</a></li>
+                    </ul>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
+        </nav>
+        <div>
+          {children}
         </div>
-      </nav>
-      <div>
-        {children}
       </div>
-    </div>
+    )
   }
 
   render() {
