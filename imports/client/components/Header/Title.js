5c84f6cc4b7f111250aca348e26115c25c811727
rohscx <R_E__g_@Macintosh.local>
2017-07-12 08:34:15 -0400

    setup

--- imports/client/components/Header/Title.js
+++ imports/client/components/Header/Title.js
@@ -4,10 +4,11 @@ import { Session } from 'meteor/session';
 export default class Title extends Component {
 
   render() {
+      const divStyles = {paddingButtom: "5%"};
     return(
-      <div>
+      <div sytle={divStyles}>
         <h1>{this.props.greeting}</h1>
-        
+
       </div>
     )
   }
