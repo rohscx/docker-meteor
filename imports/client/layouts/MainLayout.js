9a80de9b78f688d994341a40f94a97b60274c02e
rohscx <R_E__g_@Macintosh.local>
2017-07-12 11:17:21 -0400

    setup

--- imports/client/layouts/MainLayout.js
+++ imports/client/layouts/MainLayout.js
@@ -17,17 +17,32 @@ const activeStyleObj = {
 const MainLayout = ({children}) =>{
 
   return (
-    <div className='main-layout'>
-      <header>
-        <h1><Link to='/' style={styleObj} activeStyle={activeStyleObj}>FROST</Link></h1>
-        <LoginButtons/>
-        <nav>
-          <Link to='/apic' style={styleObj} activeStyle={activeStyleObj}> APIC-EM </Link>
-          <Link to='/ise' style={styleObj} activeStyle={activeStyleObj}> ISE </Link>
-          <Link to='/about' style={styleObj} activeStyle={activeStyleObj}> About </Link>
-
-        </nav>
-      </header>
+    <div>
+      <Navbar inverse collapseOnSelect>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="#">React-Bootstrap</a>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav>
+            <NavItem eventKey={1} onClick={ e => this.props.history.push("/apic") }>Link</NavItem>
+            <NavItem eventKey={2} href="#">Link</NavItem>
+            <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+              <MenuItem eventKey={3.1}>Action</MenuItem>
+              <MenuItem eventKey={3.2}>Another action</MenuItem>
+              <MenuItem eventKey={3.3}>Something else here</MenuItem>
+              <MenuItem divider />
+              <MenuItem eventKey={3.3}>Separated link</MenuItem>
+            </NavDropdown>
+          </Nav>
+          <Nav pullRight>
+            <NavItem eventKey={1} href="#">Link Right</NavItem>
+            <NavItem eventKey={2} href="#">Link Right</NavItem>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
       {children}
     </div>
   )
