0eb82be6e899f9514fe98a60df9c0e84d7d9225c
rohscx <R_E__g_@Macintosh.local>
2017-07-11 23:13:42 -0400

    setup

--- imports/client/layouts/MainLayout.js
+++ imports/client/layouts/MainLayout.js
@@ -32,33 +32,32 @@ const activeStyleObj = {
 
 // stateless functional component. Functuion returns what ever you like
 const MainLayout = ({children}) =>
-<div>
   <Navbar inverse collapseOnSelect>
-   <Navbar.Header>
-     <Navbar.Brand>
-       <a href="/">FROST</a>
-     </Navbar.Brand>
-     <Navbar.Toggle />
-   </Navbar.Header>
-   <Navbar.Collapse>
-     <Nav>
-       <NavItem eventKey={1} href="/apic">APIC-EM</NavItem>
-       <NavItem eventKey={2} href="/ise">ISE</NavItem>
-       <NavItem eventKey={3} href="/about">ISE</NavItem>
-       <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-         <MenuItem eventKey={3.1}>Action</MenuItem>
-         <MenuItem eventKey={3.2}>Another action</MenuItem>
-         <MenuItem eventKey={3.3}>Something else here</MenuItem>
-         <MenuItem divider />
-         <MenuItem eventKey={3.3}>Separated link</MenuItem>
-       </NavDropdown>
-     </Nav>
-     <Nav pullRight>
-       <NavItem eventKey={1} href="#">Link Right</NavItem>
-       <NavItem eventKey={2} href="#">Link Right</NavItem>
-     </Nav>
-   </Navbar.Collapse>
+    <Navbar.Header>
+      <Navbar.Brand>
+        <a href="#">React-Bootstrap</a>
+      </Navbar.Brand>
+      <Navbar.Toggle />
+    </Navbar.Header>
+    <Navbar.Collapse>
+      <Nav>
+        <NavItem eventKey={1} href="#">Link</NavItem>
+        <NavItem eventKey={2} href="#">Link</NavItem>
+        <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+          <MenuItem eventKey={3.1}>Action</MenuItem>
+          <MenuItem eventKey={3.2}>Another action</MenuItem>
+          <MenuItem eventKey={3.3}>Something else here</MenuItem>
+          <MenuItem divider />
+          <MenuItem eventKey={3.3}>Separated link</MenuItem>
+        </NavDropdown>
+      </Nav>
+      <Nav pullRight>
+        <NavItem eventKey={1} href="#">Link Right</NavItem>
+        <NavItem eventKey={2} href="#">Link Right</NavItem>
+      </Nav>
+    </Navbar.Collapse>
   </Navbar>
+  <div>
   {children}
 </div>
 
