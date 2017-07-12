9bf41ffcc6ddafe9c6231b3e23d4ac488fd26a70
rohscx <R_E__g_@Macintosh.local>
2017-07-12 15:33:13 -0400

    setup

--- imports/client/actions/utilActions.js
+++ imports/client/actions/utilActions.js
@@ -16,7 +16,7 @@ export function validateMac(mac) {
   return dispatch => {
       var regexp = /^(([A-Fa-f0-9]{2}[:]){5}[A-Fa-f0-9]{2}[,]?)+$/i;
       console.log(mac);
-      var mac_address = mac.target.value;
+      var mac_address = mac;
       console.log("Input: "+ mac);
     if(regexp.test(mac_address)) {
       console.log("Valid: "+ mac_address);
