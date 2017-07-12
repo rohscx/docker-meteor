d0c6b7fd48e971722a013540d381246e8aa0add6
rohscx <R_E__g_@Macintosh.local>
2017-07-12 15:17:00 -0400

    setup

--- imports/client/actions/utilActions.js
+++ imports/client/actions/utilActions.js
@@ -15,7 +15,7 @@ export function subtractNumber(number) {
 export function validateMac(mac) {
   return dispatch => {
       var regexp = /^(([A-Fa-f0-9]{2}[:]){5}[A-Fa-f0-9]{2}[,]?)+$/i;
-      var mac_address = mac;
+      var mac_address = mac.target.value;
       console.log("Input: "+ mac);
     if(regexp.test(mac_address)) {
       console.log("Valid: "+ mac_address);
