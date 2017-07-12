f018e93b3ba1182c4719d83a83543f18e496f6da
rohscx <R_E__g_@Macintosh.local>
2017-07-11 22:28:51 -0400

    setup

--- imports/client/layouts/MainLayout.js
+++ imports/client/layouts/MainLayout.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {LoginButtons} from 'meteor/okgrow:accounts-ui-react'
 import { Link } from 'react-router';
+import {DropdownButton} from 'react-bootstrap';
 
 
 // style object for nav bar
