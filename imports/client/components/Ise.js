153ef5aa2d0d0f12b3166c810bfd7714765a7d0d
rohscx <rohscx@gmail.com>
2017-07-12 12:50:22 -0400

    Update Ise.js

--- imports/client/components/Ise.js
+++ imports/client/components/Ise.js
@@ -12,7 +12,7 @@ class Ise extends Component {
       width: "40%"
     };
     return (
-      <div>
+      <div style={divStyles}>
               <Form horizontal>
     <FormGroup controlId="formHorizontalMac">
       <Col componentClass={ControlLabel} sm={2}>
