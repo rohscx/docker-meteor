3e774bc0d86613ec8b790f2da3ae3f629aff488e
rohscx <R_E__g_@Macintosh.local>
2017-07-12 14:44:48 -0400

    setup

--- imports/client/reducers/apicReducer.js
+++ imports/client/reducers/apicReducer.js
@@ -1,25 +1,71 @@
-const iseReducer = (state = {
-  result: 1,
-  lastValues: []
+const apicReducer = (state = {
+  ticket: "NO APIC TICKET",
+  devices: "NO DEVICES",
+  trace: "NO TRACES",
+  flowId: "NO FLOW ID",
+  flow: [{}],
+  showTrace: false,
+  traceIp:{
+    source:"10.2.1.22",
+    destination:"10.1.12.20"
+  },
+  traceStatus: {
+    status: null,
+    isReady: true
+  }
 }, action) => {
   switch (action.type) {
-    case "ADD":
-      state = {
-        ...state,
-        result: state.result + action.payload,
-        lastValues: [...state.lastValues, action.payload]
-      };
-      break;
-    case "SUBTRACT":
-      state.result = {
-        ...state,
-        result: state.result - action.payload,
-        lastValues: [...state.lastValues, action.payload]
-      };
-      state.lastValues.push(action.payload);
-      break;
-  }
+    case "SET_TICKET":
+    state = {
+      ...state,
+      ticket: action.payload
+    };
+    break;
+    case "SET_DEVICES":
+    state = {
+      ...state,
+      devices: action.payload
+    };
+    state.lastValues.push(action.payload);
+    break;
+    case "SET_TRACE":
+    state = {
+      ...state,
+      trace: action.payload
+    };
+    break;
+    case "SET_FLOW_ID":
+    state = {
+      ...state,
+      flowId: action.payload
+    };
+    break;
+    case "SET_FLOW":
+    state = {
+      ...state,
+      flow: action.payload
+    };
+    break;
+    case "SET_SHOW_TRACE":
+    state = {
+      ...state,
+      showTrace: action.payload
+    };
+    break;
+    case "SET_TRACEIP":
+    state = {
+      ...state,
+      traceIp: action.payload
+    };
+    break;
+    case "SET_TRACESTATUS":
+    state = {
+      ...state,
+      traceStatus: action.payload
+    };
+    break;
+    }
   return state;
 };
 
-export default iseReducer;
+export default apicReducer;
