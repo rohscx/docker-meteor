e1067968d23b11911cf36d2ae55df81733ba486c
rohscx <R_E__g_@Macintosh.local>
2017-07-12 16:25:12 -0400

    setup

--- imports/client/components/Ise.js
+++ imports/client/components/Ise.js
@@ -29,7 +29,7 @@ class Ise extends Component {
             </Col>
           </FormGroup>
           <FormGroup>
-            {this.props.util.macValidation.btnStyle ? <Col smOffset={2} sm={10}><Button type="button" bsStyle="success" block>Submit</Button></Col> : <Col smOffset={2} sm={10}><Button  type="submit" disabled> <b> . . . </b></Button></Col>}
+            {this.props.util.macValidation.btnStyle ? <Col smOffset={2} sm={10}><Button type="button" bsStyle="primary" block>Submit</Button></Col> : <Col smOffset={2} sm={10}><Button  type="submit" disabled> <b> . . . </b></Button></Col>}
           </FormGroup>
         </Form>
 
