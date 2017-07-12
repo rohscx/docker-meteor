e1405c17449d57d6e57c63ee6db303892cf9a0ed
rohscx <R_E__g_@Macintosh.local>
2017-07-12 00:27:56 -0400

    setup

--- imports/client/layouts/MainLayout.js
+++ imports/client/layouts/MainLayout.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {LoginButtons} from 'meteor/okgrow:accounts-ui-react'
-import { Link } from 'react-router';
+import { Route, RouteHandler, Link } from 'react-router';
 import {Navbar,Nav,NavItem,NavDropdown,MenuItem} from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
