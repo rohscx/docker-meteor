9014cea69e1868fe28f6b1acfb049722333e33f9
rohscx <R_E__g_@Macintosh.local>
2017-07-12 01:16:27 -0400

    setup

--- imports/client/layouts/MainLayout.js
+++ imports/client/layouts/MainLayout.js
@@ -29,17 +29,7 @@ const MainLayout = ({children}) =>
 
       </nav>
       <Nav pullRight>
-  <LinkContainer to="/home">
-    <NavItem eventKey={1}>Home</NavItem>
-  </LinkContainer>
-  <LinkContainer to="/book">
-    <NavItem eventKey={2}>Book Inv</NavItem>
-  </LinkContainer>
-  <NavDropdown eventKey={3} title="Authorization" id="basic-nav-dropdown">
-    <LinkContainer to="/logout">
-      <MenuItem eventKey={3.1}>Logout</MenuItem>
-    </LinkContainer>
-  </NavDropdown>
+<LoginButtons/>
 </Nav>
     </header>
     {children}
