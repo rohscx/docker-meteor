493982b9f202040ce3dadb20d386112d18c9f3e3
rohscx <R_E__g_@Macintosh.local>
2017-07-12 07:52:53 -0400

    setup

--- imports/client/layouts/MainLayout.js
+++ imports/client/layouts/MainLayout.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import {LoginButtons} from 'meteor/okgrow:accounts-ui-react'
 import { Link } from 'react-router';
-import { Button, Nav, Navbar, NavDropdown, MenuItem, NavItem } from 'react-bootstrap';
-import { LinkContainer } from 'react-router-bootstrap';
 
 
 // style object for nav bar
@@ -17,7 +15,6 @@ const activeStyleObj = {
 
 // stateless functional component. Functuion returns what ever you like
 const MainLayout = ({children}) =>
-
   <div className='main-layout'>
     <header>
       <h1><Link to='/' style={styleObj} activeStyle={activeStyleObj}>FROST</Link></h1>
@@ -28,9 +25,6 @@ const MainLayout = ({children}) =>
         <Link to='/about' style={styleObj} activeStyle={activeStyleObj}>About</Link>
 
       </nav>
-      <Nav pullRight>
-<LoginButtons/>
-</Nav>
     </header>
     {children}
   </div>
