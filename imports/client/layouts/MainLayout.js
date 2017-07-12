47de0e30f9bc1eead71f43717a0884ebf6eb7257
rohscx <R_E__g_@Macintosh.local>
2017-07-11 21:41:41 -0400

    setup

--- imports/client/layouts/MainLayout.js
+++ imports/client/layouts/MainLayout.js
@@ -16,12 +16,12 @@ const activeStyleObj = {
 /*
 <div className='main-layout'>
   <header>
-    <h1><Link to='/' >FROST</Link></h1>
+    <h1><Link to='/' style={styleObj} activeStyle={activeStyleObj}>FROST</Link></h1>
     <LoginButtons/>
     <nav>
-      <Link to='/apic' >APIC-EM</Link> |
-      <Link to='/ise' >ISE</Link> |
-      <Link to='/about' s>About</Link>
+      <Link to='/apic' style={styleObj} activeStyle={activeStyleObj}>APIC-EM</Link> |
+      <Link to='/ise' style={styleObj} activeStyle={activeStyleObj}>ISE</Link> |
+      <Link to='/about' style={styleObj} activeStyle={activeStyleObj}>About</Link>
 
     </nav>
   </header>
@@ -42,18 +42,18 @@ const MainLayout = ({children}) =>
             <span className="icon-bar"></span>
             <span className="icon-bar"></span>
         </button>
-        <a className="navbar-brand">Frost</a>
+        <a className="navbar-brand" href="#">WebSiteName</a>
       </div>
       <div>
         <div className="collapse navbar-collapse" id="myNavbar">
           <ul className="nav navbar-nav">
-            <li><a>Section 1</a></li>
-            <li><a>Section 2</a></li>
-            <li><a>Section 3</a></li>
-            <li className="dropdown"><a className="dropdown-toggle" dataToggle="dropdown">Section 4 <span className="caret"></span></a>
+            <li><a href="#section1">Section 1</a></li>
+            <li><a href="#section2">Section 2</a></li>
+            <li><a href="#section3">Section 3</a></li>
+            <li className="dropdown"><a className="dropdown-toggle" dataToggle="dropdown" href="#">Section 4 <span className="caret"></span></a>
               <ul className="dropdown-menu">
-                <li><a>Section 4-1</a></li>
-                <li><a>Section 4-2</a></li>
+                <li><a href="#section41">Section 4-1</a></li>
+                <li><a href="#section42">Section 4-2</a></li>
               </ul>
             </li>
           </ul>
