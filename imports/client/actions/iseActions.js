9138883a23cb69ec1c2bc3089060b697d1755148
rohscx <R_E__g_@Macintosh.local>
2017-07-13 08:38:02 -0400

    setup

--- imports/client/actions/iseActions.js
+++ imports/client/actions/iseActions.js
@@ -11,3 +11,41 @@ export function subtractNumber(number) {
     payload: number
   };
 }
+
+export function getRestIse(ticket,sourceIp,destIp) {
+  return dispatch => {
+    apicAPI = 'https://devnetapi.cisco.com/sandbox/apic_em';
+    apicTicket = '/api/v1/ticket';
+    apicFlow = '/api/v1/flow-analysis';
+    apicFlowAnalysisId= '';
+    apicTicketURL = this.apicAPI + this.apicTicket;
+    apicFlowURL = this.apicAPI + this.apicFlow;
+    apicFlowAnalysisIdURL= this.apicAPI + this.apicFlowAnalysisId;
+    apicOptions = {
+      headers: {
+        'content-type': 'application/json',
+        'x-auth-token': ticket
+      },
+      data: {
+        'sourceIP': sourceIp,
+       'destIP': destIp
+     }
+    };
+    return Meteor.call('checkApic', 'POST', apicFlowURL, apicOptions, (err, res) => {
+      if (err) {
+        alert(err);
+      } else {
+          // success!
+          //this.ticket = res.data.response.serviceTicket;
+          //this.apicFlowOptions.headers['x-auth-token'] = res.data.response.serviceTicket;
+          //Session.set("apicTicket", res.data.response.serviceTicket);
+          console.log(res);	// debug
+          console.log(this); // debug
+          //this.makeFlowID();
+          flowId = res.data.response.flowAnalysisId;
+          dispatch(getFlowStatus(ticket, flowId))
+          return dispatch(setFlowId(flowId));
+        }
+    })
+  }
+}
