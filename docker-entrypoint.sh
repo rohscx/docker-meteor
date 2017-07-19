064081a70b7e2770b1ede210154e21cf4328f331
rohscx <R_E__g_@Macintosh.local>
2017-07-18 22:30:53 -0400

    setup

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -15,6 +15,7 @@ METEOR_PROJECT_HOME=/home/meteor/meteor-app
 METEOR_PROJECT_APP=/home/meteor/meteor-app
 METEOR_PROJECT_CD="cd ~/meteor-app"
 METEOR_PROJECT_GIT_PULL="git pull"
+METEOR_PROFILE="export METEOR_PROFILE=100"
 
 
 
@@ -46,6 +47,7 @@ initConfig() {
 start() {
   #sleep ${START_DELAY}
   cd ~/meteor-app
+  ${METEOR_PROFILE}
   meteor --settings settings.json debug > logs/stdout.log 2> logs/stderr.log
 }
 
