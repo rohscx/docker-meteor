7e9b946edcd24805ea04f41faab152d254382022
rohscx <R_E__g_@Macintosh.local>
2017-08-03 17:03:27 -0400

    setup

--- imports/api/request.js
+++ imports/api/request.js
@@ -43,18 +43,7 @@ if (Meteor.isServer) {
     let uName = Meteor.settings.prtgRest.uName;
     let uPass = Meteor.settings.prtgRest.uPass;
     let options = "";
-    checkApic(type, url, options) {
-      this.unblock();
-      try {
-        const result = HTTP.call(type, url, options);
-        // console.log(result); // debug
-        return result;
-      } catch (e) {
-        // Got a network error, timeout, or HTTP error in the 400 or 500 range.
-        console.log(e) // debug
-        return e;
-      }
-    }
+    const result = HTTP.call(type, url, options);
   });
 
 
