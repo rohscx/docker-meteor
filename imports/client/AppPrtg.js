b01ca63a0fe6e4fa861579abf5e93388efe8261e
rohscx <R_E__g_@Macintosh.local>
2017-08-04 19:15:49 -0400

    setup

--- imports/client/AppPrtg.js
+++ imports/client/AppPrtg.js
@@ -76,7 +76,7 @@ export default createContainer(({params}) => {
   let showAll = Session.get('showAll');
   let prtgItemsSub = Meteor.subscribe('prtgDeviceList');
   let prtgArray;
-  prtgArray = ItemsPrtg.find({}, {})
+  prtgArray = ItemsPrtg.find({}, {}).fetch()
   return {
     showAll,
     ready: prtgItemsSub.ready(),
