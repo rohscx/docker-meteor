15dd4f075e1a9560849927e4cc29f507d066d714
rohscx <R_E__g_@Macintosh.local>
2017-08-04 18:59:04 -0400

    setup

--- imports/client/AppPrtg.js
+++ imports/client/AppPrtg.js
@@ -9,7 +9,7 @@ import { Template } from 'meteor/templating';
 import Header from './components/Header';
 import store from './store';
 import { Provider } from 'react-redux';
-import ItemsApic from '../api/prtg';
+import ItemsApic from '../api/request';
 
 
 
