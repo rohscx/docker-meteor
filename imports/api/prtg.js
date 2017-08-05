7050c1d188a68ccce9930f38503b8eb371443d09
rohscx <R_E__g_@Macintosh.local>
2017-08-04 21:18:16 -0400

    setup

--- imports/api/prtg.js
+++ imports/api/prtg.js
@@ -82,9 +82,10 @@ if (Meteor.isServer) {
       //console.log("PUBLISHED KEYS",publishedKeys)
       ItemsPrtg.remove({"prtgData.requestTime": {"$lte" : Math.round(new Date().getTime()/1000 - 30) }})
       newData.sensors.map((data,value) => {
+        data = JSON.parse(data);
         console.log("DOCCCC ",value)
-        console.log("DOCCCC ",newData[value])
-        console.log(typeof('newData[value]'))
+        console.log("DOCCCC ",data)
+        console.log(typeof('data'))
         //console.log("DATA ID ",data._id)
         if (publishedKeys[data._id]) {
           let timeNow = Math.round(new Date().getTime() / 1000);
