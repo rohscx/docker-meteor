1ef4364546e97d0d2f0755b9657a71492cbb3af4
rohscx <rohscx@gmail.com>
2017-08-05 19:45:38 -0400

    Update Devices.js

--- imports/client/components/Prtg/Devices.js
+++ imports/client/components/Prtg/Devices.js
@@ -3,16 +3,59 @@ import Modalfrom 'react-modal';
 import { Session } from 'meteor/session';
 
 export default class Devices extends Component {
-   state = {
-    modalVisible: false,
+  constructor() {
+    super();
+ 
+    this.state = {
+      modalIsOpen: false
+    };
+ 
+    this.openModal = this.openModal.bind(this);
+    this.afterOpenModal = this.afterOpenModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
   }
 
- setModalVisible(visible) {
-    this.setState({modalVisible: visible});
+  openModal() {
+    this.setState({modalIsOpen: true});
   }
+ 
+  afterOpenModal() {
+    // references are now sync'd and can be accessed. 
+    this.subtitle.style.color = '#f00';
+  }
+ 
+  closeModal() {
+    this.setState({modalIsOpen: false});
+  }
+   
+   
 
 
   handleClick(){
+     
+    return (
+            <Modal
+          isOpen={this.state.modalIsOpen}
+          onAfterOpen={this.afterOpenModal}
+          onRequestClose={this.closeModal}
+          style={customStyles}
+          contentLabel="Example Modal"
+        >
+ 
+          <h2 ref={subtitle => this.subtitle = subtitle}>Hello</h2>
+          <button onClick={this.closeModal}>close</button>
+          <div>I am a modal</div>
+          <form>
+            <input />
+            <button>tab navigation</button>
+            <button>stays</button>
+            <button>inside</button>
+            <button>the modal</button>
+          </form>
+        </Modal>
+    
+    
+    )
     return (
       <div id="myModal" className="modal fade" role="dialog">
         <div className="modal-dialog">
