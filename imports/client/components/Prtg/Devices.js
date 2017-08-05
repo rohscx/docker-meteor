02fde2b4b264795543cb5aa6e0e2d30e005febb8
rohscx <rohscx@gmail.com>
2017-08-05 19:57:19 -0400

    Update Devices.js

--- imports/client/components/Prtg/Devices.js
+++ imports/client/components/Prtg/Devices.js
@@ -37,6 +37,16 @@ export default class Devices extends Component {
   }
 
   render() {
+    const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+};
     returnList = this.props.prtgItems.map((data)=>{
       let newData = data.prtgData.dataObj;
       // debug
