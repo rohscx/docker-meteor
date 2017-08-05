b518f57fc31b5ce706979ae9e9a3608887c85c47
rohscx <R_E__g_@Macintosh.local>
2017-08-05 13:51:44 -0400

    setup

--- imports/client/components/Prtg/Devices.js
+++ imports/client/components/Prtg/Devices.js
@@ -4,15 +4,33 @@ import { Session } from 'meteor/session';
 export default class Devices extends Component {
   render() {
     returnList = this.props.prtgItems.map((data)=>{
+      let newData = data.prtgData.dataObj;
       // debug
       console.log(data.prtgData.dataObj);
       //console.log("DB_ID",data["_id"])
-      for (let [key, value] of Object.entries(data.prtgData.dataObj)){
-        console.log(key,value)
-        return (
-          <div className='two-a' key={data._id}><sup>{key}</sup><p >{value}</p></div>
-        );
-      }
+      return (
+        <div key={data._id}>
+          <table className = "table table-striped">
+            <thead>
+              <tr>
+                <th>ACA</th>
+                <th>Device Name</th>
+                <th>Status</th>
+                <th>Sensor</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>{newData.group}</td>
+                <td>{newData.device}</td>
+                <td>{newData.status}</td>
+                <td>{newData.sensor}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+      );
     })
     console.log(this)
     return(
