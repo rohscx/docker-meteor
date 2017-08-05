c2949a3b1398db621d199c70c1e8e5453ec766d8
rohscx <R_E__g_@Macintosh.local>
2017-08-05 13:29:43 -0400

    setup

--- imports/client/components/Prtg/Devices.js
+++ imports/client/components/Prtg/Devices.js
@@ -5,8 +5,8 @@ export default class Devices extends Component {
   render() {
     returnList = this.props.prtgItems.map((data)=>{
       // debug
-      console.log(data);
-      //console.log("adsfadf",data["_id"])
+      console.log(data.prtgData.dataObj);
+      //console.log("DB_ID",data["_id"])
       for (let [key, value] of Object.entries(data.prtgData.dataObj)){
         console.log(key,value)
         return (
