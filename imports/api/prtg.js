c7df67dbaed6114767e642c25bb6c5ce1f3e1536
rohscx <R_E__g_@Macintosh.local>
2017-08-05 15:13:13 -0400

    setup

--- imports/api/prtg.js
+++ imports/api/prtg.js
@@ -115,9 +115,10 @@ if (Meteor.isServer) {
         });
       };
     } else {
+      // gets the current time epoch
       let currentTime = Math.round(new Date().getTime()/1000;
       let oldestDocument = ItemsPrtg.find({},{sort:{"prtgData.requestTime": -1},fields:{"prtgData.requestTime": 1,_id:0},limit:1}).fetch();
-      console.log(oldestDocument[0]prtgData.requestTime);
+      console.log(oldestDocument[0].prtgData.requestTime);
 
 
     }
