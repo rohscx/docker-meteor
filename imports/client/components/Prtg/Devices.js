a1f28a3551337418677dac88c072a71561025ce8
rohscx <R_E__g_@Macintosh.local>
2017-08-05 16:28:16 -0400

    setup

--- imports/client/components/Prtg/Devices.js
+++ imports/client/components/Prtg/Devices.js
@@ -2,31 +2,30 @@ import React, {Component} from 'react';
 import { Session } from 'meteor/session';
 
 export default class Devices extends Component {
-  handClick(){
-  return (
-    <div id="myModal" class="modal fade" role="dialog">
-  <div class="modal-dialog">
+  handleClick(){
+    return (
+      <div id="myModal" className="modal fade" role="dialog">
+        <div className="modal-dialog">
 
-    <!-- Modal content-->
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 class="modal-title">Modal Header</h4>
-      </div>
-      <div class="modal-body">
-        <p>Some text in the modal.</p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-      </div>
-    </div>
+          <!-- Modal content-->
+          <div className="modal-content">
+            <div className="modal-header">
+              <button type="button" className="close" dataDismiss="modal">&times;</button>
+              <h4 className="modal-title">Modal Header</h4>
+            </div>
+            <div className="modal-body">
+              <p>Some text in the modal.</p>
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-default" dataDismiss="modal">Close</button>
+            </div>
+          </div>
 
-  </div>
-</div>
-  )
-  
+        </div>
+      </div>
+    )
   }
-  
+
   render() {
     returnList = this.props.prtgItems.map((data)=>{
       let newData = data.prtgData.dataObj;
@@ -35,7 +34,7 @@ export default class Devices extends Component {
       //console.log("DB_ID",data["_id"])
       return (
         <div key={data._id}>
-          <table className = "table table-striped table-hover table-responsive">
+          <table className = "table table-striped table-hover table-responsive" onClick={handleClick()}>
             <thead>
               <tr>
                 <th>ACA</th>
