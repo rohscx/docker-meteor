ac909da05fb7a693ccb174af1c2c8678f2283815
rohscx <R_E__g_@Macintosh.local>
2017-08-05 19:43:54 -0400

    setup

--- imports/client/components/Prtg/Devices.js
+++ imports/client/components/Prtg/Devices.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {View} from 'react-native';
+import Modalfrom 'react-modal';
 import { Session } from 'meteor/session';
 
 export default class Devices extends Component {
@@ -11,38 +11,6 @@ export default class Devices extends Component {
     this.setState({modalVisible: visible});
   }
 
-  handleClick2(){
-      return (
-      <View style={{marginTop: 22}}>
-        <Modal
-          animationType={"slide"}
-          transparent={false}
-          visible={this.state.modalVisible}
-          onRequestClose={() => {alert("Modal has been closed.")}}
-          >
-         <View style={{marginTop: 22}}>
-          <View>
-            <Text>Hello World!</Text>
-
-            <TouchableHighlight onPress={() => {
-              this.setModalVisible(!this.state.modalVisible)
-            }}>
-              <Text>Hide Modal</Text>
-            </TouchableHighlight>
-
-          </View>
-         </View>
-        </Modal>
-
-        <TouchableHighlight onPress={() => {
-          this.setModalVisible(true)
-        }}>
-          <Text>Show Modal</Text>
-        </TouchableHighlight>
-
-      </View>
-    );
-  }
 
   handleClick(){
     return (
