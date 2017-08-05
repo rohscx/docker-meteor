10758f0ced4104fc042b85bcad53b4ab67f8679f
rohscx <rohscx@gmail.com>
2017-08-05 16:51:36 -0400

    Update Devices.js

--- imports/client/components/Prtg/Devices.js
+++ imports/client/components/Prtg/Devices.js
@@ -2,6 +2,47 @@ import React, {Component} from 'react';
 import { Session } from 'meteor/session';
 
 export default class Devices extends Component {
+   state = {
+    modalVisible: false,
+  }
+
+ setModalVisible(visible) {
+    this.setState({modalVisible: visible});
+  }
+
+  handleCick2(){
+      return (
+      <View style={{marginTop: 22}}>
+        <Modal
+          animationType={"slide"}
+          transparent={false}
+          visible={this.state.modalVisible}
+          onRequestClose={() => {alert("Modal has been closed.")}}
+          >
+         <View style={{marginTop: 22}}>
+          <View>
+            <Text>Hello World!</Text>
+
+            <TouchableHighlight onPress={() => {
+              this.setModalVisible(!this.state.modalVisible)
+            }}>
+              <Text>Hide Modal</Text>
+            </TouchableHighlight>
+
+          </View>
+         </View>
+        </Modal>
+
+        <TouchableHighlight onPress={() => {
+          this.setModalVisible(true)
+        }}>
+          <Text>Show Modal</Text>
+        </TouchableHighlight>
+
+      </View>
+    );
+  }
+
   handleClick(){
     return (
       <div id="myModal" className="modal fade" role="dialog">
