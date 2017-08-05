6a603311fa549e258071e178ea203cb27df4fbf5
rohscx <rohscx@gmail.com>
2017-08-05 16:24:32 -0400

    Update Devices.js

--- imports/client/components/Prtg/Devices.js
+++ imports/client/components/Prtg/Devices.js
@@ -2,6 +2,31 @@ import React, {Component} from 'react';
 import { Session } from 'meteor/session';
 
 export default class Devices extends Component {
+  handClick(){
+  return (
+    <div id="myModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Modal Header</h4>
+      </div>
+      <div class="modal-body">
+        <p>Some text in the modal.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
+</div>
+  )
+  
+  }
+  
   render() {
     returnList = this.props.prtgItems.map((data)=>{
       let newData = data.prtgData.dataObj;
