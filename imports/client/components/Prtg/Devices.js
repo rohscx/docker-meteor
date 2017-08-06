2228239c12493b8df4cd48031bb01a21a5fa60f5
rohscx <R_E__g_@Macintosh.local>
2017-08-05 21:54:16 -0400

    setup

--- imports/client/components/Prtg/Devices.js
+++ imports/client/components/Prtg/Devices.js
@@ -67,10 +67,10 @@ export default class Devices extends Component {
             </thead>
             <tbody>
               <tr>
-                <td>{newData.group}</td>
-                <td>{newData.device}</td>
-                <td>{newData.status}</td>
-                <td>{newData.sensor}</td>
+                <td>{newData.group.trim()}</td>
+                <td>{newData.device.trim()}</td>
+                <td>{newData.status.trim()}</td>
+                <td>{newData.sensor.trim()}</td>
               </tr>
             </tbody>
           </table>
