3f1c92cb291dda5b4d7dbfe83ce30f85f5824028
rohscx <R_E__g_@Macintosh.local>
2017-08-05 20:10:49 -0400

    setup

--- imports/client/components/Prtg/Devices.js
+++ imports/client/components/Prtg/Devices.js
@@ -85,25 +85,19 @@ export default class Devices extends Component {
             isOpen={this.state.modalIsOpen}
             onAfterOpen={this.afterOpenModal}
             onRequestClose={this.closeModal}
+            style={customStyles}
+            contentLabel="Example Modal"
           >
-            <div id="myModal" className="modal fade" role="dialog">
-              <div class="modal-dialog">
-
-                <div className="modal-content">
-                  <div className="modal-header">
-                    <button type="button" class="close" dataDismiss="modal">&times;</button>
-                    <h4 className="modal-title">Modal Header</h4>
-                  </div>
-                  <div className="modal-body">
-                    <p>Some text in the modal.</p>
-                  </div>
-                  <div className="modal-footer">
-                    <button type="button" className="btn btn-default" dataDismiss="modal">Close</button>
-                  </div>
-                </div>
-
-              </div>
-            </div>
+              <h2 ref={subtitle => this.subtitle = subtitle}>Hello</h2>
+              <button onClick={this.closeModal}>close</button>
+              <div>I am a modal</div>
+              <form>
+                <input />
+                <button>tab navigation</button>
+                <button>stays</button>
+                <button>inside</button>
+                <button>the modal</button>
+              </form>
             </Modal>
         </div>
       </div>
