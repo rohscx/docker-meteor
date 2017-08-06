371a037a0fdba0429d28f32dab1c11c0dc1a0428
rohscx <rohscx@gmail.com>
2017-08-05 20:03:22 -0400

    Update Devices.js

--- imports/client/components/Prtg/Devices.js
+++ imports/client/components/Prtg/Devices.js
@@ -105,6 +105,10 @@ export default class Devices extends Component {
   }
 }
 
+<div id="myModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal">&times;</button>
@@ -117,3 +121,6 @@ export default class Devices extends Component {
         <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
       </div>
     </div>
+
+  </div>
+</div>
