54d7f71051723e042a60beb50a417b37f61cda51
rohscx <rohscx@gmail.com>
2017-08-05 20:02:18 -0400

    Update Devices.js

--- imports/client/components/Prtg/Devices.js
+++ imports/client/components/Prtg/Devices.js
@@ -80,7 +80,7 @@ export default class Devices extends Component {
     return(
       <div>
         {returnList}
-        <div >
+        <div className="modal-content">
           <Modal
             isOpen={this.state.modalIsOpen}
             onAfterOpen={this.afterOpenModal}
@@ -104,3 +104,16 @@ export default class Devices extends Component {
     )
   }
 }
+
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Modal Header</h4>
+      </div>
+      <div class="modal-body">
+        <p>Some text in the modal.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
