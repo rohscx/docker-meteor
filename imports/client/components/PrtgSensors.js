1ef9fd637b8df55aa77fd7db62ec8fb3db01f3d7
rohscx <R_E__g_@Macintosh.local>
2017-08-07 17:46:41 -0400

    setup

--- imports/client/components/PrtgSensors.js
+++ imports/client/components/PrtgSensors.js
@@ -100,14 +100,14 @@ class PrtgSensors extends Component {
     return(
       <div>
         <FormGroup controlId="browserInput"
-    bsSize={bsSize} validationState={validationState}>
+  >
   <ControlLabel>Browser</ControlLabel>
   <Autosuggest
     datalist={['Chrome', 'Firefox', 'Internet Explorer', 'Opera', 'Safari']}
     placeholder="What browser do you use?"
     value={browser}
     onChange={onChange}
-    bsSize={bsSize} />
+ />
   {validationState && <FormControl.Feedback />}
   {validationState && <HelpBlock>Please select a browser</HelpBlock>}
 </FormGroup>
