50edea295b61b60f5b191a3087165791325427c1
rohscx <R_E__g_@Macintosh.local>
2017-08-07 15:03:58 -0400

    setup

--- imports/client/components/PrtgSensors.js
+++ imports/client/components/PrtgSensors.js
@@ -2,8 +2,8 @@ import React, {Component} from 'react';
 import { Session } from 'meteor/session';
 import { connect } from 'react-redux';
 import {Accordion, AccordionSection}  from 'redux-accordion';
-import { setName }from '../actions/userActions';
-import { Form, FormGroup, FormControl, ControlLabel, Col, Button, Typeahead, Checkbox, InputGroup } from 'react-bootstrap';
+import { setName } from '../actions/userActions';
+import Select from 'react-select';
 import { hostName, getDevices } from '../actions/prtgActions';
 import Table from './Prtg/Table';
 
@@ -33,24 +33,7 @@ class PrtgSensors extends Component {
     const {submitFormOnEnter} = this.state;
     let   options={firstName:"adsf"};
     return(
-            <form onSubmit={e => alert('Form submitted!')}>
-        <InputGroup>
-          <Typeahead
-            labelKey="name"
-            options={options}
-            placeholder="Choose a state..."
-            submitFormOnEnter={submitFormOnEnter}
-          />
-          <InputGroup.Button>
-            <Button type="submit">Submit</Button>
-          </InputGroup.Button>
-        </InputGroup>
-        <Checkbox
-          checked={submitFormOnEnter}
-          onChange={e => this.setState({submitFormOnEnter: e.target.checked})}>
-          Allow form submission
-        </Checkbox>
-      </form>
+      
     )
   }
 }
