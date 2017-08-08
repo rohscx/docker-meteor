d798ec8dd19b06fd5b976a90b1d5a00ecc24afcd
rohscx <R_E__g_@Macintosh.local>
2017-08-08 17:55:56 -0400

    setup

--- imports/client/components/Prtg/Table.js
+++ imports/client/components/Prtg/Table.js
@@ -43,10 +43,12 @@ export default class Table extends Component {
         }
       }
       return (
-        `<td>{newData.group}</td>
+        <div>
+        <td>{newData.group}</td>
         <td>{newData.device}</td>
         <td className={statusDanger()}> {newData.status}</td>
-        <td>{newData.sensor}</td>`
+        <td>{newData.sensor}</td>
+      </div>
       )
     })
     return (
