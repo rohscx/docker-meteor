f4ffefcd79701f3820bf9eea92f474c37f58a879
rohscx <R_E__g_@Macintosh.local>
2017-08-08 11:08:57 -0400

    setup

--- imports/client/AppPrtgSensors.js
+++ imports/client/AppPrtgSensors.js
@@ -76,7 +76,7 @@ store.subscribe(() => {
 export default createContainer(({params}) => {
   let userSub = Meteor.subscribe('currentUser');
   let showAll = Session.get('showAll');
-  let prtgItemsSub = Meteor.subscribe('prtgDeviceList');
+  //let prtgItemsSub = Meteor.subscribe('prtgDeviceList');
   let prtgArray = Session.get('myMethodResult');
   let prtgDeviceNamesArray = Session.get('deviceNameArray');
   Meteor.call('getPrtgData', function(err,res){
