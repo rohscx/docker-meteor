965491d9e80626648acbb6badb1ed6d8d573afd7
rohscx <R_E__g_@Macintosh.local>
2017-08-08 15:29:17 -0400

    setup

--- imports/client/AppPrtgSensors.js
+++ imports/client/AppPrtgSensors.js
@@ -68,7 +68,7 @@ store.subscribe(() => {
               Show {this.props.showAll ? 'None': 'All'}
             </button>
           </IsRole>
-          <Header  {... this.state dbSearch.bind(this)}/>
+          <Header  {dbSearch.bind(this)}/>
           <PrtgSensors {... this.props}/>
         </main>
       </Provider>
