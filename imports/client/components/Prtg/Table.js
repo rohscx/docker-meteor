ccd0b41290df5a74e01bcf43c1b1cd620a9e48f8
rohscx <R_E__g_@Macintosh.local>
2017-08-08 18:00:45 -0400

    setup

--- imports/client/components/Prtg/Table.js
+++ imports/client/components/Prtg/Table.js
@@ -43,7 +43,7 @@ export default class Table extends Component {
         }
       }
       return (
-        <tr key={data._id} onClick={()=>{this.openModal({newData})}>
+        <tr key={data._id} onClick={()=>{this.openModal({newData})}}>
         <td>{newData.group}</td>
         <td>{newData.device}</td>
         <td className={statusDanger()}> {newData.status}</td>
@@ -53,7 +53,7 @@ export default class Table extends Component {
     })
     return (
       <div key={12}>
-        <table className = "table table-striped table-hover table-responsive"}>
+        <table className = "table table-striped table-hover table-responsive">
           <thead>
             <tr>
               <th>ACA</th>
