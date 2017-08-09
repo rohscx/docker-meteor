6128073e8b333a3b26e2e064ed8d7cc248ff2a2f
rohscx <R_E__g_@Macintosh.local>
2017-08-09 14:34:44 -0400

    setup

--- imports/client/components/TransferRate.js
+++ imports/client/components/TransferRate.js
@@ -22,6 +22,83 @@ class TransferRate extends Component {
     this.props.getDevices();
   }
 
+  handleSearchFormInput(event) {
+    let value = event.target.value
+    console.log(value)
+    this.props.dbSearch(value);
+    this.props.hostName(value);
+  }
+
+  preventDefault(e){
+    e.preventDefault();
+  }
+
+  getDevices(){
+    this.props.getDevices();
+  }
+
+  prtgSearchForm(){
+    const btnEnabled = () => {
+      return (
+        <Col smOffset={2} sm={10}><Button type="button" bsStyle="primary" block>Submit</Button></Col>
+      );
+    };
+    const btnDisabled = () => {
+      return (
+        <Col smOffset={2} sm={10}><Button  type="button" disabled block> <b> . . . </b></Button></Col>
+      );
+    };
+    const hostName = () => {
+      return (
+        this.props.util.hostName.name
+      );
+    };
+    const formInput = () => {
+      return (
+        this.handleSearchFormInput.bind(this)
+      );
+    };
+    const validationStatus = () => {
+      let dataCount = this.props.dbReturn.length;
+      let dataReady = this.props.dbReturnRdy;
+      if(dataReady === false){
+        return null;
+      } else if(dataReady === true && dataCount <= 0){
+        return "error";
+      } else{
+        return "success";
+      }
+      /*
+      return (
+        this.props.util.hostName.validationStatus
+      );*/
+    };
+
+    const divStyles = {
+      margin: "auto",
+      width: "40%"
+    };
+
+    return (
+      <div style={divStyles}>
+        <form onSubmit= {e =>{this.preventDefault(e)}}>
+          <FormGroup controlId="formHorizontalHost" validationState={validationStatus()}>
+            <Col componentClass={ControlLabel} sm={2}>
+              Search
+            </Col>
+            <Col sm={10}>
+              <FormControl type="text" value={hostName()} placeholder="Host Name" onChange={formInput()} />
+              <FormControl.Feedback />
+            </Col>
+          </FormGroup>
+          <FormGroup>
+
+          </FormGroup>
+        </form>
+      </div>
+    );
+  }
+
 
   render() {
     const divStyles = {
---
+++ imports/client/components/TransferRate/Table.js
@@ -0,0 +1,112 @@
+import React, {Component} from 'react';
+import Modal from 'react-modal';
+import { Session } from 'meteor/session';
+
+export default class Table extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      modalIsOpen: false,
+      modalLink: {newData:{graph:false}}
+    };
+
+    this.openModal = this.openModal.bind(this);
+    this.afterOpenModal = this.afterOpenModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+
+  openModal(link) {
+    this.setState({modalIsOpen: true});
+    this.setState({modalLink: link});
+    // debug
+    //console.log(this.state.modalLink);
+  }
+
+  afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    //this.subtitle.style.color = '#f00';
+  }
+
+  closeModal() {
+    this.setState({modalIsOpen: false});
+  }
+
+  returnList() {
+    let trGenerator = this.props.dbReturn.map((data)=>{
+      let newData = data.siteData.dataObj;
+      let statusDanger = () =>{
+        if(!newData){
+          return 'danger';
+        } else {
+          return 'success';
+        }
+      }
+      return (
+        <tr key={data._id} onClick={()=>{this.openModal({newData})}}>
+        <td>{newData.aca}</td>
+        <td>{newData.branch}</td>
+        <td className={statusDanger()}> {newData.wPortSpeed}</td>
+        <td>{newData.wPortType}</td>
+      </tr>
+      )
+    })
+    return (
+      <div key={12}>
+        <table className = "table table-striped table-hover table-responsive">
+          <thead className="thead-default">
+            <tr>
+              <th>ACA</th>
+              <th>Branch</th>
+              <th>WAN Speed</th>
+              <th>WAN TYPE</th>
+            </tr>
+          </thead>
+          <tbody>
+            {trGenerator}
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+
+
+
+  render() {
+    const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    height                : '300px',
+    width                 : '920px',
+    transform             : 'translate(-50%, -50%)',
+    background            : `url(${this.state.modalLink.newData.graph})`,
+    backgroundSize        : 'contain',
+    opacity               : '100'
+  }
+
+};
+
+
+    tableDiv = this.props.dbReturnRdy ? this.returnList() : "";
+    console.log(this)
+    return(
+      <div>
+        {tableDiv}
+        <div className="modal modal-content modal-responsive">
+          <Modal
+            isOpen={this.state.modalIsOpen}
+            onAfterOpen={this.afterOpenModal}
+            onRequestClose={this.closeModal}
+            style={customStyles}
+            contentLabel="PRTG Modal"
+          >
+          </Modal>
+        </div>
+      </div>
+    )
+  }
+}
