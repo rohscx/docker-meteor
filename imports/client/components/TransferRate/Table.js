0e30c3ab85eab5acb0e197d1a7457949cf644ac0
rohscx <R_E__g_@Macintosh.local>
2017-08-09 18:32:34 -0400

    setup

--- imports/client/components/TransferRate/Table.js
+++ imports/client/components/TransferRate/Table.js
@@ -45,11 +45,11 @@ export default class Table extends Component {
             let NaNCheck = isNaN(inSeconds);
             if (inSeconds <= 60){
               return (<td>{NaNCheck ? "n/a" : inSeconds} {NaNCheck ? "n/a" : " Seconds"}</td>)
-            } else if (inSeconds <= 60){
+            } else if (inSeconds == 60 && < 3600){
               return (<td>{NaNCheck ? "n/a" : inSeconds/60}{NaNCheck ? "n/a" : " Minutes"}</td>)
-            } else if (inSeconds <= 3600){
+            } else if (inSeconds == 3600 && < 86400){
               return (<td>{NaNCheck ? "n/a" : inSeconds/3600}{NaNCheck ? "n/a" : " Hours"}</td>)
-            } else if (inSeconds >= 86400 || inSeconds > 3600){
+            } else if (inSeconds >= 86400){
               return (<td>{NaNCheck ? "n/a" : inSeconds/86400}{NaNCheck ? "n/a" : " Days"}</td>)
             }
             break;
