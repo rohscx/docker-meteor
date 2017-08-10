c758a05851393e1db89e8e848ab1276759796652
rohscx <R_E__g_@Macintosh.local>
2017-08-10 11:15:14 -0400

    setup

--- imports/client/components/TransferRate.js
+++ imports/client/components/TransferRate.js
@@ -91,7 +91,7 @@ class TransferRate extends Component {
                <DropdownButton
                  componentClass={InputGroup.Button}
                  id="input-dropdown-addon"
-                 title={this.byteType}
+                 title={this.byteType.bind(this)}
                >
                  <MenuItem key="1" onSelect= {()=>{this.setByteType("MB")}}>MB</MenuItem>
                  <MenuItem key="2" onSelect= {()=>{this.setByteType("GB")}}>GB</MenuItem>
