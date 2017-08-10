cd472e32a6bf08ce574ea1bca5b0401a23a248a0
rohscx <R_E__g_@Macintosh.local>
2017-08-10 12:23:31 -0400

    setup

--- imports/client/components/TransferRate/Table.js
+++ imports/client/components/TransferRate/Table.js
@@ -56,7 +56,7 @@ export default class Table extends Component {
           case "GB":
             // debug
             //console.log(dataSize,"  ",portSpeed)
-            let inSeconds = ((dataSize * 1024) / portSpeed);
+            let inSeconds = (dataSize * 1024) / portSpeed;
             let NaNCheck = isNaN(inSeconds);
             if (inSeconds <= 60){
               return (<td className={statusDanger(Math.round(inSeconds))}>{NaNCheck ? "n/a" : Math.round(inSeconds)} {NaNCheck ? "n/a" : " Seconds"}</td>)
