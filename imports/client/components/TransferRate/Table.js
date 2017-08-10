40ffb5842eb4850321eb7c3171fec400ae167b9d
rohscx <R_E__g_@Macintosh.local>
2017-08-10 14:04:00 -0400

    setup

--- imports/client/components/TransferRate/Table.js
+++ imports/client/components/TransferRate/Table.js
@@ -97,7 +97,7 @@ export default class Table extends Component {
               return (<td className={statusDanger(Math.round(inSeconds))}>{NaNCheck ? "n/a" : Math.round(inSeconds/60)}{NaNCheck ? "n/a" : " Minutes"}</td>)
             } else if (inSeconds > 3600 && inSeconds < 86400){
               return (<td className={statusDanger(Math.round(inSeconds))}>{NaNCheck ? "n/a" : Math.round(inSeconds/3600)}{NaNCheck ? "n/a" : " Hours"}</td>)
-            } else if (inSeconds >= 86400){
+            } else if (inSeconds > 86400 && inSeconds < 31540000000){
               return (<td className={statusDanger(Math.round(inSeconds))}>{NaNCheck ? "n/a" : Math.round(inSeconds/86400)}{NaNCheck ? "n/a" : " Days"}</td>)
             } else {
               return (<td className={statusDanger(Math.round(inSeconds))}>{NaNCheck ? "n/a" : Math.round(inSeconds/31540000000)}{NaNCheck ? "n/a" : " Years"}</td>)
@@ -114,11 +114,11 @@ export default class Table extends Component {
               return (<td className={statusDanger(Math.round(inSeconds))}>{NaNCheck ? "n/a" : Math.round(inSeconds/60)}{NaNCheck ? "n/a" : " Minutes"}</td>)
             } else if (inSeconds > 3600 && inSeconds < 86400){
               return (<td className={statusDanger(Math.round(inSeconds))}>{NaNCheck ? "n/a" : Math.round(inSeconds/3600)}{NaNCheck ? "n/a" : " Hours"}</td>)
-            } else if (inSeconds >= 86400){
+            } else if (inSeconds > 86400 && inSeconds < 31540000000){
               return (<td className={statusDanger(Math.round(inSeconds))}>{NaNCheck ? "n/a" : Math.round(inSeconds/86400)}{NaNCheck ? "n/a" : " Days"}</td>)
             } else {
               return (<td className={statusDanger(Math.round(inSeconds))}>{NaNCheck ? "n/a" : Math.round(inSeconds/31540000000)}{NaNCheck ? "n/a" : " Years"}</td>)
-            } 
+            }
             break;
           default:
         }
