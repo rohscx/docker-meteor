644850c4a629c91feef73ea8f7899e442af38686
rohscx <R_E__g_@Macintosh.local>
2017-08-11 09:40:48 -0400

    setup

--- imports/client/layouts/MainLayout.js
+++ imports/client/layouts/MainLayout.js
@@ -27,28 +27,7 @@ const MainLayout = ({children}) =>
 */
 
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar w/ text</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarText">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Features</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
-      </li>
-    </ul>
-    <span class="navbar-text">
-      Navbar text with an inline element
-    </span>
-  </div>
-</nav>
+
 
 
 
@@ -60,54 +39,30 @@ const divStyles = {
 const MainLayout = ({children}) =>
   <div>
     <header>
-      <LinkContainer to='/'>
-      <Navbar inverse collapseOnSelect>
-        <Navbar.Header>
-          <Navbar.Brand>
-            FROST
-          </Navbar.Brand>
-          <Navbar.Toggle />
-        </Navbar.Header>
-        <Navbar.Collapse>
-          <Nav>
-            <LinkContainer to='/apic'>
-              <NavItem eventKey={1}>
-                APIC-EM
-              </NavItem>
-            </LinkContainer>
-            <LinkContainer to='/ise'>
-            <NavItem eventKey={2}>
-              ISE
-            </NavItem>
-            </LinkContainer>
-            <LinkContainer to='/prtg'>
-            <NavItem eventKey={3}>
-              PRTG
-            </NavItem>
-            </LinkContainer>
-            <NavDropdown eventKey={10} title="Util" id="basic-nav-dropdown">
-              <LinkContainer to='/trfr'>
-                <MenuItem eventKey={10.1}>TRANSF..RATE</MenuItem>
-              </LinkContainer>
-              <MenuItem eventKey={10.2}>Another action</MenuItem>
-              <MenuItem eventKey={10.3}>Something else here</MenuItem>
-              <MenuItem divider />
-              <MenuItem eventKey={10.3} >Separated link</MenuItem>
-            </NavDropdown>
-          </Nav>
-          <Nav pullRight>
-            <NavItem eventKey={1} >
-              <LoginButtons/>
-            </NavItem>
-            <LinkContainer to='/about'>
-            <NavItem eventKey={2}>
-              About
-            </NavItem>
-            </LinkContainer>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-      </LinkContainer>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <h1 className="navbar-brand">
+          <Link to='/' style={styleObj} activeStyle={activeStyleObj}>FROST</Link>
+        </h1>
+        <button className="navbar-toggler" type="button" dataToggle="collapse" dataTarget="#navbarText" ariaControls="navbarText" ariaCxpanded="false" ariaLabel="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarText">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Features</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Pricing</a>
+            </li>
+          </ul>
+          <span className="navbar-text">
+            Navbar text with an inline element
+          </span>
+        </div>
+      </nav>
     </header>
     <div style={divStyles}>
       {children}
