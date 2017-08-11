20e39de2487eb65b44447af712273ffb2a889eaf
rohscx <R_E__g_@Macintosh.local>
2017-08-11 10:21:33 -0400

    setup

--- server/tempData.js
+++ server/tempData.js
@@ -1,14 +1,19 @@
 module.exports = {
   tempData0: () => {
+    let data = "";
+    return data;
 
   },
   tempData1: () => {
-
+    let data = "";
+    return data;
   },
   tempData2: () => {
-
+    let data = "";
+    return data;
   },
   tempData3: () => {
-
+    let data = "";
+    return data;
   }
 }
