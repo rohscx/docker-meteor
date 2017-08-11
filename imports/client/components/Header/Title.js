a542584d2421db54e845db9969b71c41fdd7d182
rohscx <R_E__g_@Macintosh.local>
2017-08-11 11:25:13 -0400

    setup

--- imports/client/components/Header/Title.js
+++ imports/client/components/Header/Title.js
@@ -2,10 +2,13 @@ import React, {Component} from 'react';
 import { Session } from 'meteor/session';
 
 export default class Title extends Component {
-
+  const divStyles = {
+    fontSize: "2em",
+    margin: "0.67em 0"
+  };
   render() {
     return(
-      <div>
+      <div style={divStyles}>
         <h1>{this.props.greeting}</h1>
 
       </div>
