939b142de413b8bddde4faa81ba5e1ac991322c8
rohscx <R_E__g_@Macintosh.local>
2017-08-12 11:43:56 -0400

    setup

--- imports/client/components/TransferRate/Table.js
+++ imports/client/components/TransferRate/Table.js
@@ -36,7 +36,9 @@ export default class Table extends Component {
   returnList() {
     let inBits = this.props.util.bandwidthCalcData.numberToBits;
     let byteType = this.props.util.bandwidthCalcData.byteType;
-    let anothertest = this.props.dbReturn("aca");
+    let anothertest = this.props.dbReturn("aca", (response)=>{
+      return response;
+    });
     let test = anothertest.map((data)=>{
       let transTimeSeconds = (dataSize, dataType, portSpeed)=>{
         let inSeconds, NaNCheck
