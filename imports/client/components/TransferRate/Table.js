1aeb6942de5787c98d7c5bd3222b4ba3df0b2d9a
rohscx <R_E__g_@Macintosh.local>
2017-08-12 14:52:23 -0400

    setup

--- imports/client/components/TransferRate/Table.js
+++ imports/client/components/TransferRate/Table.js
@@ -179,12 +179,12 @@ export default class Table extends Component {
         <table className = "table table-striped table-hover table-responsive">
           <thead className="thead-default">
             <tr>
-              <th onClick={()=>{setMan(this.props.sortBy("aca"))}}>ACA</th>
-              <th onClick={()=>{setMan(this.props.sortBy("branch"))}}>Branch</th>
-              <th onClick={()=>{setMan(this.props.sortBy("wPortSpeed"))}}>WAN Speed</th>
-              <th onClick={()=>{setMan(this.props.sortBy("wPortType"))}}>WAN TYPE</th>
-              <th onClick={()=>{setMan(this.props.sortBy("iSpeedUp"))}}>INET Speed Down/Up</th>
-              <th onClick={()=>{setMan(this.props.sortBy("iPortType"))}}>INET TYPE</th>
+              <th onClick={()=>{this.props.sortBy("aca")}}>ACA</th>
+              <th onClick={()=>{this.props.sortBy("branch")}}>Branch</th>
+              <th onClick={()=>{this.props.sortBy("wPortSpeed")}}>WAN Speed</th>
+              <th onClick={()=>{this.props.sortBy("wPortType")}>WAN TYPE</th>
+              <th onClick={()=>{this.props.sortBy("iSpeedUp")}}>INET Speed Down/Up</th>
+              <th onClick={()=>{this.props.sortBy("iPortType")}}>INET TYPE</th>
               {inBits ? <th>R1 WAN Speed Down/Up Time</th> : null}
               {inBits ? <th>R2 INET Speed Down Time</th> : null}
               {inBits ? <th>R2 INET Speed Up Time</th> : null}
