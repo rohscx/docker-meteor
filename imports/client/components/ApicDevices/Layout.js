8858e09a59f4a01267b168c36085e7a4e3914f3f
rohscx <R_E__g_@Macintosh.local>
2017-08-14 11:01:37 -0400

    setup

--- imports/client/components/ApicDevices/Layout.js
+++ imports/client/components/ApicDevices/Layout.js
@@ -53,7 +53,7 @@ export default class Table extends Component {
             <Col xs={6} md={1}>{data.siteData.dataObj.managementIpAddress}</Col>
             <Col xs={6} md={1}>{data.siteData.dataObj.reachabilityStatus}</Col>
             <Col xs={6} md={1}>{data.siteData.dataObj.softwareVersion}</Col>
-            <Col xs={6} md={1}>{data.siteData.dataObj.upTime}</Col>
+            <Col xs={6} md={2}>{data.siteData.dataObj.upTime}</Col>
             <Col xs={6} md={4}>{data.siteData.dataObj.series}</Col>
             <Col xs={6} md={1}>{data.siteData.dataObj.interfaceCount}</Col>
           </Row>
