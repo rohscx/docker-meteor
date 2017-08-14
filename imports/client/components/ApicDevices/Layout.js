d56abda916bbed77c906f733152210620ef29b00
rohscx <R_E__g_@Macintosh.local>
2017-08-14 17:22:59 -0400

    setup

--- imports/client/components/ApicDevices/Layout.js
+++ imports/client/components/ApicDevices/Layout.js
@@ -112,11 +112,11 @@ export default class Table extends Component {
     };
     timeout = setTimeout (function(){this.returnLayout()},500)
     //tableDiv = this.props.dbReturnRdy ? this.returnList() : "";
-    tableDiv = this.props.apic.apicDevicesFind.validationStatus ? this.returnLayout() : "";
+    //tableDiv = this.props.apic.apicDevicesFind.validationStatus ? this.returnLayout() : "";
     //console.log(this)
     return(
       <div>
-        {tableDiv}
+        {timout}
       </div>
     )
   }
