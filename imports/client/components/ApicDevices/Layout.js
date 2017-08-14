e2929ce2faffa3797535315a7a7ae2d5639242ad
rohscx <R_E__g_@Macintosh.local>
2017-08-14 11:29:19 -0400

    setup

--- imports/client/components/ApicDevices/Layout.js
+++ imports/client/components/ApicDevices/Layout.js
@@ -57,11 +57,9 @@ export default class Table extends Component {
             <Col xs={6} md={1}>{data.siteData.dataObj.managementIpAddress}</Col>
             <Col xs={6} md={1}>{data.siteData.dataObj.reachabilityStatus}</Col>
             <Col xs={6} md={2}>Ver: {data.siteData.dataObj.softwareVersion}</Col>
-            <Col xs={6} md={2}>Up Time: {data.siteData.dataObj.upTime}</Col>
+            <Col xs={6} md={3}>Up Time: {data.siteData.dataObj.upTime}</Col>
             <Col xs={6} md={4}>{data.siteData.dataObj.series}</Col>
-            <Clearfix visibleXsBlock><code>&lt;{'Clearfix visibleSmBlock MD'} /&gt;</code></Clearfix>
-            <Clearfix visibleMdBlock><code>&lt;{'Clearfix visibleSmBlockXS'} /&gt;</code></Clearfix>
-            <Col xs={6} md={1}>Interface Count: {data.siteData.dataObj.interfaceCount}</Col>
+            <Col xs={6} md={4}>Interface Count: {data.siteData.dataObj.interfaceCount}</Col>
             <Col xs={6} md={4}>Serial Number: {data.siteData.dataObj.serialNumber}</Col>
           </Row>
         </div>
