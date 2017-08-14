1c1e1eac1e3321608b2e00c0eb1d1d6d515d2e1d
rohscx <R_E__g_@Macintosh.local>
2017-08-14 16:09:47 -0400

    setup

--- imports/client/components/ApicDevices/Layout.js
+++ imports/client/components/ApicDevices/Layout.js
@@ -108,7 +108,10 @@ export default class Table extends Component {
 
 
     //tableDiv = this.props.dbReturnRdy ? this.returnList() : "";
-    tableDiv = this.props.apic.apicDevicesFind.validationStatus ? this.returnLayout((response)=>{return response}) : "";
+    tableDiv = this.props.apic.apicDevicesFind.validationStatus ? this.returnLayout((response)=>{
+      console.log(response)
+      return response}
+    ) : "";
     //console.log(this)
     return(
       <div>
