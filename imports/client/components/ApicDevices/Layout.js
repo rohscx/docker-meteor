2a437afe3acc25a0f4a0e156964d8f36de420d08
rohscx <R_E__g_@Macintosh.local>
2017-08-14 10:21:20 -0400

    setup

--- imports/client/components/ApicDevices/Layout.js
+++ imports/client/components/ApicDevices/Layout.js
@@ -39,7 +39,7 @@ export default class Table extends Component {
     let sortField = this.props.apic.sortBy.field;
     let sortOrderField = this.props.apic.sortBy.order;
     let dbData = this.props.dbReturn(findField,sortField,sortOrderField);
-    let return = dbData.map((data)=>{
+    return = dbData.map((data)=>{
       return (
         <div>
           <Row className="show-grid">
