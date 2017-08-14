ef1c83927895d32bb938d1474a788f01cb805ce3
rohscx <R_E__g_@Macintosh.local>
2017-08-14 09:39:28 -0400

    setup

--- imports/client/components/ApicDevices/Layout.js
+++ imports/client/components/ApicDevices/Layout.js
@@ -37,7 +37,11 @@ export default class Table extends Component {
   returnLayout() {
     return (
       <Row className="show-grid">
-      <Col xs={6} xsOffset={6}><code>&lt;{'Col xs={6} xsOffset={6}'} /&gt;</code></Col>
+        <Col xs={6} xsOffset={6}><code>&lt;{'Col xs={6} xsOffset={6}'} /&gt;</code></Col>
+      </Row>
+      <Row className="show-grid">
+        <Col md={6} mdPush={6}><code>&lt;{'Col md={6} mdPush={6}'} /&gt;</code></Col>
+        <Col md={6} mdPull={6}><code>&lt;{'Col md={6} mdPull={6}'} /&gt;</code></Col>
       </Row>
     )
   }
