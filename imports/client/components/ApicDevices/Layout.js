bfb08843443e16313516dadb86716ef531fc45c9
rohscx <R_E__g_@Macintosh.local>
2017-08-15 15:09:32 -0400

    setup

--- imports/client/components/ApicDevices/Layout.js
+++ imports/client/components/ApicDevices/Layout.js
@@ -85,11 +85,11 @@ export default class Table extends Component {
             </Row>
             <Row className="show-grid">
               <Col xs={5} sm={6} md={3} lg={1}><a href={sshLinkGen(mgmtIpAddress)}>{mgmtIpAddress}</a></Col>
-              <Col xs={6} sm={6} md={3} lg={1}>  {reachCheck(status)}  </Col>
-              <Col xs={6} sm={6} md={2} lg={1}>Ver: {data.siteData.dataObj.softwareVersion}</Col>
+              <Col xs={6} sm={6} md={3} lg={2}>  {reachCheck(status)}  </Col>
+              <Col xs={6} sm={6} md={2} lg={2}>Ver: {data.siteData.dataObj.softwareVersion}</Col>
               <Col xs={6} sm={6} md={4} lg={3}>Up Time: {data.siteData.dataObj.upTime}</Col>
-              <Col xs={6} sm={6} md={2} lg={2}>Int#: {data.siteData.dataObj.interfaceCount}</Col>
-              <Col xs={6} sm={6} md={5} lg={5}>{data.siteData.dataObj.series}</Col>
+              <Col xs={6} sm={6} md={2} lg={1}>Int#: {data.siteData.dataObj.interfaceCount}</Col>
+              <Col xs={6} sm={6} md={5} lg={4}>{data.siteData.dataObj.series}</Col>
               <Col xs={6} sm={6} md={1} lg={1}>{data.siteData.dataObj.serialNumber}</Col>
             </Row>
           </div>
