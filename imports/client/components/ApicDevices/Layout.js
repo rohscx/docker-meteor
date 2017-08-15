744641d21874ab76cf084adab44da4896449c69d
rohscx <R_E__g_@Macintosh.local>
2017-08-15 15:32:05 -0400

    setup

--- imports/client/components/ApicDevices/Layout.js
+++ imports/client/components/ApicDevices/Layout.js
@@ -90,7 +90,7 @@ export default class Table extends Component {
               <Col xs={6} sm={6} md={3}>Up Time: {data.siteData.dataObj.upTime}</Col>
               <Col xs={6} sm={6} md={1}>Int#: {data.siteData.dataObj.interfaceCount}</Col>
               <Col xs={6} sm={6} md={1}>{data.siteData.dataObj.serialNumber}</Col>
-              <Col xs={6} sm={6} md={3}>{data.siteData.dataObj.series}</Col>
+              <Col xs={6} sm={6} md={4}>{data.siteData.dataObj.series}</Col>
             </Row>
           </div>
         )
