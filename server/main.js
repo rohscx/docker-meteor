4a327ad09547be2701a185cbde4f2e718b4e35ca
rohscx <R_E__g_@Macintosh.local>
2017-08-14 23:56:56 -0400

    setup

--- server/main.js
+++ server/main.js
@@ -85,7 +85,8 @@ Meteor.publish('apicDevices', function() {
       "siteData.dataObj.interfaceCount":1,
       "siteData.dataObj.series":1,
       "siteData.dataObj.serialNumber":1,
-      "siteData.dataObj.reachabilityStatus":1
+      "siteData.dataObj.reachabilityStatus":1,
+      "siteData.dataObj.normalizeHostName":1
     }
      })
   } else {
@@ -100,7 +101,8 @@ Meteor.publish('apicDevices', function() {
       "siteData.dataObj.interfaceCount":1,
       "siteData.dataObj.series":1,
       "siteData.dataObj.serialNumber":1,
-      "siteData.dataObj.reachabilityStatus":1
+      "siteData.dataObj.reachabilityStatus":1,
+      "siteData.dataObj.normalizeHostName":1
     }
      })
   }
