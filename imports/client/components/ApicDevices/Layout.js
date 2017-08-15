c54aad8c7ae748f1f929bf912d34842f6db96ce8
rohscx <R_E__g_@Macintosh.local>
2017-08-15 14:28:22 -0400

    setup

--- imports/client/components/ApicDevices/Layout.js
+++ imports/client/components/ApicDevices/Layout.js
@@ -79,18 +79,18 @@ export default class Table extends Component {
         return (
           <div key={data["_id"]} style= {divStyles}>
             <Row className="show-grid" style={rowStylesMain}>
-              <Col xs={6} sm={6} md={3}>{data.siteData.dataObj.hostname}</Col>
-              <Col xs={4} sm={4} md={2}>{data.siteData.dataObj.role}</Col>
-              <Col xs={2} sm={2} md={3}>Updated @ UTC {data.siteData.dataObj.lastUpdated}</Col>
+              <Col xs={6} sm={6} md={3} lg={6}>{data.siteData.dataObj.hostname}</Col>
+              <Col xs={6} sm={6} md={2} lg={6}>{data.siteData.dataObj.role}</Col>
+              <Col xs={6} sm={6} md={3} lg={6}>Updated @ UTC {data.siteData.dataObj.lastUpdated}</Col>
             </Row>
             <Row className="show-grid">
-              <Col xs={5} sm={5} md={1}><a href={sshLinkGen(mgmtIpAddress)}>{mgmtIpAddress}</a></Col>
-              <Col xs={1} sm={1} md={1}>  {reachCheck(status)}  </Col>
-              <Col xs={1} sm={1} md={2}>Ver: {data.siteData.dataObj.softwareVersion}</Col>
-              <Col xs={1} sm={1} md={2}>Up Time: {data.siteData.dataObj.upTime}</Col>
-              <Col xs={1} sm={1} md={1}>Int#: {data.siteData.dataObj.interfaceCount}</Col>
-              <Col xs={1} sm={1} md={4}>{data.siteData.dataObj.series}</Col>
-              <Col xs={1} sm={1} md={1}>{data.siteData.dataObj.serialNumber}</Col>
+              <Col xs={5} sm={6} md={1} lg={6}><a href={sshLinkGen(mgmtIpAddress)}>{mgmtIpAddress}</a></Col>
+              <Col xs={6} sm={6} md={1} lg={6}>  {reachCheck(status)}  </Col>
+              <Col xs={6} sm={6} md={2} lg={6}>Ver: {data.siteData.dataObj.softwareVersion}</Col>
+              <Col xs={6} sm={6} md={2} lg={6}>Up Time: {data.siteData.dataObj.upTime}</Col>
+              <Col xs={6} sm={6} md={1} lg={6}>Int#: {data.siteData.dataObj.interfaceCount}</Col>
+              <Col xs={6} sm={6} md={4} lg={6}>{data.siteData.dataObj.series}</Col>
+              <Col xs={6} sm={6} md={1} lg={6}>{data.siteData.dataObj.serialNumber}</Col>
             </Row>
           </div>
         )
