0356912b023b9e4bdb80d2f5ba7a57bf2bd22b11
rohscx <R_E__g_@Macintosh.local>
2017-08-15 14:51:13 -0400

    setup

--- imports/client/components/ApicDevices/Layout.js
+++ imports/client/components/ApicDevices/Layout.js
@@ -88,8 +88,8 @@ export default class Table extends Component {
               <Col xs={6} sm={6} md={3} lg={2}>  {reachCheck(status)}  </Col>
               <Col xs={6} sm={6} md={3} lg={4}>Ver: {data.siteData.dataObj.softwareVersion}</Col>
               <Col xs={6} sm={6} md={4} lg={2}>Up Time: {data.siteData.dataObj.upTime}</Col>
-              <Col xs={6} sm={6} md={1} lg={1}>Int#: {data.siteData.dataObj.interfaceCount}</Col>
-              <Col xs={6} sm={6} md={5} lg={4}>{data.siteData.dataObj.series}</Col>
+              <Col xs={6} sm={6} md={1} lg={2}>Int#: {data.siteData.dataObj.interfaceCount}</Col>
+              <Col xs={6} sm={6} md={5} lg={5}>{data.siteData.dataObj.series}</Col>
               <Col xs={6} sm={6} md={1} lg={1}>{data.siteData.dataObj.serialNumber}</Col>
             </Row>
           </div>
