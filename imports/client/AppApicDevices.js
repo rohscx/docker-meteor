f565dc41f72e4ce295bf7a9d367277d59586184b
rohscx <R_E__g_@Macintosh.local>
2017-08-16 11:51:32 -0400

    setup

--- imports/client/AppApicDevices.js
+++ imports/client/AppApicDevices.js
@@ -87,9 +87,7 @@ ItemsApicDevices.deny({
 export default createContainer(({params}) => {
   let userSub = Meteor.subscribe('currentUser');
   let showAll = Session.get('showAll');
-  let aicDevicesItemsSub = (hostName)=>{
-    return Meteor.subscribe('apicDevices',hostName)
-  };
+  let aicDevicesItemsSub = Meteor.subscribe('apicDevices',"fpi");
   let prtgArray = Session.get('myMethodResult');
   let dbData = ItemsApicDevices.find().fetch()
   sortBy = (findValue,sortValue, sortOrder) =>{
@@ -100,12 +98,12 @@ export default createContainer(({params}) => {
     let keyObj ={};
     keyObj[keyString] = sortOrder
     sortObj["sort"] = keyObj;
-    aicDevicesItemsSub(findValue)
+
     return ItemsApicDevices.find({"siteData.dataObj.normalizeHostName":{$regex: findValue}},sortObj).fetch();
   }
   return {
     showAll,
-    ready: "aicDevicesItemsSub.ready()",
+    ready: aicDevicesItemsSub.ready(),
     dbReturn: function data(findValue,sortValue, sortOrder){
       //debug
       //console.log(sortBy(sortValue, sortOrder))
