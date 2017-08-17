8650295304a6f8ec05a3f209e450d76f1b75b2fa
rohscx <R_E__g_@Macintosh.local>
2017-08-17 16:06:30 -0400

    setup

--- imports/client/components/PrimeHostPortInfo/Layout.js
+++ imports/client/components/PrimeHostPortInfo/Layout.js
@@ -69,7 +69,7 @@ export default class Table extends Component {
         return ipAddress;
       }
     }
-    if (findField == "."){
+    if (findField.length >= 1 || findField == "."){
       //console.log(findField.length)
       //console.log(findField)
       let dbData = this.props.dbReturn(findField,sortField,sortOrderField);
