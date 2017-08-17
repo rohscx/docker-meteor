f90380e2b06808271b0ec033663bc5cf5e3c9138
rohscx <R_E__g_@Macintosh.local>
2017-08-17 15:16:17 -0400

    setup

--- server/main.js
+++ server/main.js
@@ -30,7 +30,7 @@ Meteor.publish('primeHosts', function(hostName) {
   let baseUrl = Meteor.settings.private.prime.uName ? Meteor.settings.private.prime.baseUrl : Meteor.settings.public.ciscoApicEM.baseUrl;
   let uName = Meteor.settings.private.prime.uName ? Meteor.settings.private.prime.uName : Meteor.settings.public.ciscoApicEM.uName;
   let uPass = Meteor.settings.private.prime.uName ? Meteor.settings.private.prime.uPass : Meteor.settings.public.ciscoApicEM.uPass;
-  let primeLookupUrn = '/webacs/api/v1/data/Clients.json?.full=true&securityPolicyStatus=eq("FAILED")';
+  let primeLookupUrn = '/webacs/api/v1/data/Clients.json?.full=true&securityPolicyStatus=eq("FAILED")&.maxResults=400';
   let devicesUrl = baseUrl + primeLookupUrn;
   let primeOptions = {
     headers: { 'authorization': uName+" "+uPass, "accept": "application/json" }
