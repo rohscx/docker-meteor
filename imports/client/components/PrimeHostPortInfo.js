c44197af7d47e4eda7ec67e4471ce9e0b60306c0
rohscx <R_E__g_@Macintosh.local>
2017-08-17 09:14:55 -0400

    setup

--- imports/client/components/PrimeHostPortInfo.js
+++ imports/client/components/PrimeHostPortInfo.js
@@ -18,7 +18,7 @@ class PrimeHostPortInfo extends Component {
     let suffix = this.props.util.dnsSuffix
     if (value.length == 3){
       let blah = Meteor.call('getDnsLookup',value+suffix)
-      console.log("asdfad",blah)
+      console.log("asdfad",Meteor.call('getDnsLookup',value+suffix))
     }
   }
 
