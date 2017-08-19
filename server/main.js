d1ca8f81405ce4512672b1cac51da8e856254784
rohscx <R_E__g_@Macintosh.local>
2017-08-19 15:25:57 -0400

    setup

--- server/main.js
+++ server/main.js
@@ -132,8 +132,8 @@ Meteor.publish('apicDevices', function() {
   };
 
   async function httpRequest(method,url,options){
-    const httpDevices = await Meteor.call('httpRequest', method,url,options);
-    const apicDevices = await httpDevices.data.response;
+    let httpDevices = await Meteor.call('httpRequest', method,url,options);
+    let apicDevices = await httpDevices.data.response;
     return await Promise.all(apicDevices.map((data)=>{
       console.log(data)
       let normalize = data.hostname ? data.hostname.toLowerCase() : "Null";
