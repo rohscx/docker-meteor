90fe4cc2e3c32842fac2293872137fa073c697fe
rohscx <R_E__g_@Macintosh.local>
2017-08-20 16:00:22 -0400

    setup

--- server/main.js
+++ server/main.js
@@ -180,10 +180,10 @@ Meteor.publish('apicDevices', function() {
     return await Promise.all(apicDevices.map((data)=>{
       const managementIpAddress = data.managementIpAddress;
       const lastUpdateTime = data.lastUpdateTime;
-      const dataCheck =  ItemsApicDevices.find({"siteData.dataObj.managementIpAddress":managementIpAddress}).fetch()
+      const dataCheck =  ItemsApicDevices.find({"siteData.dataObj.managementIpAddress":managementIpAddress}).fetch();
       const normalize = data.hostname ? data.hostname.toLowerCase() : "Null";
       data.normalizeHostName = normalize;
-      if (dataCheck.length <= 0){
+      const dbInsert = ()=>{
         ItemsApicDevices.insert({
           siteData: {
             dataObj: data,
@@ -191,15 +191,12 @@ Meteor.publish('apicDevices', function() {
             dateTime: dateTime
           }
         });
+      }
+      if (dataCheck.length <= 0){
+        dbInsert()
       } else {
         ItemsApicDevices.remove({"siteData.dataObj.managementIpAddress":managementIpAddress,"siteData.dataObj.lastUpdateTime":{"$lte":lastUpdateTime}});
-        ItemsApicDevices.insert({
-          siteData: {
-            dataObj: data,
-            requestTime: timeNow,
-            dateTime: dateTime
-          }
-        });
+        dbInsert()
       }
     }))
   }
