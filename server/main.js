d754dc326db325b6b0a4b918d6ddab8e01538b25
rohscx <R_E__g_@Macintosh.local>
2017-08-20 10:53:11 -0400

    setup

--- server/main.js
+++ server/main.js
@@ -122,9 +122,9 @@ Meteor.publish('apicDevices', function() {
 
 
   const identClient = (ip)=>{
-    clientId: false,
-    clientIp: ip,
-    genClientId:()=>{
+    this.clientId = false;
+    this.clientIp = ip;
+    this.genClientId = ()=>{
       if (this.clientId === false){
         const newClientId = this.clientIp+":"+Random.id();
         console.log(newClientId)
