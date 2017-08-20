8e44b1b29e68c69f6a731daca8daf5cf683a0ef7
rohscx <R_E__g_@Macintosh.local>
2017-08-20 11:17:06 -0400

    setup

--- server/main.js
+++ server/main.js
@@ -224,12 +224,12 @@ Meteor.publish('apicDevices', function() {
   }
   const intervalId = Meteor.setInterval(()=>{
     counter++;
-    console.log("Apic Data Publish Counter: ",clientIdent(this.connection.clientAddress) +": "+counter);
+    console.log("Apic Data Publish on client %s Counter: %n",clientIdent(this.connection.clientAddress),counter);
     apicTicket()
     return poll();
   },90000)
   self.onStop(()=>{
-    console.log("Terminating Apic Publish Counter After: ",clientIdent(this.connection.clientAddress) +": "+counter);
+    console.log("Terminating Apic Publish on client %s Counter After: %n",clientIdent(this.connection.clientAddress),counter);
     Meteor.clearInterval(intervalId)
   })
   apicTicket()
