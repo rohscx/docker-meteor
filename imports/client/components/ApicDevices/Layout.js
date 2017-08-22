7db2c6346b527875d7c3db68b95d731327f3444d
rohscx <R_E__g_@Macintosh.local>
2017-08-22 09:58:02 -0400

    setup

--- imports/client/components/ApicDevices/Layout.js
+++ imports/client/components/ApicDevices/Layout.js
@@ -81,7 +81,7 @@ export default class Table extends Component {
           let dataToReturn = ""
           vlanArray.map((data,key)=>{
             for (var [key, value] of Object.entries(data)) {
-              let vlanInfo = key+"  "+value+" ";
+              let vlanInfo = key+"  "+value+"</br>";
               console.log(vlanInfo)
               dataToReturn += vlanInfo;
             }
