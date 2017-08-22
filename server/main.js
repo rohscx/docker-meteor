e5b64f2fa30904dc519e54e6d433ac0e19d6349e
rohscx <R_E__g_@Macintosh.local>
2017-08-22 08:22:00 -0400

    setup

--- server/main.js
+++ server/main.js
@@ -184,11 +184,12 @@ Meteor.publish('apicDevices', function() {
       const lastUpdateTime = data.lastUpdateTime;
       const dataCheck = ItemsApicDevices.find({"siteData.dataObj.managementIpAddress":managementIpAddress}).fetch();
       const normalize = data.hostname ? data.hostname.toLowerCase() : "Null";
-      const devicesVlanUrl = baseUrl + "/api/v1/network-device" +"/"+ data.id+"/vlan";
-      const vlanDetail = Meteor.call('apicHttpRequest',"GET",devicesVlanUrl,options);
+      let devicesVlanUrl = baseUrl + "/api/v1/network-device" +"/"+ data.id+"/vlan";
+      let vlanDetail = Meteor.call('apicHttpRequest',"GET",devicesVlanUrl,options);
       data.normalizeHostName = normalize;
       console.log(vlanDetail.statusCode)
       if (vlanDetail.statusCode == 200){
+        console.log("hit")
         data.vlanDetail = vlanDetail.data.response;
       } else {
 
