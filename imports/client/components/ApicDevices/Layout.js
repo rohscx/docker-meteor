1816ef370ffc74767c80c9697505fbe5cca8fdf7
rohscx <R_E__g_@Macintosh.local>
2017-09-06 15:59:01 -0400

    setup

--- imports/client/components/ApicDevices/Layout.js
+++ imports/client/components/ApicDevices/Layout.js
@@ -49,7 +49,7 @@ export default class Table extends Component {
             no ip access-list acl-fia <br/>
             ip access-list acl-fia <br/>
             <div style={textIdent}>
-              permit ip <b contenteditable="true">srcIP</b> <b contenteditable="true">dstIP</b><br/>
+              permit ip <b contentEditable="true">srcIP</b> <b contentEditable="true">dstIP</b><br/>
             </div>
           </p>
           <p>
