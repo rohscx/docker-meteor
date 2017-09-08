d90463af830228059a3dc9e878d63c4a549c335f
rohscx <R_E__g_@Macintosh.local>
2017-09-08 17:10:01 -0400

    setup

--- imports/client/components/ApicDevices/Layout.js
+++ imports/client/components/ApicDevices/Layout.js
@@ -16,45 +16,75 @@ export default class Table extends Component {
     this.closeModal = this.closeModal.bind(this);
   }
 
-  cssGrid() {
-  <style>
-* {
-    box-sizing: border-box;
-}
-
-[class*="col-"] {
-    float: left;
-    padding: 15px;
-    border: 1px solid red;
-}
-.col-1 {width: 8.33%;}
-.col-2 {width: 16.66%;}
-.col-3 {width: 25%;}
-.col-4 {width: 33.33%;}
-.col-5 {width: 41.66%;}
-.col-6 {width: 50%;}
-.col-7 {width: 58.33%;}
-.col-8 {width: 66.66%;}
-.col-9 {width: 75%;}
-.col-10 {width: 83.33%;}
-.col-11 {width: 91.66%;}
-.col-12 {width: 100%;}
-</style>
-</head>
-<body>
-
-
-<div class="row">
-<div class="col-11">
-  <h1>The City</h1>
-  <p>Chania is the capital of the Chania region on the island of Crete. The city can be divided in two parts, the old town and the modern city.</p>
-  <p>Resize the browser window to see how the content respond to the resizing.</p>
-</div>
-<div class="col-1">
-	copy
-</div>
-</div>
+  cssGrid12BoxSize() {
+    cssGrid12BoxSize = {
+      boxSizing: "border-box"
+    }
+    return cssGrid12BoxSize;
   }
+  cssGrid12Class() {
+    cssGrid12Class = {
+      float: "left"
+      padding: "15px"
+      border: "1px solid red"
+    }
+    return cssGrid12Class;
+  }
+
+  cssGrid12Col() {
+    cssGrid12Col = {
+      col1: {
+        width: "8.33%"
+      },
+      col2: {
+        width: "16.66%"
+      },
+      col3: {
+        width: "25%"
+      },
+      col4: {
+        width: "33.33%"
+      },
+      col5: {
+        width: "41.66%"
+      },
+      col6: {
+        width: "50%"
+      },
+      col7: {
+        width: "58.33%"
+      },
+      col8: {
+        width: "66.66%"
+      },
+      col9: {
+        width: "75%"
+      },
+      col10: {
+        width: "83.33%"
+      },
+      col11: {
+        width: "91.66%"
+      },
+      col12: {
+        width: "100%"}
+      }
+      return cssGrid12Col
+  }
+
+
+
+
+    <div class="row">
+    <div class="col-11">
+      <h1>The City</h1>
+      <p>Chania is the capital of the Chania region on the island of Crete. The city can be divided in two parts, the old town and the modern city.</p>
+      <p>Resize the browser window to see how the content respond to the resizing.</p>
+    </div>
+    <div class="col-1">
+    	copy
+    </div>
+    </div>
 
   openModal(vlanData) {
     if (vlanData === null){
@@ -79,48 +109,49 @@ export default class Table extends Component {
   routerPcap() {
     return (
       <Popover id="popover-trigger-click-root-close" title="Router Packet Capture">
-        <div>
-          <p>*         *         *<br/>
-          ##Sets Capture parameters <br/>
-          ip access-list extended HOSTCAP <br/>
-          deny ip any host 224.0.0.2 <br/>
-          permit ip any any <br/></p>
-          <p>*         *         *<br/>
-          ##Starts Capture## <br/>
-          monitor capture CAP1 int
-           <b contentEditable="true" suppressContentEditableWarning={true}><mark>gi0/0/0.200</mark> </b>
-           both access-list HOSTCAP <br/>
-          monitor capture CAP1 start <br/></p>
-
+        <div >
+          <div style={this.cssGrid12BoxSize()}>
+            <div>
+              <p>*         *         *<br/>
+              ##Sets Capture parameters <br/>
+              ip access-list extended HOSTCAP <br/>
+              deny ip any host 224.0.0.2 <br/>
+              permit ip any any <br/></p>
+              <p>*         *         *<br/>
+              ##Starts Capture## <br/>
+              monitor capture CAP1 int
+               <b contentEditable="true" suppressContentEditableWarning={true}><mark>gi0/0/0.200</mark> </b>
+               both access-list HOSTCAP <br/>
+              monitor capture CAP1 start <br/></p>
+            </div>
+          </div>
+          <div>
+            <p>*         *         *<br/>
+            ##View Caputure## <br/>
+            show monitor capture CAP1 buffer detailed | in TCP|# <br/>
+            show monitor capture  CAP1 buffer <br/></p>
+          </div>
+          <div>
+            <p>*         *         *<br/>
+            ##Exports PCAP <br/>
+            monitor capture CAP1 export tftp://
+            <b contentEditable="true" suppressContentEditableWarning={true}><mark>11.16.15.16</mark></b>
+            <b>/</b>
+            <b contentEditable="true" suppressContentEditableWarning={true}><mark>mega-yards1000-r2</mark></b>
+            .pcap </p><br/>
+          </div>
+          <div>
+            <p>*         *         *<br/>
+            ##Stops Capture## <br/>
+            monitor capture CAP1 stop <br/></p>
+          </div>
+          <div>
+            <p>*         *         *<br/>
+            ##removes configuration## <br/>
+            No ip access-list extended HOSTCAP <br/>
+            No monitor capture CAP1 <br/></p>
+          </div>
         </div>
-        <div>
-          <p>*         *         *<br/>
-          ##View Caputure## <br/>
-          show monitor capture CAP1 buffer detailed | in TCP|# <br/>
-          show monitor capture  CAP1 buffer <br/></p>
-        </div>
-        <div>
-          <p>*         *         *<br/>
-          ##Exports PCAP <br/>
-          monitor capture CAP1 export tftp://
-          <b contentEditable="true" suppressContentEditableWarning={true}><mark>11.16.15.16</mark></b>
-          <b>/</b>
-          <b contentEditable="true" suppressContentEditableWarning={true}><mark>mega-yards1000-r2</mark></b>
-          .pcap </p><br/>
-        </div>
-        <div>
-          <p>*         *         *<br/>
-          ##Stops Capture## <br/>
-          monitor capture CAP1 stop <br/></p>
-        </div>
-        <div>
-          <p>*         *         *<br/>
-          ##removes configuration## <br/>
-          No ip access-list extended HOSTCAP <br/>
-          No monitor capture CAP1 <br/></p>
-        </div>
-
-
       </Popover>
     )
   }
