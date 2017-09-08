e15f08444032a7bb1f7848ea02badaccd4fa1beb
rohscx <rohscx@gmail.com>
2017-09-08 16:49:04 -0400

    Update Layout.js

--- imports/client/components/ApicDevices/Layout.js
+++ imports/client/components/ApicDevices/Layout.js
@@ -16,7 +16,45 @@ export default class Table extends Component {
     this.closeModal = this.closeModal.bind(this);
   }
 
+  cssGrid() {
+  <style>
+* {
+    box-sizing: border-box;
+}
 
+[class*="col-"] {
+    float: left;
+    padding: 15px;
+    border: 1px solid red;
+}
+.col-1 {width: 8.33%;}
+.col-2 {width: 16.66%;}
+.col-3 {width: 25%;}
+.col-4 {width: 33.33%;}
+.col-5 {width: 41.66%;}
+.col-6 {width: 50%;}
+.col-7 {width: 58.33%;}
+.col-8 {width: 66.66%;}
+.col-9 {width: 75%;}
+.col-10 {width: 83.33%;}
+.col-11 {width: 91.66%;}
+.col-12 {width: 100%;}
+</style>
+</head>
+<body>
+
+
+<div class="row">
+<div class="col-11">
+  <h1>The City</h1>
+  <p>Chania is the capital of the Chania region on the island of Crete. The city can be divided in two parts, the old town and the modern city.</p>
+  <p>Resize the browser window to see how the content respond to the resizing.</p>
+</div>
+<div class="col-1">
+	copy
+</div>
+</div>
+  }
 
   openModal(vlanData) {
     if (vlanData === null){
