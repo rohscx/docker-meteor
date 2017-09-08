296c9f09142bb902a03b6e7eac3688ea523eaab5
rohscx <R_E__g_@Macintosh.local>
2017-09-08 15:14:07 -0400

    setup

--- imports/client/components/ApicDevices/Layout.js
+++ imports/client/components/ApicDevices/Layout.js
@@ -314,11 +314,8 @@ export default class Table extends Component {
                 {fiaDetail(role) ? <OverlayTrigger trigger="click" rootClose placement="bottom" overlay={this.fiaTrace()}>
                   <Button bsSize="xsmall">fiaTrace</Button>
                 </OverlayTrigger> : ""}
-                {fiaDetail(role) ? <OverlayTrigger trigger="click" rootClose placement="bottom" overlay={this.routerPcap()}>
-                  <Button bsSize="xsmall">rPCAP</Button>
-                </OverlayTrigger> : ""}
                 {switchPcapDetail(series) ? <OverlayTrigger trigger="click" rootClose placement="bottom" overlay={this.switchPcap()}>
-                  <Button bsSize="xsmall">sPCAP</Button>
+                  <Button bsSize="xsmall">swPCAP</Button>
                 </OverlayTrigger> : ""}
               </ButtonToolbar>
             </Row>
