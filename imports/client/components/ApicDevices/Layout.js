b9549e9ad2896e8aac0750fcc229eb5753cb09ff
rohscx <R_E__g_@Macintosh.local>
2017-09-11 11:14:04 -0400

    setup

--- imports/client/components/ApicDevices/Layout.js
+++ imports/client/components/ApicDevices/Layout.js
@@ -18,7 +18,7 @@ export default class Table extends Component {
 
   handleCopyClick(event) {
     let value = event.target.value;
-    console.log(event)
+    console.log(event.currentTarget.textContent)
   }
 
   openModal(vlanData) {
