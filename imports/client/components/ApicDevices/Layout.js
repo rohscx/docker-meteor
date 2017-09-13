f12793d03085f7e2dec02a2e6b63ebad21930d95
rohscx <R_E__g_@Macintosh.local>
2017-09-13 09:30:43 -0400

    setup

--- imports/client/components/ApicDevices/Layout.js
+++ imports/client/components/ApicDevices/Layout.js
@@ -95,25 +95,26 @@ export default class Table extends Component {
 
   routerPcap() {
     const clipboardButton = (clipName)=>{
-      {
-    background-color: #4CAF50; /* Green */
-    border: none;
-    color: white;
-    padding: 15px 32px;
-    text-align: center;
-    text-decoration: none;
-    display: inline-block;
-    font-size: 16px;
-    margin: 4px 2px;
-    cursor: pointer;
-    -webkit-transition-duration: 0.4s; /* Safari */
-    transition-duration: 0.4s;
-        box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24),0 17px 50px 0 rgba(0,0,0,0.19);
-}
+      buttonStyle = {
+      backgroundColor: "#4CAF50" /* Green */
+      border: "none"
+      color: "white"
+      padding: "15px 32px"
+      textAlign: "center"
+      textDecoration: "none"
+      display: "inline-block"
+      fontSize: "16px"
+      margin: "4px 2px"
+      cursor: "pointer"
+      WebkitTransitionDuration: "0.4s "/* Safari */
+      transitionDuration: "0.4s"
+      boxShadow: "0 12px 16px 0 rgba(0,0,0,0.24),0 17px 50px 0 rgba(0,0,0,0.19)"
+      textAlign:"right"
+      }
       return (
         <Row className="show-grid">
           <Col xs={6} xsOffset={6}>
-            <div style={{textAlign:"right"}} onClick={() => {this.handleCopyClick(clipName)}}>
+            <div style={buttonStyle} onClick={() => {this.handleCopyClick(clipName)}}>
               copyPlaceHolder
             </div>
           </Col>
