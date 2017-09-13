9a2ca1c35764b17dd602bbc9f5adf0899144722e
rohscx <rohscx@gmail.com>
2017-09-13 09:24:14 -0400

    Update Layout.js

--- imports/client/components/ApicDevices/Layout.js
+++ imports/client/components/ApicDevices/Layout.js
@@ -95,6 +95,21 @@ export default class Table extends Component {
 
   routerPcap() {
     const clipboardButton = (clipName)=>{
+      {
+    background-color: #4CAF50; /* Green */
+    border: none;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    cursor: pointer;
+    -webkit-transition-duration: 0.4s; /* Safari */
+    transition-duration: 0.4s;
+        box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24),0 17px 50px 0 rgba(0,0,0,0.19);
+}
       return (
         <Row className="show-grid">
           <Col xs={6} xsOffset={6}>
