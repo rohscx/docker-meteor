f0fd01076f348e6879ae623b4ad1a267894e21fb
rohscx <R_E__g_@Macintosh.local>
2017-09-14 16:08:08 -0400

    setup

--- imports/client/utilities/IsRole.js
+++ imports/client/utilities/IsRole.js
@@ -3,7 +3,7 @@ import React, { PropTypes} from 'react';
 const IsRole = ({role, children}) => {
   const cssBlur = {
     color: "transparent",
-    textShadow: "rgba(0, 0, 0, 0.5) 0px 0px 10px",
+    textShadow: "rgba(0, 0, 0, 0.5) 0px 0px 7px",
     userSelect: "none",
     pointerEvents: "none",
     cursor: "default",
