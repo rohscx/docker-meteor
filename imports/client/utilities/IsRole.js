cb8b1ae867c9a3a71972991e24c37ca527fa3057
rohscx <R_E__g_@Macintosh.local>
2017-09-14 13:26:39 -0400

    setup

--- imports/client/utilities/IsRole.js
+++ imports/client/utilities/IsRole.js
@@ -1,13 +1,17 @@
 import React, { PropTypes} from 'react';
 
 const IsRole = ({role, children}) => {
+  const cssBlur = {
+    color: "transparent",
+    textShadow: "rgba(0, 0, 0, 0.5) 0px 0px 5px"
+  }
   // debug
   //console.log(children,)
   //typeof(children)
   if (Roles.userIsInRole(Meteor.userId(), role)) {
     return children;
   }
-  return null;
+  return <div style={cssBlur}>{children}</div>;
 }
 
 // checks type, throws and error. children should be simple object in a div
