de8731c9f37ae15e8f3ce6d3d7acbc5b792f8995
rohscx <R_E__g_@Macintosh.local>
2017-09-14 11:19:30 -0400

    setup

--- imports/client/utilities/IsRole.js
+++ imports/client/utilities/IsRole.js
@@ -1,6 +1,7 @@
 import React, { PropTypes} from 'react';
 
 const IsRole = ({role, children}) => {
+  console.log("typeof",typeOf(children))
   if (Roles.userIsInRole(Meteor.userId(), role)) {
     return children;
   }
