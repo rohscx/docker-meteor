5de457f8e9f75da2727e0cbee5d45e15374f38ba
rohscx <R_E__g_@Macintosh.local>
2017-09-14 13:58:29 -0400

    setup

--- imports/client/utilities/IsRole.js
+++ imports/client/utilities/IsRole.js
@@ -1,6 +1,7 @@
 import React, { PropTypes} from 'react';
 
 const IsRole = ({role, children}) => {
+
   const cssBlur = {
     color: "transparent",
     textShadow: "rgba(0, 0, 0, 0.5) 0px 0px 5px",
@@ -12,7 +13,20 @@ const IsRole = ({role, children}) => {
   if (Roles.userIsInRole(Meteor.userId(), role)) {
     return children;
   }
-  return <div style={cssBlur}>{children}</div>;
+  return (
+    <style dangerouslySetInnerHTML={{
+  __html: [
+     '.my-special-div:after {',
+     '  text-shadow: "rgba(0, 0, 0, 0.5) 0px 0px 5px;"',
+     '  color: "transparent"',
+     '  position: absolute',
+     '  ::selection',
+     '}'
+    ].join('\n')
+  }}>
+</style>
+<div className='my-special-div'>{children}</div>
+  )
 }
 
 // checks type, throws and error. children should be simple object in a div
