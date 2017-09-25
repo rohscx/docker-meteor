f404998ae0b7cdbd6cedb00bd835e24ad88681fa
rohscx <rohscx@gmail.com>
2017-09-25 09:36:32 -0400

    Update Layout.js

--- imports/client/components/ApicDevices/Layout.js
+++ imports/client/components/ApicDevices/Layout.js
@@ -18,6 +18,63 @@ export default class Table extends Component {
     this.closeModal = this.closeModal.bind(this);
   }
 
+  
+  
+  import React from 'react';
+import classNames from 'classnames';
+
+class ScrollDetector extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      message:'not at bottom'
+    };
+    this.handleScroll = this.handleScroll.bind(this);
+  }
+
+  handleScroll() {
+    const windowHeight = "innerHeight" in window ? window.innerHeight : document.documentElement.offsetHeight;
+    const body = document.body;
+    const html = document.documentElement;
+    const docHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight,  html.scrollHeight, html.offsetHeight);
+    const windowBottom = windowHeight + window.pageYOffset;
+    if (windowBottom >= docHeight) {
+      this.setState({
+        message:'bottom reached'
+      });
+    } else {
+      this.setState({
+        message:'not at bottom'
+      });
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.handleScroll);
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="fixedDiv">{this.state.message}</div>
+        <div className="scrollDiv"></div>
+      </div>
+    );
+  }
+}
+
+ScrollDetector.propTypes = {
+}
+
+ScrollDetector.defaultProps = {
+}
+
+export default ScrollDetector;
 
   handleCopyClick = (clipName) => {
 
