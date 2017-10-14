9c3d0bcb91e3eb2382152d5bb6fd562e737612fe
rohscx <R_E__g_@Macintosh.local>
2017-10-14 14:07:42 -0400

    setup

--- server/main.js
+++ server/main.js
@@ -7,7 +7,7 @@ import ItemsTransferRate from '../imports/api/transferRate';
 import ItemsPrimeHosts from '../imports/api/prime';
 import tempData from './tempData';
 
-import  {webServerStatus,blah} from '../imports/server/webServerStatus';
+import  {blah} from '../imports/server/webServerStatus';
 
 import '../imports/server/accounts';
 import '../imports/api/request';
