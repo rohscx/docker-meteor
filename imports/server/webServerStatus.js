8afcc5b1a34bada6512a33ad6cf940adeda1a739
rohscx <R_E__g_@Macintosh.local>
2017-10-14 12:27:44 -0400

    serup

---
+++ imports/server/webServerStatus.js
@@ -0,0 +1,66 @@
+import { Meteor } from 'meteor/meteor';
+
+const webServerStatus = (webServerObj)=>{
+  const statusCodeParser = (statusCode) =>{
+    if (statusCode === 200) {
+      return 0;
+    } else {
+      return 1;
+    }
+  }
+  const getTimeNow = () =>{
+    return new Date().getTime()
+  }
+  const convertDateTime = (dateString) =>{
+    let someDate = new Date(dateString);
+    return someDate.getTime();
+  }
+  httpReturn.data.response.map((data)=>{}
+
+  const webServerMethod = "GET";
+  const webServerUrl = data.webServerUrl;
+  const webServerOptions = {};
+  const currentTime = getTimeNow()
+  async function httpRequest(method,url,options){
+    const httpDevices = await Meteor.call('httpRequest', method,url,options);
+    const httpReturn = await httpDevices;
+    const httpReturnTime = convertDateTime(httpReturn.date);
+    // error checking REST request. If not 200 do nothing and log
+    // http status code
+
+    // webServers name
+
+    // language discribing the server
+
+    // 0 returned on status code 200, 1 returned on all else
+
+    let dataBaseObj = {
+      name: data.name,
+      description: data.description,
+      url: date.url,
+      statistics:{
+        responseTimeTotal:””,
+        responseTimeLast:””,
+        reaponseTimeCount:””,
+        responseTimeHighest:””,
+        responseTimeLowest:””
+      },
+      httpRequest:{
+        responseStatusCode: httpReturn.statusCode,
+        webServerFailueStatus: statusCodeParser(httpReturn.statusCode)
+      }
+    }
+
+    const dbInsert = ()=>{
+      ItemsWebServerStatus.insert({
+        siteData: {
+          dataObj: data,
+          requestTime: timeNow(),
+          dateTime: dateTime
+        }
+      });
+    }
+  }
+};
+
+export default webServerStatus;
