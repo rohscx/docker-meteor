1223611a1e497de659b8cea358aa5ab5ebc9814b
rohscx <R_E__g_@Macintosh.local>
2017-10-16 09:35:54 -0400

    setup

--- server/main.js
+++ server/main.js
@@ -2,10 +2,10 @@ import { Meteor } from 'meteor/meteor';
 import { Random } from 'meteor/random';
 import Items from '../imports/api/Items';
 import ItemsPrtg from '../imports/api/prtg';
-import {ItemsApicDevices} from '../imports/api/apic';
+import ItemsApicDevices from '../imports/api/apic';
 import ItemsTransferRate from '../imports/api/transferRate';
 import ItemsPrimeHosts from '../imports/api/prime';
-import ItemsWebServerStatus from '../imports/api/request';
+import {ItemsWebServerStatus} from '../imports/api/request';
 import tempData from './tempData';
 
 import { webServerStatus, blah } from '../imports/server/webServerStatus';
