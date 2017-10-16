e4df94ba14a997b37fe065d448d3b74ae56868ad
rohscx <R_E__g_@Macintosh.local>
2017-10-16 11:17:40 -0400

    setup

--- imports/client/components/WebServerStatus/ViewGenerator.js
+++ imports/client/components/WebServerStatus/ViewGenerator.js
@@ -49,7 +49,7 @@ export default class ViewGenerator extends Component {
     //this.props.apicDbReady(true)
     return colData;
     */
-    return dbData
+    return dbData["0"]._id
   }
 render() {
     //console.log(this)
