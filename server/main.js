73881e71cd8da28990f50d1b6e70b24f4895d2f9
rohscx <R_E__g_@Macintosh.local>
2017-10-16 11:05:26 -0400

    setup

--- server/main.js
+++ server/main.js
@@ -122,7 +122,7 @@ Meteor.publish('webServerStatus', function() {
       }
       if (ticketIdleTimeout === 0 && ticketSessionTimeout === 0 ){
         let httpRequest = Meteor.call('apicTicket', "POST",ticketUrl,apicTicketOptions);
-        console.log(httpRequest)
+        console.log("error",httpRequest)
         if (httpRequest) {
           oldApicTicket = httpRequest.data.response.serviceTicket;
           setTimeouts(1800,21600);
