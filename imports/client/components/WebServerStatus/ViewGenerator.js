938f18d00a6c05ad1648854bcdba0c36699f479d
rohscx <R_E__g_@Macintosh.local>
2017-10-18 12:59:41 -0400

    setup

--- imports/client/components/WebServerStatus/ViewGenerator.js
+++ imports/client/components/WebServerStatus/ViewGenerator.js
@@ -35,7 +35,7 @@ export default class ViewGenerator extends Component {
       let flexObj1 = (color)=> {
         return {
           backgroundColor: color,
-          width: "120px",
+          width: "80px",
           height: "flex",
           margin: "10px",
         };
