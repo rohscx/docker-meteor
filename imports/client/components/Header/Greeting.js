d781d1f6d4c348897a5f392157c4dc8f09d7fd4b
rohscx <R_E__g_@Macintosh.local>
2017-10-18 13:15:00 -0400

    setup

--- imports/client/components/Header/Greeting.js
+++ imports/client/components/Header/Greeting.js
@@ -5,11 +5,24 @@ import {PageHeader} from 'react-bootstrap';
 export default class Greeting extends Component {
 
   render() {
-    const pageHeaderStyles = {
-      textAlign:"center"
+    const divStylesGenerator = ()=>{
+      // debug
+      console.log("Mobile Device Connected",/Mobi/.test(navigator.userAgent))
+      let divStyles = (widthPercent)=>{
+        return {
+          width: widthPercent
+        };
+      };
+      // dynamically set the the width of the primary div. Mobile devices are 100%
+      if (/Mobi/.test(navigator.userAgent) === true) {
+        return divStyles("90%");
+      } else {
+        return divStyles("100%");
+      }
     };
+    
     return(
-      <div>
+      <div {divStylesGenerator()}>
         <PageHeader><p><small>{this.props.greeting}</small></p></PageHeader>
       </div>
     )
