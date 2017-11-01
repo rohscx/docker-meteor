01e271465bae21e4b7d9dd35f69b3ea9795980bc
rohscx <R_E__g_@Macintosh.local>
2017-11-01 14:44:13 -0400

    setup

--- imports/client/components/ApicDevices.js
+++ imports/client/components/ApicDevices.js
@@ -48,7 +48,43 @@ class TransferRate extends Component {
     this.props.getDevices();
   }
 
+
   downloadData(){
+    const LoadingButton = React.createClass({
+      getInitialState() {
+        return {
+          isLoading: false,
+        };
+      },
+
+      render() {
+        let isLoading = this.state.isLoading;
+        return (
+          <Button
+            bsStyle="primary"
+            disabled={isLoading}
+            onClick={!isLoading ? this.handleClick : null}
+          >
+            {isLoading ? 'Loading...' : 'Loading state'}
+          </Button>
+        );
+      },
+
+      handleClick() {
+        this.setState({ isLoading: true });
+
+        // This probably where you would have an `ajax` call
+        setTimeout(() => {
+          // Completed of async action, set loading state back
+          this.setState({ isLoading: false });
+        }, 2000);
+      },
+    });
+
+    return (
+      <LoadingButton/>
+    )
+    /*
     return (
       <div>
         <ButtonToolbar>
@@ -56,6 +92,7 @@ class TransferRate extends Component {
         </ButtonToolbar>
       </div>
     )
+    */
     //return <FileDownload fileName="dataDownload.csv" fileText="RandomeDatadadsfasdf asdnd Stuff"/>
   }
 
