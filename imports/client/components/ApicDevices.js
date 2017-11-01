f7132b5b4142e7e9064e54bff19ed9ff92709cb2
rohscx <R_E__g_@Macintosh.local>
2017-11-01 15:16:31 -0400

    setup

--- imports/client/components/ApicDevices.js
+++ imports/client/components/ApicDevices.js
@@ -185,7 +185,7 @@ class TransferRate extends Component {
     const divStyles = {
       paddingBottom:"5%"
     };
-    //console.log(this);
+    console.log(this);
     return(
       <div style={divStyles}>
         {this.form()}
