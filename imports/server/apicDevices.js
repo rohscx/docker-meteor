b2aef3a72fe6c11b4138578fab407b8eb1fc9b8e
rohscx <R_E__g_@Macintosh.local>
2017-11-03 16:53:01 -0400

    setup

--- imports/server/apicDevices.js
+++ imports/server/apicDevices.js
@@ -184,7 +184,7 @@ let apicDevices = ()=>{
                 console.log(taskStausCall.data.response.endTime)
               }
               while (taskStausCall.data.response.endTime == 'undefined' );
-              //console.log(taskStausCall)
+              console.log(askStausCall.data.response.endTime);
             }
             if (roleStatus[0].role =="ROLE_ADMIN" ){
               //console.log(roleStatus[0].role)
