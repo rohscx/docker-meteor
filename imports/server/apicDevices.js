98e30dccb0ed9ca1aca83be62e52ba60efd11a50
rohscx <R_E__g_@Macintosh.local>
2017-11-06 12:36:12 -0500

    setup

--- imports/server/apicDevices.js
+++ imports/server/apicDevices.js
@@ -163,7 +163,7 @@ let apicDevices = ()=>{
             const interfaceInfoCall = Meteor.call('apicHttpRequest',"GET",interfaceInfoUrl,options);
             if (interfaceInfoCall.statusCode == 200){
               // creates a record of when the interface has gone down, and how long it has been down
-              console.log(dataCheck[0].dataObj.siteData.interfaceDetail)
+              console.log(dataCheck[0])
               interfaceInfoCall.data.response.map((data,index)=>{
                 if (data.status == "down") {
                   if (!data.downAsOf){
