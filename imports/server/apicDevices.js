22887c45f7f5a62bec233e5d1417aba059fa8a79
rohscx <R_E__g_@Macintosh.local>
2017-11-06 11:14:45 -0500

    setup

--- imports/server/apicDevices.js
+++ imports/server/apicDevices.js
@@ -164,7 +164,7 @@ let apicDevices = ()=>{
             if (interfaceInfoCall.statusCode == 200){
               // creates a record of when the interface has gone down, and how long it has been down
               interfaceInfoCall.data.response.map((data,index)=>{
-                //console.log(dataCheck[0].siteData.dataObj.interfaceDetail)
+                console.log(dataCheck[0].siteData.dataObj.interfaceDetail)
                 if (data.status == "down") {
                   dataCheck[0].siteData.dataObj.interfaceDetail.map((data1,index1)=>{
                     // debug
