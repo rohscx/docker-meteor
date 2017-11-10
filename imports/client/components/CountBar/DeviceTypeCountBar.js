cb19f225ecd09b83e8a1a269b4a6549b8baccdb1
rohscx <R_E__g_@Macintosh.local>
2017-11-10 11:09:33 -0500

    setup

--- imports/client/components/CountBar/DeviceTypeCountBar.js
+++ imports/client/components/CountBar/DeviceTypeCountBar.js
@@ -63,7 +63,7 @@ export default class DeviceTypeCountBar extends Component {
         <div key={1} style= {divStyles} target="_blank" onClick={(event) => {event.preventDefault(); window.open("www.google.com")}} >
           <div style= {{backgroundColor:"#5cb85c",width: "299px",height: "flex",margin: "5px"}}>
             <Row className="show-grid" style={rowStylesMain} className="container-fluid">
-              <Col xs={6} sm={6} md={6}>{data.siteData.dataObj.family}</Col>
+              <Col xs={6} sm={6} md={6}>{data.siteData}</Col>
             </Row>
             <Row className="show-grid" className="container-fluid">
               <Col xs={6} sm={6} md={12}>{dbData}</Col>
