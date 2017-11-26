3c5e629e6869c0c78e63c0badeccc224414cfb8e
rohscx <R_E__g_@Macintosh.local>
2017-11-26 17:53:44 -0500

    setup

--- imports/client/components/ApicDevices/ApicModal.js
+++ imports/client/components/ApicDevices/ApicModal.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
 import createReactClass from 'create-react-class';
 import {Row,Col,Clearfix,Popover,ButtonToolbar,OverlayTrigger,Button,Tooltip,Modal} from 'react-bootstrap';
 
