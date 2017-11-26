78c92fe0019064cc3b5bd6173a237b37ccece5cb
rohscx <R_E__g_@Macintosh.local>
2017-11-26 13:06:50 -0500

    setup

--- imports/client/components/ApicDevices/ApicModal.js
+++ imports/client/components/ApicDevices/ApicModal.js
@@ -45,7 +45,7 @@ class ApicModal extends React.Component {
           {that.props.modalName}
         </Button>
 
-        <Modal {...that.props} bsSize="large" aria-labelledby="contained-modal-title-lg">
+        <Modal show={that.state.lgShow} onHide={that.close} aria-labelledby="contained-modal-title-lg">
           <Modal.Header closeButton>
             <Modal.Title id="contained-modal-title-lg">Modal heading</Modal.Title>
           </Modal.Header>
