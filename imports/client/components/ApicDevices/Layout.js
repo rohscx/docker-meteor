6c5da903705dcc66cc2d0925a43baf24db8a6c4f
rohscx <R_E__g_@Macintosh.local>
2017-11-26 15:56:17 -0500

    setup

--- imports/client/components/ApicDevices/Layout.js
+++ imports/client/components/ApicDevices/Layout.js
@@ -412,7 +412,7 @@ export default class Table extends Component {
             key == "mappedPhysicalInterfaceId" || key == "mappedPhysicalInterfaceName" ||
             key == "ifIndex" || key == "ospfSupport" || key =="lastUpdated" || key == "ipv4Address" ||
             key == "ipv4Mask" || key == "interfaceType" || key == "className" || key == "downAsOf" ||
-            key == "mediaType" || key == "featureVersion" || key == "hostId" || key == "maxUsage"
+            key == "mediaType" || key == "featureVersion" || key == "hostId" || key == "maxUsage" ||
             key == "validityPeriodRemaining"
           )
             {
