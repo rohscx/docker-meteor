4bfb3e81b7356301f4153531f29958b676431a67
rohscx <R_E__g_@Macintosh.local>
2017-11-26 12:44:44 -0500

    setup

--- imports/client/components/ApicDevices/ApicModal.js
+++ imports/client/components/ApicDevices/ApicModal.js
@@ -62,7 +62,7 @@ class ApicModal extends React.Component {
             <hr />
 
             <h4>Overflowing text to show scroll behavior</h4>
-            {that.props.modalData}
+            <div>{that.props.modalData}</div>
           </Modal.Body>
           <Modal.Footer>
             <Button onClick={that.close}>Close</Button>
