dffeed731268d21b3392ef75cedb37ba4db3ee14
rohscx <R_E__g_@Macintosh.local>
2017-11-26 16:06:07 -0500

    setup

--- imports/client/components/ApicDevices/Layout.js
+++ imports/client/components/ApicDevices/Layout.js
@@ -413,7 +413,8 @@ export default class Table extends Component {
             key == "ifIndex" || key == "ospfSupport" || key =="lastUpdated" || key == "ipv4Address" ||
             key == "ipv4Mask" || key == "interfaceType" || key == "className" || key == "downAsOf" ||
             key == "mediaType" || key == "featureVersion" || key == "hostId" || key == "maxUsage" ||
-            key == "validityPeriodRemaining" || key == "usageCountRemaining" || key == "isEulaAccepted"
+            key == "validityPeriodRemaining" || key == "usageCountRemaining" || key == "isEulaAccepted" ||
+            || key == "nativeVlanId"
           )
             {
               // do nothing with these matches
