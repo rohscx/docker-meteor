0c85422d37465d42f0d8fd288575855bb700f7d4
rohscx <R_E__g_@Macintosh.local>
2017-11-29 16:07:32 -0500

    setup

--- imports/api/apic.js
+++ imports/api/apic.js
@@ -165,7 +165,7 @@ if (Meteor.isServer) {
             console.log(data.data.response.progress);
             if (data.data.response.progress != undefined){
               console.log("hit!!!")
-              let responseFileURL = baseUrl +"/api/v1/task/"+responseFileID.data.response.progress.fileId;
+              let responseFileURL = baseUrl +"/api/v1/task/"+data.data.response.progress.fileId;
               console.log("***** ",responseFileURL)
             }
           });
