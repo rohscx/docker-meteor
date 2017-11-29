14d5408e6b1fc9b287f463773639a39687daf140
rohscx <R_E__g_@Macintosh.local>
2017-11-29 10:20:33 -0500

    setup

--- imports/client/components/ApicDevices/Layout.js
+++ imports/client/components/ApicDevices/Layout.js
@@ -6,6 +6,7 @@ import {Row,Col,Clearfix,Popover,ButtonToolbar,OverlayTrigger,Button,Tooltip} fr
 import IsRole from '../../utilities/IsRole';
 import ScrollHandler from '../../utilities/ScrollHandler';
 import ApicModal from './ApicModal';
+import { Meteor } from 'meteor/meteor';
 
 export default class Table extends Component {
   constructor() {
