cbcd0ff4a28ba56c9d03b6469ce944bc62262dc9
rohscx <R_E__g_@Macintosh.local>
2017-11-29 14:00:34 -0500

    setup

--- imports/client/components/ApicDevices.js
+++ imports/client/components/ApicDevices.js
@@ -188,7 +188,7 @@ class ApicDevices extends Component {
   showCommandButton() {
     return (
 
-      <SplitButton bsSize="xsmall" bsStyle={title.toLowerCase()} title={title} key={i} id={`split-button-basic-${i}`} onClick={()=>{Meteor.call('apicShowCommands', function(error, result){ if (error){console.log(error)}})}}>
+      <SplitButton bsSize="xsmall" title="showCommandButton" onClick={()=>{Meteor.call('apicShowCommands', function(error, result){ if (error){console.log(error)}})}}>
           <MenuItem key="1" onSelect={()=>{this.setApicShowCommands("showClock",1)}}>showClock</MenuItem>
           <MenuItem key="2" onSelect={()=>{this.setApicShowCommands("showHSRP",2)}}>showHSRP</MenuItem>
       </SplitButton>
