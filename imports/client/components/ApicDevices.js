6b543205abeba77b9381e78bba3abec34b3b5e48
rohscx <R_E__g_@Macintosh.local>
2017-12-01 22:20:48 -0500

    setup

--- imports/client/components/ApicDevices.js
+++ imports/client/components/ApicDevices.js
@@ -222,7 +222,7 @@ class ApicDevices extends Component {
     }
     return (
 
-      <SplitButton bsSize="xsmall" title="Commands" id="split-button-dropdown" onClick={()=>{commandRunner(showC,deviceI,dbId)}} {disableButton(deviceID,deviceI) ? "disabled" : ""}>
+      <SplitButton bsSize="xsmall" title="Commands" id="split-button-dropdown" onClick={()=>{commandRunner(showC,deviceI,dbId)}}>
           <MenuItem eventKey="1" onSelect={()=>{this.setApicShowCommands("show Clock",deviceID,1)}}>showClock</MenuItem>
           <MenuItem eventKey="2" onSelect={()=>{this.setApicShowCommands("show standby brief",deviceID,2)}}>showHSRP</MenuItem>
           <MenuItem eventKey="3" onSelect={()=>{this.setApicShowCommands("show run | s bgp",deviceID,3)}}>showBgp</MenuItem>
