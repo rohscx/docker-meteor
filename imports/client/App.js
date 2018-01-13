467d675cb8d016f6c35ef287fe22a9da22e9a019
rohscx <R_E__g_@Macintosh.local>
2018-01-13 17:20:58 -0500

    Code clean up
    Removed unneeded JS modules and training code. Updated Readme

--- imports/client/App.js
+++ imports/client/App.js
@@ -98,23 +98,10 @@ import Header from './components/Header';
 
 
 export default withTracker(({params}) => {
-  let itemsSub = Meteor.subscribe('allItems');
   let userSub = Meteor.subscribe('currentUser');
   let showAll = Session.get('showAll');
-  let itemsArray;
-  if(params.id) {
-    itemsArray = Items.find({_id: params.id}).fetch();
-  } else {
-    itemsArray = Items.find({}, {
-      // ternary operator. a form of IF THEN statement
-      limit: showAll ? 50 : 1,
-      // value 1 (OLDEST) or -1 (NEWEST) determines directions of lastUpdated
-      sort: {lastUpdated: 1}
-    }).fetch()
-  }
   return {
     showAll,
-    ready: itemsSub.ready() && userSub.ready(),
-    items: itemsArray
+    ready: userSub.ready(),
   }
 })(App);
